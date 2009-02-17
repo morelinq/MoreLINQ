da7e20e2483ba210025b88f44a69fb2778059af0
Atif Aziz <code@raboof.com>
2009-02-17 09:19:19 +0000

    Added Batch operator to break up a sequence into buckets of a given size.

--- MoreLinq.Test/Pull/GroupingTest.cs
+++ MoreLinq.Test/Pull/GroupingTest.cs
@@ -1,6 +1,8 @@
 namespace MoreLinq.Test.Pull
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using MoreLinq.Pull;
     using NUnit.Framework;
     using NUnit.Framework.SyntaxHelpers;
@@ -250,6 +252,94 @@ namespace MoreLinq.Test.Pull
                 delegate { throw new NotImplementedException(); });
         }
         
+        #endregion
+
+        #region Batch
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BatchNullSequence()
+        {
+            Grouping.Batch<object>(null, 1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BatchZeroSize()
+        {
+            Grouping.Batch(new object[0], 0);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void BatchNegativeSize()
+        {
+            Grouping.Batch(new object[0], -1);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BatcWithhNullResultSelector()
+        {
+            Grouping.Batch<object, object>(new object[0], 1, null);
+        }
+
+        [Test]
+        public void BatchEvenlyDivisibleSequence()
+        {
+            var result = Grouping.Batch(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 3);
+            using (var reader = Read(result))
+            {
+                reader.Read().AssertSequenceEqual(1, 2, 3);
+                reader.Read().AssertSequenceEqual(4, 5, 6);
+                reader.Read().AssertSequenceEqual(7, 8, 9);
+                reader.ReadEnd();
+            }
+        }
+
+        [Test]
+        public void BatchUnevenlyDivisbleSequence()
+        {
+            var result = Grouping.Batch(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4);
+            using (var reader = Read(result))
+            {
+                reader.Read().AssertSequenceEqual(1, 2, 3, 4);
+                reader.Read().AssertSequenceEqual(5, 6, 7, 8);
+                reader.Read().AssertSequenceEqual(9);
+                reader.ReadEnd();
+            }
+        }
+
+        [Test]
+        public void BatchSequenceTransformingResult()
+        {
+            var result = Grouping.Batch(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 4, batch => batch.Sum());
+            result.AssertSequenceEqual(10, 26, 9);
+        }
+
+        [Test]
+        public void BatchSequenceYieldsBatches()
+        {
+            var result = Grouping.Batch(new[] { 1, 2, 3 }, 2);
+            using (var reader = Read(result))
+            {
+                Assert.That(reader.Read(), Is.Not.InstanceOfType(typeof(ICollection<int>)));
+                Assert.That(reader.Read(), Is.Not.InstanceOfType(typeof(ICollection<int>)));
+                reader.ReadEnd();
+            }
+        }
+
+        [Test]
+        public void BatchIsLazy()
+        {
+            Grouping.Batch(new BreakingSequence<object>(), 1);
+        }
+
+        private static SequenceReader<T> Read<T>(IEnumerable<T> source)
+        {
+            return new SequenceReader<T>(source);
+        }
+
         #endregion
     }
 }
