64b0ca148455a9814fd1857f43165e273be42fc8
Chris Ammerman <cammerman@gmail.com>
2009-02-18 03:17:15 +0000

    Added Series class with Expand method for generating sequences according to a delegate.

---
+++ MoreLinq.Test/Pull/SeriesTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using MoreLinq.Pull;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace MoreLinq.Test.Pull
+{
+    [TestFixture]
+    public class SeriesTest
+    {
+        [Test]
+        public void ExpandTerminatesWhenCheckReturnsFalse()
+        {
+            var result = Series.Expand<int>(1, n => n + 2).TakeWhile(n => n < 10);
+
+            result.AssertSequenceEqual(1, 3, 5, 7, 9);
+        }
+
+        [Test]
+        public void ExpandProcessesNonNumerics()
+        {
+            var result = Series.Expand<string>("", s => s + 'a').TakeWhile(s => s.Length < 5);
+
+            result.AssertSequenceEqual("", "a", "aa", "aaa", "aaaa");
+        }
+
+        [Test]
+        public void ExpandIsLazy()
+        {
+            Func<int, int> generateFail =
+                n =>
+                {
+                  throw new InvalidOperationException();
+                };
+
+            var result = Series.Expand<int>(0, generateFail).TakeWhile(n => false);
+
+            result.Exhaust();
+        }
+    }
+}
