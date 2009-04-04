eb300d0a5a0bd1e3ab631f3bad1d80c13c15ec09
Atif Aziz <code@raboof.com>
2009-04-04 18:18:11 +0000

    Re-organized SideEffectsTest as EnumerableTest.

--- MoreLinq.Test/Pull/TraceTest.cs
+++ MoreLinq.Test/Pull/TraceTest.cs
@@ -2,101 +2,25 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
     using System.Diagnostics;
     using System.Globalization;
     using System.IO;
     using MoreLinq.Pull;
     using NUnit.Framework;
-    using NUnit.Framework.SyntaxHelpers;
-    using Enumerable = MoreLinq.Pull.Enumerable;
 
-    [TestFixture]
-    public class SideEffectsTest
+    partial class EnumerableTest
     {
-        #region ForEach
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ForEachNullSequence()
-        {
-            Enumerable.ForEach<int>(null, x => { throw new InvalidOperationException(); });
-        }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ForEachNullAction()
-        {
-            Enumerable.ForEach(new[] { 1, 2, 3 }, null);
-        }
-
-        [Test]
-        public void ForEachWithSequence()
-        {
-            List<int> results = new List<int>();
-            Enumerable.ForEach(new[] { 1, 2, 3 }, results.Add);
-            results.AssertSequenceEqual(1, 2, 3);
-        }
-        #endregion
-
-        #region Pipe
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void PipeNullSequence()
-        {
-            Enumerable.Pipe<int>(null, x => { throw new InvalidOperationException(); });
-        }
-
-        [Test]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void PipeNullAction()
-        {
-            Enumerable.Pipe(new[] { 1, 2, 3 }, null);
-        }
-
-        [Test]
-        public void PipeWithSequence()
-        {
-            List<int> results = new List<int>();
-            var returned = Enumerable.Pipe(new[] { 1, 2, 3 }, results.Add);
-            // Lazy - nothing has executed yet
-            Assert.That(results, Is.Empty);
-            returned.AssertSequenceEqual(1, 2, 3);
-            // Now it has...
-            results.AssertSequenceEqual(1, 2, 3);
-        }
-
-        [Test]
-        public void PipeIsLazy()
-        {
-            new BreakingSequence<int>().Pipe(x => { });
-        }
-
-        [Test]
-        public void PipeActionOccursBeforeYield()
-        {
-            var source = new[] { new StringBuilder(), new StringBuilder() };
-            // The action will occur "in" the pipe, so by the time Where gets it, the
-            // sequence will be empty.
-            Assert.That(source.Pipe(sb => sb.Append("x"))
-                              .Where(x => x.Length==0), 
-                        Is.Empty);
-        }
-        #endregion
-
-        #region Trace
-
-        [Test]
+        [Test, Category("SideEffects")]
         public void TraceSequence()
         {
             var trace = Lines(CaptureTrace(delegate
             {
-                Enumerable.Trace("the quick brown fox".Split()).Consume(); 
+                Enumerable.Trace("the quick brown fox".Split()).Consume();
             }));
             trace.AssertSequenceEqual("the", "quick", "brown", "fox");
         }
 
-        [Test]
+        [Test, Category("SideEffects")]
         public void TraceSequenceWithSomeNullElements()
         {
             var trace = Lines(CaptureTrace(delegate
@@ -106,7 +30,7 @@
             trace.AssertSequenceEqual("1", string.Empty, "2", string.Empty, "3");
         }
 
-        [Test]
+        [Test, Category("SideEffects")]
         public void TraceSequenceWithSomeNullReferences()
         {
             var trace = Lines(CaptureTrace(delegate
@@ -117,7 +41,7 @@
             trace.AssertSequenceEqual("the", string.Empty, "quick", string.Empty, "brown", string.Empty, "fox");
         }
 
-        [Test]
+        [Test, Category("SideEffects")]
         public void TraceSequenceWithFormatting()
         {
             var trace = Lines(CaptureTrace(delegate
@@ -129,20 +53,20 @@
             trace.AssertSequenceEqual("1,234", "5,678");
         }
 
-        [Test]
+        [Test, Category("SideEffects")]
         [ExpectedException(typeof(ArgumentNullException))]
         public void TraceSequenceWithNullFormatter()
         {
-            Enumerable.Trace(new object[0], (Func<object, string>) null);
+            Enumerable.Trace(new object[0], (Func<object, string>)null);
         }
 
-        [Test]
+        [Test, Category("SideEffects")]
         public void TraceSequenceWithFormatter()
         {
             var trace = Lines(CaptureTrace(delegate
             {
                 var formatter = System.Globalization.CultureInfo.InvariantCulture;
-                Enumerable.Trace(new int?[] { 1234, null, 5678 }, 
+                Enumerable.Trace(new int?[] { 1234, null, 5678 },
                     n => n.HasValue ? n.Value.ToString("N0", formatter) : "#NULL").Consume();
             }));
 
@@ -151,8 +75,8 @@
 
         private static IEnumerable<string> Lines(string str)
         {
-            return Lines(string.IsNullOrEmpty(str) 
-                         ? TextReader.Null 
+            return Lines(string.IsNullOrEmpty(str)
+                         ? TextReader.Null
                          : new StringReader(str));
         }
 
@@ -180,7 +104,5 @@
             }
             return writer.ToString();
         }
-
-        #endregion
     }
 }
