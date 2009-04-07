b1aadf296a4f80752ec2cb25e9de5002de57bf71
Atif Aziz <code@raboof.com>
2009-04-07 10:05:25 +0000

    Added missing test fixture files.

---
+++ MoreLinq.Test/EquiZipTest.cs
@@ -0,0 +1,106 @@
+﻿namespace MoreLinq.Test
+{
+    using System;
+    using NUnit.Framework;
+    using NUnit.Framework.SyntaxHelpers;
+
+    [TestFixture]
+    public class EquiZipTest
+    {
+        private static Tuple<TFirst, TSecond> Tuple<TFirst, TSecond>(TFirst a, TSecond b)
+        {
+            return new Tuple<TFirst, TSecond>(a, b);
+        }
+
+        [Test]
+        public void BothSequencesDisposedWithUnequalLengths()
+        {
+            var longer = DisposeTestingSequence.Of(1, 2, 3);
+            var shorter = DisposeTestingSequence.Of(1, 2);
+
+            // Yes, this will throw... but then we should still have disposed both sequences
+            try
+            {
+                longer.EquiZip(shorter, (x, y) => x + y).Consume();
+            }
+            catch (InvalidOperationException)
+            {
+                // Expected
+            }
+            longer.AssertDisposed();
+            shorter.AssertDisposed();
+
+            // Just in case it works one way but not the other...
+            try
+            {
+                shorter.EquiZip(longer, (x, y) => x + y).Consume();
+            }
+            catch (InvalidOperationException)
+            {
+                // Expected
+            }
+            longer.AssertDisposed();
+            shorter.AssertDisposed();
+        }
+
+        [Test]
+        public void ZipWithEqualLengthSequencesFailStrategy()
+        {
+            var zipped = MoreEnumerable.EquiZip(new[] { 1, 2, 3 }, new[] { 4, 5, 6 },
+                (x, y) => Tuple(x, y));
+            Assert.That(zipped, Is.Not.Null);
+            zipped.AssertSequenceEqual(Tuple(1, 4), Tuple(2, 5), Tuple(3, 6));
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ZipWithFirstSequenceShorterThanSecondFailStrategy()
+        {
+            var zipped = MoreEnumerable.EquiZip(new[] { 1, 2 }, new[] { 4, 5, 6 },
+                (x, y) => Tuple(x, y));
+            Assert.That(zipped, Is.Not.Null);
+            zipped.Consume();
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ZipWithFirstSequnceLongerThanSecondFailStrategy()
+        {
+            var zipped = MoreEnumerable.EquiZip(new[] { 1, 2, 3 }, new[] { 4, 5 },
+                (x, y) => Tuple(x, y));
+            Assert.That(zipped, Is.Not.Null);
+            zipped.Consume();
+            zipped.AssertSequenceEqual(Tuple(1, 4), Tuple(2, 5));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipWithNullFirstSequence()
+        {
+            MoreEnumerable.EquiZip(null, new[] { 4, 5, 6 }, BreakingFunc.Of<int, int, int>());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipWithNullSecondSequence()
+        {
+            MoreEnumerable.EquiZip(new[] { 1, 2, 3 }, null, BreakingFunc.Of<int, int, int>());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipWithNullResultSelector()
+        {
+            MoreEnumerable.EquiZip<int, int, int>(new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, null);
+        }
+
+        [Test]
+        public void ZipIsLazy()
+        {
+            MoreEnumerable.EquiZip<int, int, int>(
+                new BreakingSequence<int>(),
+                new BreakingSequence<int>(),
+                delegate { throw new NotImplementedException(); });
+        }
+    }
+}
---
+++ MoreLinq.Test/ZipLongestTest.cs
@@ -0,0 +1,88 @@
+﻿namespace MoreLinq.Test
+{
+    using System;
+    using NUnit.Framework;
+    using NUnit.Framework.SyntaxHelpers;
+
+    [TestFixture]
+    public class ZipLongestTest
+    {
+        private static Tuple<TFirst, TSecond> Tuple<TFirst, TSecond>(TFirst a, TSecond b)
+        {
+            return new Tuple<TFirst, TSecond>(a, b);
+        }
+
+        [Test]
+        public void BothSequencesDisposedWithUnequalLengths()
+        {
+            var longer = DisposeTestingSequence.Of(1, 2, 3);
+            var shorter = DisposeTestingSequence.Of(1, 2);
+
+            longer.ZipLongest(shorter, (x, y) => x + y).Consume();
+            longer.AssertDisposed();
+            shorter.AssertDisposed();
+
+            // Just in case it works one way but not the other...
+            shorter.ZipLongest(longer, (x, y) => x + y).Consume();
+            longer.AssertDisposed();
+            shorter.AssertDisposed();
+        }
+
+        [Test]
+        public void ZipWithEqualLengthSequences()
+        {
+            var zipped = MoreEnumerable.ZipLongest(new[] { 1, 2, 3 }, new[] { 4, 5, 6 },
+                (x, y) => Tuple(x, y));
+            Assert.That(zipped, Is.Not.Null);
+            zipped.AssertSequenceEqual(Tuple(1, 4), Tuple(2, 5), Tuple(3, 6));
+        }
+
+        [Test]
+        public void ZipWithFirstSequenceShorterThanSecond()
+        {
+            var zipped = MoreEnumerable.ZipLongest(new[] { 1, 2 }, new[] { 4, 5, 6 },
+                (x, y) => Tuple(x, y));
+            Assert.That(zipped, Is.Not.Null);
+            zipped.AssertSequenceEqual(Tuple(1, 4), Tuple(2, 5), Tuple(0, 6));
+        }
+
+        [Test]
+        public void ZipWithFirstSequnceLongerThanSecond()
+        {
+            var zipped = MoreEnumerable.ZipLongest(new[] { 1, 2, 3 }, new[] { 4, 5 },
+                (x, y) => Tuple(x, y));
+            Assert.That(zipped, Is.Not.Null);
+            zipped.AssertSequenceEqual(Tuple(1, 4), Tuple(2, 5), Tuple(3, 0));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipWithNullFirstSequence()
+        {
+            MoreEnumerable.ZipLongest(null, new[] { 4, 5, 6 }, BreakingFunc.Of<int, int, int>());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipWithNullSecondSequence()
+        {
+            MoreEnumerable.ZipLongest(new[] { 1, 2, 3 }, null, BreakingFunc.Of<int, int, int>());
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ZipWithNullResultSelector()
+        {
+            MoreEnumerable.ZipLongest<int, int, int>(new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, null);
+        }
+
+        [Test]
+        public void ZipLongestIsLazy()
+        {
+            MoreEnumerable.ZipLongest<int, int, int>(
+                new BreakingSequence<int>(),
+                new BreakingSequence<int>(),
+                delegate { throw new NotImplementedException(); });
+        }
+    }
+}
