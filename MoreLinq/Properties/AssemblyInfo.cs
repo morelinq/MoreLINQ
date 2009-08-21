220ae3142df875f1d122276fc9d7dd0bf4efcdcc
Atif Aziz <code@raboof.com>
2009-08-21 18:28:12 +0000

    Cleaned-up assembly information attributes in anticipation of release.

--- MoreLinq/Properties/AssemblyInfo.cs
+++ MoreLinq/Properties/AssemblyInfo.cs
@@ -2,41 +2,36 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MoreLinq")]
 #if SILVERLIGHT
-[assembly: AssemblyDescription("MoreLinq for Silverlight")]
+[assembly: AssemblyTitle("MoreLINQ for Silverlight")]
 #else
-[assembly: AssemblyDescription("MoreLinq")]
+[assembly: AssemblyTitle("MoreLINQ")]
 #endif
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Extensions to LINQ to Objects")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MoreLinq")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyProduct("MoreLINQ")]
+[assembly: AssemblyCopyright("Copyright (c) 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// Version numbers
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc632c9d-390e-4902-8c1c-3e57b08c1d38")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
+// Debug or release configuration?
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+// CLS compliance and COM visibility
+
 [assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+// ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("fc632c9d-390e-4902-8c1c-3e57b08c1d38")]
