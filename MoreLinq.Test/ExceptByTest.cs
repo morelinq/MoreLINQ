5c8c01465dc6cd0817c13c0aba55732a50ba5e28
Jon Skeet <skeet@pobox.com>
2009-08-23 15:17:27 +0000

    Added ExceptBy operator.

---
+++ MoreLinq.Test/ExceptByTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MoreLinq.Test
+{
+    [TestFixture]
+    public class ExceptByTest
+    {
+        [Test]
+        public void SimpleExceptBy()
+        {
+            string[] first = { "aaa", "bb", "c", "dddd" };
+            string[] second = { "xx", "y" };
+            var result = first.ExceptBy(second, x => x.Length);
+            result.AssertSequenceEqual("aaa", "dddd");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptByNullFirstSequence()
+        {
+            string[] first = null;
+            string[] second = { "aaa" };
+            first.ExceptBy(second, x => x.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptByNullSecondSequence()
+        {
+            string[] first = { "aaa" };
+            string[] second = null;
+            first.ExceptBy(second, x => x.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptByNullKeySelector()
+        {
+            string[] first = { "aaa" };
+            string[] second = { "aaa" };
+            first.ExceptBy(second, (Func<string, string>)null);
+        }
+        
+        [Test]
+        public void ExceptByIsLazy()
+        {
+            new BreakingSequence<string>().ExceptBy(new string[0], x => x.Length);
+        }
+
+        [Test]
+        public void ExceptByDoesNotRepeatSourceElementsWithDuplicateKeys()
+        {
+            string[] first = { "aaa", "bb", "c", "a", "b", "c", "dddd" };
+            string[] second = { "xx" };
+            var result = first.ExceptBy(second, x => x.Length);
+            result.AssertSequenceEqual("aaa", "c", "dddd");
+        }
+
+        [Test]
+        public void ExceptByWithComparer()
+        {
+            string[] first = { "first", "second", "third", "fourth" };
+            string[] second = { "FIRST" , "thiRD", "FIFTH" };
+            var result = first.ExceptBy(second, word => word, StringComparer.OrdinalIgnoreCase);
+            result.AssertSequenceEqual("second", "fourth");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptByNullFirstSequenceWithComparer()
+        {
+            string[] first = null;
+            string[] second = { "aaa" };
+            first.ExceptBy(second, x => x.Length, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptByNullSecondSequenceWithComparer()
+        {
+            string[] first = { "aaa" };
+            string[] second = null;
+            first.ExceptBy(second, x => x.Length, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExceptByNullKeySelectorWithComparer()
+        {
+            string[] first = { "aaa" };
+            string[] second = { "aaa" };
+            first.ExceptBy(second, null, EqualityComparer<string>.Default);
+        }
+
+        [Test]
+        public void ExceptByNullComparer()
+        {
+            string[] first = { "aaa", "bb", "c", "dddd" };
+            string[] second = { "xx", "y" };
+            var result = first.ExceptBy(second, x => x.Length, null);
+            result.AssertSequenceEqual("aaa", "dddd");
+        }
+
+        [Test]
+        public void ExceptByIsLazyWithComparer()
+        {
+            new BreakingSequence<string>().ExceptBy(new string[0], x => x, StringComparer.Ordinal);
+        }
+    }
+}
---
+++ MoreLinq/ExceptBy.cs
@@ -0,0 +1,97 @@
+﻿#region License and Terms
+//
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008-9 Jonathan Skeet. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreLinq
+{
+    public static partial class MoreEnumerable
+    {
+        /// <summary>
+        /// Returns the set of elements in the first sequence which aren't
+        /// in the second sequence, according to a given key selector.
+        /// </summary>
+        /// <remarks>
+        /// This is a set operation; if multiple elements in <paramref name="first"/> have
+        /// equal keys, only the first such element is returned.
+        /// </remarks>
+        /// <typeparam name="TSource">The type of the elements in the input sequences.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/>.</typeparam>
+        /// <param name="first">The sequence of potentially included elements.</param>
+        /// <param name="second">The sequence of elements whose keys may prevent elements in
+        /// <paramref name="first"/> from being returned.</param>
+        /// <param name="keySelector">The mapping from source element to key.</param>
+        /// <returns>A sequence of elements from <paramref name="first"/> whose key was not also a key for
+        /// any element in <paramref name="second"/>.</returns>
+        public static IEnumerable<TSource> ExceptBy<TSource, TKey>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            Func<TSource, TKey> keySelector)
+        {
+            return ExceptBy(first, second, keySelector, null);
+        }
+
+        /// <summary>
+        /// Returns the set of elements in the first sequence which aren't
+        /// in the second sequence, according to a given key selector.
+        /// </summary>
+        /// <remarks>
+        /// This is a set operation; if multiple elements in <paramref name="first"/> have
+        /// equal keys, only the first such element is returned.
+        /// </remarks>
+        /// <typeparam name="TSource">The type of the elements in the input sequences.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by <paramref name="keySelector"/>.</typeparam>
+        /// <param name="first">The sequence of potentially included elements.</param>
+        /// <param name="second">The sequence of elements whose keys may prevent elements in
+        /// <paramref name="first"/> from being returned.</param>
+        /// <param name="keySelector">The mapping from source element to key.</param>
+        /// <param name="keyComparer">The equality comparer to use to determine whether or not keys are equal.
+        /// If null, the default equality comparer for <c>TSource</c> is used.</param>
+        /// <returns>A sequence of elements from <paramref name="first"/> whose key was not also a key for
+        /// any element in <paramref name="second"/>.</returns>
+        public static IEnumerable<TSource> ExceptBy<TSource, TKey>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            Func<TSource, TKey> keySelector,
+            IEqualityComparer<TKey> keyComparer)
+        {
+            first.ThrowIfNull("first");
+            second.ThrowIfNull("second");
+            keySelector.ThrowIfNull("keySelector");
+            return ExceptByImpl(first, second, keySelector, keyComparer);
+        }
+
+        private static IEnumerable<TSource> ExceptByImpl<TSource, TKey>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            Func<TSource, TKey> keySelector,
+            IEqualityComparer<TKey> keyComparer)
+        {
+            HashSet<TKey> keys = new HashSet<TKey>(second.Select(keySelector), keyComparer);
+            foreach (var element in first)
+            {
+                TKey key = keySelector(element);
+                if (keys.Contains(key))
+                {
+                    continue;
+                }
+                yield return element;
+                keys.Add(key);
+            }
+        }
+    }
+}
