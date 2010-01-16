8502f2db5e19bb08ed722e8529c695269c0d15b3
Johannes Rudolph <jojo.rudolph@gmail.com>
2010-01-16 19:16:08 +0000

    Implemented row based loading for ToDataTable(). Changed implementation to use MemberInfos, extract them from supplied expressions. Added expression and table schema validation. Added tests.

---
+++ MoreLinq.Test/ToDataTableTest.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Data;
+
+namespace MoreLinq.Test
+{
+	[TestFixture, SetUpFixture]
+	public class ToDataTableTest
+	{
+		private class TestObject
+		{
+			public int KeyField;
+			public Nullable<Guid> ANullableGuidField;
+
+			public string AString { get; set; }
+			public Nullable<decimal> ANullableDecimal { get; set; }
+
+			public object this[int index]
+			{
+				get
+				{
+					return new object();
+				}
+				set
+				{ }
+			}
+
+
+			public TestObject(int key)
+			{
+				KeyField = key;
+				ANullableGuidField = Guid.NewGuid();
+
+				ANullableDecimal = key / 3;
+				AString = "ABCDEFGHIKKLMNOPQRSTUVWXYSZ";
+			}
+		}
+
+
+		private IList<TestObject> m_TestObjects;
+
+
+		public ToDataTableTest()
+		{
+			m_TestObjects = new List<TestObject>();
+			for (int i = 0; i < 3; i++)
+			{
+				m_TestObjects.Add(new TestObject(i));
+			}
+		}
+
+
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ToDataTableNullSequence()
+		{
+			IEnumerable<TestObject> source = null;
+			source.ToDataTable();
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void ToDataTableNullTable()
+		{ 
+			DataTable dt = null;
+			m_TestObjects.ToDataTable(dt);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ToDataTableNullMemberExpressionMethod()
+		{
+			Expression<Func<TestObject, object>> expression = null;
+
+			m_TestObjects.ToDataTable<TestObject>(expression);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ToDataTableTableWithWrongColumnNames()
+		{
+			DataTable dt = new DataTable();
+			dt.Columns.Add("Test");
+
+			m_TestObjects.ToDataTable(dt);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ToDataTableTableWithWrongColumnDataType()
+		{
+			DataTable dt = new DataTable();
+			dt.Columns.Add("AString", typeof(int));
+
+			m_TestObjects.ToDataTable(dt, t=>t.AString);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ToDataTableMemberExpressionMethod()
+		{
+			m_TestObjects.ToDataTable(t => t.ToString());
+		}
+
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ToDataTableMemberExpressionNonMember()
+		{
+			m_TestObjects.ToDataTable(t => t.ToString().Length);
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void ToDataTableMemberExpressionIndexer()
+		{
+			m_TestObjects.ToDataTable(t => t[0]);
+		}
+
+		[Test]
+		public void ToDataTableSchemaInDeclarationOrder()
+		{
+			DataTable dt = m_TestObjects.ToDataTable();
+
+			// Assert properties first, then fields, then in declaration order
+
+			Assert.AreEqual("AString", dt.Columns[0].Caption);
+			Assert.AreEqual(typeof(string), dt.Columns[0].DataType);
+
+			Assert.AreEqual("ANullableDecimal", dt.Columns[1].Caption);
+			Assert.AreEqual(typeof(decimal), dt.Columns[1].DataType);
+
+			Assert.AreEqual("KeyField", dt.Columns[2].Caption);
+			Assert.AreEqual(typeof(int), dt.Columns[2].DataType);
+
+			Assert.AreEqual("ANullableGuidField", dt.Columns[3].Caption);
+			Assert.AreEqual(typeof(Guid), dt.Columns[3].DataType);
+			Assert.IsTrue(dt.Columns[3].AllowDBNull);
+
+			Assert.AreEqual(4, dt.Columns.Count);
+		}
+
+		[Test]
+		public void ToDataTableContainsAllElements()
+		{
+			DataTable dt = m_TestObjects.ToDataTable();
+			Assert.AreEqual(m_TestObjects.Count, dt.Rows.Count);
+		}
+		
+		
+		[Test]
+		public void ToDataTableWithExpression()
+		{
+			DataTable dt = m_TestObjects.ToDataTable(t => t.AString);
+
+			Assert.AreEqual("AString", dt.Columns[0].Caption);
+			Assert.AreEqual(typeof(string), dt.Columns[0].DataType);
+
+			Assert.AreEqual(1, dt.Columns.Count);
+		}
+	}
+}
--- MoreLinq/ToDataTable.cs
+++ MoreLinq/ToDataTable.cs
@@ -7,186 +7,225 @@ using System.Reflection;
 
 namespace MoreLinq
 {
-    public static partial class MoreEnumerable
-    {
-        /// <summary>
-        /// Converts a sequence to a <see cref="DataTable"/> object.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The source.</param>
-        /// <returns>
-        /// A <see cref="DataTable"/> representing the source.
-        /// </returns>
-        
-        public static DataTable ToDataTable<T>(this IEnumerable<T> source)
-        {
-            return ToDataTable(source, new DataTable());
-        }
+	public static partial class MoreEnumerable
+	{
+		private static MemberInfo GetAccessedMember(LambdaExpression lambda)
+		{
+			Expression body = lambda.Body;
 
-        /// <summary>
-        /// Appends elements in the sequence as rows of a given <see cref="DataTable"/> object.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <typeparam name="TTable"></typeparam>
-        /// <param name="source">The source.</param>
-        /// <param name="table"></param>
-        /// <returns>
-        /// A <see cref="DataTable"/> or subclass representing the source.
-        /// </returns>
-        
-        public static TTable ToDataTable<T, TTable>(this IEnumerable<T> source, TTable table)
-            where TTable : DataTable
-        {
-            return ToDataTable(source, table, null);
-        }
+			// If it's a field access, boxing was used, we need the field
+			if ((body.NodeType == ExpressionType.Convert) || (body.NodeType == ExpressionType.ConvertChecked))
+			{
+				body = ((UnaryExpression)body).Operand;
+			}
 
-        /// <summary>
-        /// Appends elements in the sequence as rows of a given <see cref="DataTable"/> 
-        /// object with a set of lambda expressions specifying which members (property
-        /// or field) of each element in the sequence will supply the column values.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <param name="source">The source.</param>
-        /// <param name="expressions">Expressions providing access to element members.</param>
-        /// <returns>
-        /// A <see cref="DataTable"/> representing the source.
-        /// </returns>
-        
-        public static DataTable ToDataTable<T>(this IEnumerable<T> source, params Expression<Func<T, object>>[] expressions)
-        {
-            return ToDataTable(source, new DataTable(), expressions);
-        }
+			// Check if the MemberExpression is valid and is a "first level" member access e.g. not a.b.c 
+			MemberExpression memberExpression = body as MemberExpression;
+			if ((memberExpression == null) || (memberExpression.Expression.NodeType != ExpressionType.Parameter))
+			{
+				return null;
+			}
 
-        /// <summary>
-        /// Appends elements in the sequence as rows of a given <see cref="DataTable"/> 
-        /// object with a set of lambda expressions specifying which members (property
-        /// or field) of each element in the sequence will supply the column values.
-        /// </summary>
-        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
-        /// <typeparam name="TTable">The type of the input and resulting <see cref="DataTable"/> object.</typeparam>
-        /// <param name="source">The source.</param>
-        /// <param name="table">The <see cref="DataTable"/> type of object where to add rows</param>
-        /// <param name="expressions">Expressions providing access to element members.</param>
-        /// <returns>
-        /// A <see cref="DataTable"/> or subclass representing the source.
-        /// </returns>
-        
-        public static TTable ToDataTable<T, TTable>(this IEnumerable<T> source, TTable table, params Expression<Func<T, object>>[] expressions)
-            where TTable : DataTable
-        {
-            if (source == null) throw new ArgumentNullException("source");
-            if (table == null) throw new ArgumentNullException("table");
+			return memberExpression.Member;
+		}
 
-            //
-            // If no lambda expressions supplied then reflect them off
-            // the source element type.
-            //
+		private static MemberInfo[] PrepareMemberInfos<T>(Expression<Func<T, object>>[] expressions)
+		{
+			// If no lambda expressions supplied then reflect them off the source element type.
 
-            if (expressions == null || expressions.Length == 0)
-            {
-                var parameter = Expression.Parameter(typeof(T), "e");
-                var expressionz = from m in typeof(T).GetMembers()
-                                  where m.MemberType == MemberTypes.Field
-                                     || m.MemberType == MemberTypes.Property
-                                  select CreateMemberAccessorLambda<T>(parameter, m);
+			MemberInfo[] members = null;
 
-                expressions = expressionz.ToArray();
-            }
-            else
-            {
-                //
-                // Ensure none of the expressions is null.
-                //
+			if (expressions == null || expressions.Length == 0)
+			{
+				var memberz = from m in typeof(T).GetMembers()
+							  where m.MemberType == MemberTypes.Field
+								 || (m.MemberType == MemberTypes.Property && (m as PropertyInfo).GetIndexParameters().Length == 0)
+							  select m;
 
-                if (expressions.Any(e => e == null))
-                    throw new ArgumentException(null, "expressions");
-            }
+				members = memberz.ToArray();
+			}
+			else
+			{
+				//
+				// Ensure none of the expressions is null, a non-MemberExpression or accesses an invalid Member
+				//
 
-            //
-            // Compile the expressions while also digging out the member 
-            // information that may be needed to build the table schema.
-            //
+				if (expressions.Any(e => e == null))
+					throw new ArgumentException("contains null expression", "expressions");
 
-            var memberz = from e in expressions
-                          let member = GetMemberExpression(e)
-                          let type = member.Type
-                          select new
-                          {
-                              member.Member.Name,
-                              Type = type.IsGenericType 
-                                     && typeof(Nullable<>) == type.GetGenericTypeDefinition() 
-                                   ? type.GetGenericArguments()[0] 
-                                   : type,
-                              GetValue = e.Compile(),
-                         };
+				members = expressions.Select(x => GetAccessedMember(x)).ToArray();
 
-            var members = memberz.ToArray();
+				if (members.Any(m => m == null))
+					throw new ArgumentException("contains an invalid member expression", "expressions");
+			}
+			return members;
+		}
 
-            //
-            // If the table has no columns then build the schema.
-            // If it has columns, then bind to each based on the
-            // member name pulled out of the expressions.
-            //
+		private static void PrepareDataTable(DataTable table, MemberInfo[] members)
+		{
+			//
+			// retrieve member information
+			// needed to build or validate the table schema.
+			//
+			var columnInfos = from m in members
+							  let type = m.MemberType == MemberTypes.Property ? ((PropertyInfo)m).PropertyType : ((FieldInfo)m).FieldType
+							  select new
+							  {
+								  m.Name,
+								  Type = type.IsGenericType
+										 && typeof(Nullable<>) == type.GetGenericTypeDefinition()
+									   ? type.GetGenericArguments()[0]
+									   : type,
+							  };
 
-            DataColumn[] columns;
-            if (table.Columns.Count == 0)
-            {
-                columns = members.Select(m => new DataColumn(m.Name, m.Type)).ToArray();
-                table.Columns.AddRange(columns);
-            }
-            else
-            {
-                columns = members.Select(m => table.Columns[m.Name])
-                                 .ToArray();
 
-                for (var i = 0; i < members.Length; i++)
-                {
-                    if (columns[i] == null)
-                        throw new Exception(string.Format("Column named '{0}' is missing.", members[i].Name));
-                }
-            }
+			//
+			// If the table has no columns then build the schema.
+			// If it has columns, validate they are correctly ordered
+			// and of correct datatype. The table can have columns left at the end.
+			//
 
-            //
-            // Builds rows out of elements in the sequence and
-            // add them to the table.
-            //
+			if (table.Columns.Count == 0)
+			{
+				var columns = columnInfos.Select(m => new DataColumn(m.Name, m.Type)).ToArray();
+				table.Columns.AddRange(columns);
+			}
+			else
+			{
+				var expectedColumns = columnInfos.ToArray();
 
-            table.BeginLoadData();
+				for (var i = 0; i < expectedColumns.Length; i++)
+				{
+					if (table.Columns[i].ColumnName != expectedColumns[i].Name)
+						throw new ArgumentException(string.Format("Column named '{0}' is missing or the table has unexpected column ordering.", members[i].Name), "table");
 
-            try
-            {
-                foreach (var element in source)
-                {
-                    var row = table.NewRow();
-                    for (var i = 0; i < members.Length; i++)
-                        row[columns[i]] = members[i].GetValue(element) ?? DBNull.Value;
-                    table.Rows.Add(row);
-                }
-            }
-            finally
-            {
-                table.EndLoadData();
-            }
+					if (table.Columns[i].DataType != expectedColumns[i].Type)
+						throw new ArgumentException(string.Format("Column named '{0}' has wrong datatype.", members[i].Name), "table");
 
-            return table;
-        }
+					// can have more columns at the end
+				}
+			}
+		}
 
-        private static UnaryExpression CreateMemberAccessor(Expression parameter, MemberInfo member)
-        {
-            var access = Expression.MakeMemberAccess(parameter, member);
-            return Expression.Convert(access, typeof(object));
-        }
+		private static UnaryExpression CreateMemberAccessor(Expression parameter, MemberInfo member)
+		{
+			var access = Expression.MakeMemberAccess(parameter, member);
+			return Expression.Convert(access, typeof(object));
+		}
 
-        private static Expression<Func<T, object>> CreateMemberAccessorLambda<T>(ParameterExpression parameter, MemberInfo member)
-        {
-            var accessor = CreateMemberAccessor(parameter, member);
-            return Expression.Lambda<Func<T, object>>(accessor, parameter);
-        }
+		private static Func<T, object[]> CreateShredder<T>(IEnumerable<MemberInfo> members)
+		{
+			var parameter = Expression.Parameter(typeof(T), "e");
 
-        private static MemberExpression GetMemberExpression<T>(Expression<Func<T, object>> e)
-        {
-            var unary = e.Body as UnaryExpression;
-            return (MemberExpression) (unary != null ? unary.Operand : e.Body);
-        }
-    }
+			var initializers = members.Select(m => CreateMemberAccessor(parameter, m))
+									  .Cast<Expression>();
+
+			var array = Expression.NewArrayInit(typeof(object), initializers);
+
+			var lambda = Expression.Lambda<Func<T, object[]>>(array, parameter);
+
+			return lambda.Compile();
+		}
+
+
+		/// <summary>
+		/// Appends elements in the sequence as rows of a given <see cref="DataTable"/> 
+		/// object with a set of lambda expressions specifying which members (property
+		/// or field) of each element in the sequence will supply the column values.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+		/// <typeparam name="TTable">The type of the input and resulting <see cref="DataTable"/> object.</typeparam>
+		/// <param name="source">The source.</param>
+		/// <param name="table">The <see cref="DataTable"/> type of object where to add rows</param>
+		/// <param name="expressions">Expressions providing access to element members.</param>
+		/// <returns>
+		/// A <see cref="DataTable"/> or subclass representing the source.
+		/// </returns>
+
+		public static TTable ToDataTable<T, TTable>(this IEnumerable<T> source, TTable table, params Expression<Func<T, object>>[] expressions)
+			where TTable : DataTable
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			if (table == null) throw new ArgumentNullException("table");
+
+
+
+			MemberInfo[] members = PrepareMemberInfos<T>(expressions);
+
+			PrepareDataTable(table, members);
+
+			var shredder = CreateShredder<T>(members);
+
+			//
+			// Builds rows out of elements in the sequence and
+			// add them to the table.
+			//
+
+			table.BeginLoadData();
+
+			try
+			{
+				foreach (var element in source)
+				{
+					var row = table.NewRow();
+					row.ItemArray = shredder(element);
+					table.Rows.Add(row);
+				}
+			}
+			finally
+			{
+				table.EndLoadData();
+			}
+
+			return table;
+		}
+
+		/// <summary>
+		/// Appends elements in the sequence as rows of a given <see cref="DataTable"/> object.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+		/// <typeparam name="TTable"></typeparam>
+		/// <param name="source">The source.</param>
+		/// <param name="table"></param>
+		/// <returns>
+		/// A <see cref="DataTable"/> or subclass representing the source.
+		/// </returns>
+
+		public static TTable ToDataTable<T, TTable>(this IEnumerable<T> source, TTable table)
+			where TTable : DataTable
+		{
+			return ToDataTable(source, table, null);
+		}
+
+		/// <summary>
+		/// Appends elements in the sequence as rows of a given <see cref="DataTable"/> 
+		/// object with a set of lambda expressions specifying which members (property
+		/// or field) of each element in the sequence will supply the column values.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+		/// <param name="source">The source.</param>
+		/// <param name="expressions">Expressions providing access to element members.</param>
+		/// <returns>
+		/// A <see cref="DataTable"/> representing the source.
+		/// </returns>
+
+		public static DataTable ToDataTable<T>(this IEnumerable<T> source, params Expression<Func<T, object>>[] expressions)
+		{
+			return ToDataTable(source, new DataTable(), expressions);
+		}
+
+		/// <summary>
+		/// Converts a sequence to a <see cref="DataTable"/> object.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+		/// <param name="source">The source.</param>
+		/// <returns>
+		/// A <see cref="DataTable"/> representing the source.
+		/// </returns>
+
+		public static DataTable ToDataTable<T>(this IEnumerable<T> source)
+		{
+			return ToDataTable(source, new DataTable());
+		}
+	}
 }
