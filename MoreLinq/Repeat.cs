e2f5c86cbcab503621ab0d777485b88afe801f01
Leopold Bushkin <ambientlion@gmail.com>
2010-01-17 05:50:25 +0000

    Implementation of the Repeat.cs() operator. This is the logical expansion of the Enumerable.Repeat() operator, except that it repeats the elements of an entire sequence, rather than a single element. It is a building block for other extension methods such as NestedLoops() and Permutations().

---
+++ MoreLinq/Repeat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoreLinq
+{
+    public static partial class MoreEnumerable
+    {
+        /// <summary>
+        /// Repeats the specific sequences <paramref name="repeatCount"/> times.
+        /// </summary>
+        /// <param name="sequence">The sequence to repeat</param>
+        /// <param name="repeatCount">Number of times to repeat the sequence</param>
+        /// <returns>A sequence produced from the repetition of the original source sequence</returns>
+        public static IEnumerable<T> Repeat<T>(this IEnumerable<T> sequence, int repeatCount)
+        {
+            if (repeatCount < 0)
+                throw new ArgumentOutOfRangeException("repeatCount", "Repeat count must be >= 0");
+            return RepeatImpl(sequence, repeatCount);
+        }
+
+        private static IEnumerable<T> RepeatImpl<T>(this IEnumerable<T> sequence, int repeatCount)
+        {
+            while (repeatCount-- > 0)
+            {
+                foreach (var item in sequence)
+                    yield return item;
+            }
+        }
+    }
+}
