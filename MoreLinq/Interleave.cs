d37f7c11019d2154d7ed6957a118191a803d3833
Leopold Bushkin <ambientlion@gmail.com>
2010-01-25 00:56:09 +0000

    Added the implementation of the Interleave operator.
    Interleave combines two or more sequences by visiting each in turn, and returning the first element of each, then the second, third, and so forth. By default, Interleave will skip sequences that have been fully consumed and continue interleaving the remaining sequences. An overload is available where the caller may specify the strategy applied when interleaving sequences of unequal length. The options are Pad, Skip, and Stop. Pad adds default(T) elements in place of each consumed sequence. Skip skips consumed sequences. Stop terminates the interleave operation. This operator behaves in a deferred and streaming manner.

---
+++ MoreLinq/Interleave.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreLinq
+{
+    /// <summary>
+    /// Defines the strategies available when Interleave is passed sequences of unequal length
+    /// </summary>
+    public enum ImbalancedInterleaveStrategy
+    {
+        /// <summary>
+        /// Extends a sequence by padding its tail with default(T)
+        /// </summary>
+        Pad,
+        /// <summary>
+        /// Removes the sequence from the interleave set, and continues interleaving remaining sequences.
+        /// </summary>
+        Skip,
+        /// <summary>
+        /// Stops the interleave operation.
+        /// </summary>
+        Stop,
+    }
+
+    public static partial class MoreEnumerable
+    {
+        /// <summary>
+        /// Interleaves the elements of two or more sequences into a single sequence, skipping sequences as they are consumed
+        /// </summary>
+        /// <remarks>
+        /// Interleave combines sequences by visiting each in turn, and returning the first element of each, followed
+        /// by the second, then the third, and so on. So, for example:<br/>
+        /// <code>
+        /// {1,1,1}.Interleave( {2,2,2}, {3,3,3} ) => { 1,2,3,1,2,3,1,2,3 }
+        /// </code>
+        /// This operator behaves in a deferred and streaming manner.<br/>
+        /// When sequences are of unequal length, this method will skip those sequences that have been fully consumed
+        /// and continue interleaving the remaining sequences.<br/>
+        /// The sequences are interleaved in the order that they appear in the <paramref name="otherSequences"/>
+        /// collection, with <paramref name="sequence"/> as the first sequence.
+        /// </remarks>
+        /// <typeparam name="T">The type of the elements of the source sequences</typeparam>
+        /// <param name="sequence">The first sequence in the interleave group</param>
+        /// <param name="otherSequences">The other sequences in the interleave group</param>
+        /// <returns>A sequence of interleaved elements from all of the source sequences</returns>
+        public static IEnumerable<T> Interleave<T>(this IEnumerable<T> sequence, params IEnumerable<T>[] otherSequences)
+        {
+            return Interleave(sequence, ImbalancedInterleaveStrategy.Skip, otherSequences);
+        }
+
+        /// <summary>
+        /// Interleaves the elements of two or more sequences into a single sequence, applying the specified strategy when sequences are of unequal length
+        /// </summary>
+        /// <remarks>
+        /// Interleave combines sequences by visiting each in turn, and returning the first element of each, followed
+        /// by the second, then the third, and so on. So, for example:<br/>
+        /// <code>
+        /// {1,1,1}.Interleave( {2,2,2}, {3,3,3} ) => { 1,2,3,1,2,3,1,2,3 }
+        /// </code>
+        /// This operator behaves in a deferred and streaming manner.<br/>
+        /// When sequences are of unequal length, this method will use the imbalance strategy specified to
+        /// decide how to continue interleaving the remaining sequences. See <see cref="ImbalancedInterleaveStrategy"/>
+        /// for more information.<br/>
+        /// The sequences are interleaved in the order that they appear in the <paramref name="otherSequences"/>
+        /// collection, with <paramref name="sequence"/> as the first sequence.
+        /// </remarks>
+        /// <typeparam name="T">The type of the elements of the source sequences</typeparam>
+        /// <param name="sequence">The first sequence in the interleave group</param>
+        /// <param name="imbalanceStrategy">Defines the behavior of the operator when sequences are of unequal length</param>
+        /// <param name="otherSequences">The other sequences in the interleave group</param>
+        /// <returns>A sequence of interleaved elements from all of the source sequences</returns>
+        public static IEnumerable<T> Interleave<T>(this IEnumerable<T> sequence, ImbalancedInterleaveStrategy imbalanceStrategy, params IEnumerable<T>[] otherSequences)
+        {
+            sequence.ThrowIfNull("sequence");
+            otherSequences.ThrowIfNull("otherSequences");
+            if (otherSequences.Any(s => s == null))
+                throw new ArgumentNullException("otherSequences", "One or more sequences passed to Interleave was null.");
+
+            return InterleaveImpl(new[] { sequence }.Concat(otherSequences), imbalanceStrategy);
+        }
+
+        private static IEnumerable<T> InterleaveImpl<T>(IEnumerable<IEnumerable<T>> sequences, ImbalancedInterleaveStrategy imbalanceStrategy)
+        {
+            // produce an iterator collection for all IEnumerable<T> instancess passed to us
+            var seqIterators = sequences.Select(s => s.GetEnumerator()).ToList();
+            try
+            {
+                var shouldContinue = true;
+                var consumedIterators = 0;
+                var iterCount = seqIterators.Count;
+
+                while (shouldContinue)
+                {
+                    // advance every iterator and verify a value exists to be yielded
+                    for (var index = 0; index < iterCount; index++)
+                    {
+                        if (!seqIterators[index].MoveNext())
+                        {
+                            // check if all iterators have been consumed and we can terminate
+                            // or if the imbalance strategy informs us that we MUST terminate
+                            if (++consumedIterators == iterCount || imbalanceStrategy == ImbalancedInterleaveStrategy.Stop)
+                            {
+                                shouldContinue = false;
+                                break;
+                            }
+
+                            seqIterators[index].Dispose(); // dispose the iterator sice we no longer need it
+
+                            // otherwise, apply the imbalance strategy
+                            switch (imbalanceStrategy)
+                            {
+                                case ImbalancedInterleaveStrategy.Pad:
+                                    var newIter = seqIterators[index] = Generate(default(T), x => default(T)).GetEnumerator();
+                                    newIter.MoveNext();
+                                    break;
+
+                                case ImbalancedInterleaveStrategy.Skip:
+                                    seqIterators.RemoveAt(index); // no longer visit this particular iterator
+                                    --iterCount; // reduce the expected number of iterators to visit
+                                    --index; // decrement iterator index to compensate for index shifting
+                                    --consumedIterators; // decrement consumer iterator count to stay in balance
+                                    break;
+                            }
+
+                        }
+                    }
+
+                    if (shouldContinue) // only if all iterators could be advanced
+                    {
+                        // yield the values of each iterator's current position
+                        for (var index = 0; index < iterCount; index++)
+                        {
+                            yield return seqIterators[index].Current;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                // ensure all remaining iterators are disposed
+                foreach (var iter in seqIterators)
+                    iter.Dispose();
+            }
+        }
+    }
+}
