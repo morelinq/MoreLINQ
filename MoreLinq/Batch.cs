fe03ba98650ff205b646e40568276747549e1295
Johannes Rudolph <jojo.rudolph@gmail.com>
2010-04-15 15:44:44 +0000

    Improved documentation and comments on the Batch operator.

--- MoreLinq/Batch.cs
+++ MoreLinq/Batch.cs
@@ -27,18 +27,28 @@ namespace MoreLinq
     public static partial class MoreEnumerable
     {
         /// <summary>
-        /// Batches the source sequence into sized buckets.
+		/// Batches the source sequence into sized buckets.
         /// </summary>
+		/// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="size">Size of buckets.</param>
+        /// <returns>A sequence of equally sized buckets containing elements of the source collection.</returns>
+		/// <remarks> This operator uses deferred execution and streams its results (buckets and bucket content).</remarks>
         public static IEnumerable<IEnumerable<TSource>> Batch<TSource>(this IEnumerable<TSource> source, int size)
         {
             return Batch(source, size, x => x);
         }
 
-        /// <summary>
-        /// Batches the source sequence into sized buckets that are then
-        /// transformed into results.
-        /// </summary>
-
+		/// <summary>
+		/// Batches the source sequence into sized buckets and applies a projection to each bucket.
+		/// </summary>
+		/// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
+		/// <typeparam name="TResult">Type of result returned by <paramref name="resultSelector"/>.</typeparam>
+		/// <param name="source">The source sequence.</param>
+		/// <param name="size">Size of buckets.</param>
+		/// <param name="resultSelector">The projection to apply to each bucket.</param>
+		/// <returns>A sequence of projections on equally sized buckets containing elements of the source collection.</returns>
+		/// <remarks> This operator uses deferred execution and streams its results (buckets and bucket content).</remarks>
         public static IEnumerable<TResult> Batch<TSource, TResult>(this IEnumerable<TSource> source, int size,
             Func<IEnumerable<TSource>, TResult> resultSelector)
         {
@@ -55,26 +65,30 @@ namespace MoreLinq
             Debug.Assert(size > 0);
             Debug.Assert(resultSelector != null);
 
-            TSource[] items = null;
+            TSource[] bucket = null;
             var count = 0;
 
             foreach (var item in source)
             {
-                if (items == null)
-                    items = new TSource[size];
+                if (bucket == null)
+                    bucket = new TSource[size];
 
-                items[count++] = item;
+                bucket[count++] = item;
 
+				// The bucket is fully buffered before it's yielded
                 if (count != size)
                     continue;
 
-                yield return resultSelector(items.Select(x => x));
-                items = null;
+				// Select is necessary so bucket contents are streamed too
+                yield return resultSelector(bucket.Select(x => x));
+               
+				bucket = null;
                 count = 0;
             }
 
-            if (items != null && count > 0)
-                yield return resultSelector(items.Take(count));
+			// Return the last bucket with all remaining elements
+            if (bucket != null && count > 0)
+                yield return resultSelector(bucket.Take(count));
         }
     }
 }
