952a1bc290fdf1f82e502035886e6de23f4cd217
Jon Skeet <skeet@pobox.com>
2011-08-02 18:42:55 +0000

    Added or changed copyright statement on all files.

--- MoreLinq.Test/ToDataTableTest.cs
+++ MoreLinq.Test/ToDataTableTest.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿#region License and Terms
+// //
+// // MoreLINQ - Extensions to LINQ to Objects
+// // Copyright (c) 2008-2011 Jonathan Skeet. All rights reserved.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //    http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+// //
+#endregion
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using System.Diagnostics;
-using System.Linq.Expressions;
 using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
 
 namespace MoreLinq.Test
 {
-    using System.Collections;
-    using NUnit.Framework.SyntaxHelpers;
-
     [TestFixture]
     public class ToDataTableTest
     {
--- MoreLinq.Test/ZipTest.cs
+++ MoreLinq.Test/ZipTest.cs
@@ -1,9 +1,28 @@
-﻿namespace MoreLinq.Test
-{
-    using System;
-    using NUnit.Framework;
-    using NUnit.Framework.SyntaxHelpers;
+﻿#region License and Terms
+// //
+// // MoreLINQ - Extensions to LINQ to Objects
+// // Copyright (c) 2008-2011 Jonathan Skeet. All rights reserved.
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //    http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+// //
+#endregion
 
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace MoreLinq.Test
+{
     [TestFixture]
     public class ZipTest
     {
