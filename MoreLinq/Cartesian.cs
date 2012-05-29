b3f2d31735dc66645c2341156d1f35ab476aa65e
Atif Aziz <code@raboof.com>
2012-05-29 14:24:50 +0200

    Renaming Cartesian parameter names to be more conventional

--- MoreLinq/Cartesian.cs
+++ MoreLinq/Cartesian.cs
@@ -9,27 +9,27 @@ namespace MoreLinq
         /// Returns the Cartesian product of two sequences by combining each element of the first set with each in the second
         /// and applying the user=define projection to the pair.
         /// </summary>
-        /// <typeparam name="TA">The type of the elements of <paramref name="sequenceA"/></typeparam>
-        /// <typeparam name="TB">The type of the elements of <paramref name="sequenceB"/></typeparam>
+        /// <typeparam name="TFirst">The type of the elements of <paramref name="first"/></typeparam>
+        /// <typeparam name="TSecond">The type of the elements of <paramref name="second"/></typeparam>
         /// <typeparam name="TResult">The type of the elements of the result sequence</typeparam>
-        /// <param name="sequenceA">The first sequence of elements</param>
-        /// <param name="sequenceB">The second sequence of elements</param>
+        /// <param name="first">The first sequence of elements</param>
+        /// <param name="second">The second sequence of elements</param>
         /// <param name="resultSelector">A projection function that combines elements from both sequences</param>
         /// <returns>A sequence representing the Cartesian product of the two source sequences</returns>
-        public static IEnumerable<TResult> Cartesian<TA, TB, TResult>(this IEnumerable<TA> sequenceA, IEnumerable<TB> sequenceB, Func<TA, TB, TResult> resultSelector)
+        public static IEnumerable<TResult> Cartesian<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
         {
-            sequenceA.ThrowIfNull("sequenceA");
-            sequenceB.ThrowIfNull("sequenceB");
+            first.ThrowIfNull("first");
+            second.ThrowIfNull("second");
             resultSelector.ThrowIfNull("resultSelector");
 
-            return CartesianImpl(sequenceA, sequenceB, resultSelector);
+            return CartesianImpl(first, second, resultSelector);
         }
 
-        private static IEnumerable<TResult> CartesianImpl<TA, TB, TResult>(this IEnumerable<TA> sequenceA, IEnumerable<TB> sequenceB, Func<TA, TB, TResult> resultSelector)
+        private static IEnumerable<TResult> CartesianImpl<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first, IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
         {
-            foreach (var itemA in sequenceA)
-                foreach (var itemB in sequenceB)
-                    yield return resultSelector(itemA, itemB);
+            foreach (var item1 in first)
+                foreach (var item2 in second)
+                    yield return resultSelector(item1, item2);
         }
     }
 }
