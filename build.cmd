1fa25d529463eebe94fb274b65608d49e6fa037d
Atif Aziz <code@raboof.com>
2012-06-03 18:54:37 +0200

    Retiring VS 2008 solutions, keeping only 2010 ones

--- build.cmd
+++ build.cmd
@@ -1,12 +1,12 @@
 @echo off
 setlocal
 pushd "%~dp0"
-set MSBUILD35EXE=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe
-if not exist "%MSBUILD35EXE%" (
-    echo The .NET Framework 3.5 does not appear to be installed on this 
+set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+if not exist "%MSBUILDEXE%" (
+    echo The .NET Framework 4.0 does not appear to be installed on this 
     echo machine, which is required to build the solution.
     exit /b 1
 )
-for %%i in (debug release) do "%MSBUILD35EXE%" "MoreLinq VS2008.sln" /p:Configuration=%%i    
-if "%1"=="all" for %%i in (debug release) do "%MSBUILD35EXE%" MoreLinq.Silverlight.sln /p:Configuration=%%i
+for %%i in (debug release) do "%MSBUILDEXE%" "MoreLinq.sln" /p:Configuration=%%i    
+if "%1"=="all" for %%i in (debug release) do "%MSBUILDEXE%" MoreLinq.Silverlight.sln /p:Configuration=%%i
 popd
