0a382d04130fa0c51ad61f9345bd8a3ae75e1f7f
Atif Aziz <code@raboof.com>
2012-06-16 19:35:39 +0200

    Integrating builddocs into main build batch script

--- builddocs.cmd
+++ builddocs.cmd
@@ -1,7 +1,1 @@
-REM TODO: Don't do this in a batch file!
-
-REM Really force a clean build...
-rmdir /s /q src\MoreLinq\bin\Release
-
-msbuild "MoreLinq.sln" /property:Configuration=Release
-msbuild MoreLinq.shfbproj
+@"%~dp0build" docs
