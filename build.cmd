d47852dd7843ab3e4dd62ea223e35c34f207450b
Atif Aziz <code@raboof.com>
2012-06-17 01:59:09 +0200

    Fixing build batch script tail arg handling

--- build.cmd
+++ build.cmd
@@ -7,6 +7,21 @@ if not exist "%MSBUILDEXE%" (
     echo machine, which is required to build the solution.
     exit /b 1
 )
+if "%1"=="all"  call :sl   %2 %3 %4 %5 %6 %7 %8 %9 & goto :EOF
+if "%1"=="docs" call :docs %2 %3 %4 %5 %6 %7 %8 %9 & goto :EOF
+call :base %*
+goto :EOF
+
+:base
 for %%i in (debug release) do "%MSBUILDEXE%" "MoreLinq.sln" /p:Configuration=%%i %*
-if "%1"=="all" for %%i in (debug release) do "%MSBUILDEXE%" MoreLinq.Silverlight.sln /p:Configuration=%%i %*
-if "%1"=="docs" "%MSBUILDEXE%" MoreLinq.shfbproj %*
+goto :EOF
+
+:sl
+call :base %*
+for %%i in (debug release) do "%MSBUILDEXE%" MoreLinq.Silverlight.sln /p:Configuration=%%i %*
+goto :EOF
+
+:docs
+call :base %*
+"%MSBUILDEXE%" MoreLinq.shfbproj %*
+goto :EOF
