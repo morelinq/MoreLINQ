d45037874127ab7e63f762c1e8fab490e92e5047
Atif Aziz <code@raboof.com>
2012-12-21 08:09:10 +0100

    Stub for Zip when embedded in a C# project targeting BCL 4 or later

---
+++ MoreLinq/Zip.stub.cs
@@ -0,0 +1,4 @@
+// An implementation of Zip for sequences is included in Microsoft .NET 
+// Framework starting with version 4.0 and is identical to Zip in MoreLINQ. 
+// For more information on Zip, see the following reference on MSDN:
+// http://msdn.microsoft.com/en-us/library/dd267698.aspx
