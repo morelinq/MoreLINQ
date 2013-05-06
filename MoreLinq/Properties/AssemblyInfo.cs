9dd1cbc0bd07af3f5e37b580e138ca1429195797
Atif Aziz <code@raboof.com>
2013-05-06 20:46:05 +0200

    Bumping version to 1.0.16006 because while API did not change since 1.0.15631 (beta), there are minor internal implementation changes

--- MoreLinq/Properties/AssemblyInfo.cs
+++ MoreLinq/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using System.Runtime.InteropServices;
 // bld = months_since_2000 x 100 + day_of_month
 // rev = utc_hours_since_midnight + utc_minutes
 
-[assembly: AssemblyVersion("1.0.15631.0")]
-[assembly: AssemblyFileVersion("1.0.15631.834")]
+[assembly: AssemblyVersion("1.0.16006.0")]
+[assembly: AssemblyFileVersion("1.0.16006.1845")]
 
 // Debug or release configuration?
 
