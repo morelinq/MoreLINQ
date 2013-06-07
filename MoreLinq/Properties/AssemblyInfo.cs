16b7e033c9e8720b357bac8e9059cbda86a09341
Atif Aziz <code@raboof.com>
2013-06-07 19:06:22 +0200

    Moving on to v2 (alpha)

--- MoreLinq/Properties/AssemblyInfo.cs
+++ MoreLinq/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using System.Runtime.InteropServices;
 // bld = months_since_2000 x 100 + day_of_month
 // rev = utc_hours_since_midnight + utc_minutes
 
-[assembly: AssemblyVersion("1.0.16006.0")]
-[assembly: AssemblyFileVersion("1.0.16006.1845")]
+[assembly: AssemblyVersion("1.0.16107.0")]
+[assembly: AssemblyFileVersion("1.0.16107.1702")]
 
 // Debug or release configuration?
 
