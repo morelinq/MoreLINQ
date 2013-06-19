cb1ea5eac3c3f7015ca421c6fa8f958de736c13d
Atif Aziz <code@raboof.com>
2013-06-19 19:52:13 +0200

    Build script simplification

--- build.cmd
+++ build.cmd
@@ -1,11 +1,6 @@
 @echo off
 setlocal
-pushd "%~dp0"
-call :main %*
-popd
-goto :EOF
-
-:main
+cd "%~dp0"
 set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 if not exist "%MSBUILDEXE%" (
     echo The .NET Framework 4.0 does not appear to be installed on this 
@@ -13,14 +8,12 @@ if not exist "%MSBUILDEXE%" (
     exit /b 1
 )
 if "%1"=="docs" call :docs %2 %3 %4 %5 %6 %7 %8 %9 & goto :EOF
-call :base %*
-goto :EOF
 
 :base
 for %%i in (debug release) do "%MSBUILDEXE%" "MoreLinq.sln" /v:m /p:Configuration=%%i %*
 goto :EOF
 
 :docs
-call :base %*
+call :base
 "%MSBUILDEXE%" MoreLinq.shfbproj %*
 goto :EOF
