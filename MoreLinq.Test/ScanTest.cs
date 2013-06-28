dbce20d58bc858f8ecad95eed69b6b15c4a38c03
Atif Aziz <code@raboof.com>
2013-06-29 00:34:57 +0200

    Consume is semantically better than Count for purpose of, well, consuming results of a query

--- MoreLinq.Test/ScanTest.cs
+++ MoreLinq.Test/ScanTest.cs
@@ -28,7 +28,7 @@ namespace MoreLinq.Test
         [ExpectedException(typeof(InvalidOperationException))]
         public void ScanEmpty()
         {
-            (new int[] { }).Scan(SampleData.Plus).Count();
+            (new int[] { }).Scan(SampleData.Plus).Consume();
         }
 
         [Test]
