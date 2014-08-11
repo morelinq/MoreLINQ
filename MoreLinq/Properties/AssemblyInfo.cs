2ccaa700d58d6fdb2ccd6c2937077b2048c94cfa
Atif Aziz <code@raboof.com>
2014-08-11 09:12:36 +0200

    Assembly version fix

--- MoreLinq/Properties/AssemblyInfo.cs
+++ MoreLinq/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 // bld = months_since_2000 x 100 + day_of_month
 // rev = utc_hours_since_midnight + utc_minutes
 
-[assembly: AssemblyVersion("1.0.16127.0")]
-[assembly: AssemblyFileVersion("1.0.16127.1813")]
+[assembly: AssemblyVersion("2.0.17511.0")]
+[assembly: AssemblyFileVersion("2.0.17511.620")]
 
 // Debug or release configuration?
 
