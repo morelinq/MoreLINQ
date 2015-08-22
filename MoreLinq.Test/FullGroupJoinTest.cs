b526df48cb7d01097115e9c00321d3bbe33ad1b6
Felipe Sateler <fsateler@gmail.com>
2015-08-22 09:57:40 -0300

    Add FullOuterJoin method

---
+++ MoreLinq.Test/FullGroupJoinTest.cs
@@ -0,0 +1,102 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2015 Jonathan Skeet. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+
+namespace MoreLinq.Test
+{
+    [TestFixture]
+    public class FullGroupJoinTest
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FullGroupFirstNull()
+        {
+            ((IEnumerable<string>)null).FullGroupJoin(Enumerable.Empty<string>(), x => x, x => x, DummySelector);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FullGroupSecondNull()
+        {
+            Enumerable.Empty<string>().FullGroupJoin((IEnumerable<string>)null, x => x, x => x, DummySelector);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FullGroupFirstKeyNull()
+        {
+            Enumerable.Empty<string>().FullGroupJoin(Enumerable.Empty<string>(), (Func<string, string>)null, x => x, DummySelector);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FullGroupSecondKeyNull()
+        {
+            Enumerable.Empty<string>().FullGroupJoin(Enumerable.Empty<string>(), x => x, (Func<string, string>)null, DummySelector);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FullGroupResultSelectorNull()
+        {
+            Enumerable.Empty<string>().FullGroupJoin(Enumerable.Empty<string>(), x => x, x => x, (Func<string, IEnumerable<string>, IEnumerable<string>, string>)null);
+        }
+
+        [Test]
+        public void FullGroupIsLazy()
+        {
+            var listA = new BreakingSequence<int>();
+            var listB = new BreakingSequence<int>();
+
+            listA.FullGroupJoin(listB, x => x, x => x, DummySelector);
+            Assert.True(true);
+        }
+
+        [Test]
+        public void FullGroupJoinsResults()
+        {
+            var listA = new[] { 1, 2 };
+            var listB = new[] { 2, 3 };
+
+            var result = listA.FullGroupJoin(listB, x => x, x => x, (key, first, second) => new { key, first, second }).ToDictionary(a => a.key);
+
+            Assert.AreEqual(3, result.Keys.Count);
+
+            Assert.IsEmpty(result[1].second);
+            Assert.AreEqual(1, result[1].first.Single());
+
+            Assert.IsEmpty(result[3].first);
+            Assert.AreEqual(3, result[3].second.Single());
+
+            Assert.IsNotEmpty(result[2].first);
+            Assert.AreEqual(2, result[2].first.Single());
+            Assert.IsNotEmpty(result[2].second);
+            Assert.AreEqual(2, result[2].second.Single());
+        }
+
+        private static T1 DummySelector<T1, T2, T3>(T1 t1, T2 t2, T3 t3)
+        {
+            return t1;
+        }
+    }
+}
+
