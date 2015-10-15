9765ec8a0211db86ca1cb218a0763205ee0d114d
Tom Glastonbury <tomg@zanyants.com>
2015-10-15 16:28:16 +0100

    Add overload of ForEach that provides the source element index to the action.

--- MoreLinq/ForEach.cs
+++ MoreLinq/ForEach.cs
@@ -38,5 +38,25 @@ namespace MoreLinq
                 action(element);
             }
         }
+
+        /// <summary>
+        /// Immediately executes the given action on each element in the source sequence.
+        /// Each element's index is used in the logic of the action.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the sequence</typeparam>
+        /// <param name="source">The sequence of elements</param>
+        /// <param name="action">The action to execute on each element; the second parameter 
+        /// of the action represents the index of the source element.</param>
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+            int index = 0;
+            foreach (var element in source)
+            {
+                action(element, index++);
+            }
+        }
     }
 }
