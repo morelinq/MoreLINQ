2741ecb6e100fb303716e9c89036e88d682c9cd3
Atif Aziz <code@raboof.com>
2016-02-02 19:16:24 +0100

    ToHashSet type parameter documentation; closes #141

--- MoreLinq/ToHashSet.cs
+++ MoreLinq/ToHashSet.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,14 +27,15 @@ namespace MoreLinq
         /// Returns a <see cref="HashSet{T}"/> of the source items using the default equality
         /// comparer for the type.
         /// </summary>
-        /// <remarks>
-        /// This evaluates the input sequence completely.
-        /// </remarks>
+        /// <typeparam name="TSource">Type of elements in source sequence.</typeparam>
         /// <param name="source">Source sequence</param>
         /// <returns>A hash set of the items in the sequence, using the default equality comparer.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
-        
-        public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source) 
+        /// <remarks>
+        /// This evaluates the input sequence completely.
+        /// </remarks>
+
+        public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source)
         {
             return source.ToHashSet(null);
         }
@@ -43,14 +44,15 @@ namespace MoreLinq
         /// Returns a <see cref="HashSet{T}"/> of the source items using the specified equality
         /// comparer for the type.
         /// </summary>
-        /// <remarks>
-        /// This evaluates the input sequence completely.
-        /// </remarks>
+        /// <typeparam name="TSource">Type of elements in source sequence.</typeparam>
         /// <param name="source">Source sequence</param>
         /// <param name="comparer">Equality comparer to use; a value of null will cause the type's default equality comparer to be used</param>
         /// <returns>A hash set of the items in the sequence, using the default equality comparer.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
-        
+        /// <remarks>
+        /// This evaluates the input sequence completely.
+        /// </remarks>
+
         public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource> comparer)
         {
             if (source == null) throw new ArgumentNullException("source");
