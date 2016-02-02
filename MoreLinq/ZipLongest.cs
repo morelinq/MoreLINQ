0d10437341ac4a7c2f4b95dda652436e0a9cd28c
Atif Aziz <code@raboof.com>
2016-02-02 19:13:44 +0100

    ZipLongest return value documentation; closes #143

--- MoreLinq/ZipLongest.cs
+++ MoreLinq/ZipLongest.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,11 +23,11 @@ namespace MoreLinq
     static partial class MoreEnumerable
     {
         /// <summary>
-        /// Returns a projection of tuples, where each tuple contains the N-th element 
+        /// Returns a projection of tuples, where each tuple contains the N-th element
         /// from each of the argument sequences.
         /// </summary>
         /// <remarks>
-        /// If the two input sequences are of different lengths then the result 
+        /// If the two input sequences are of different lengths then the result
         /// sequence will always be as long as the longer of the two input sequences.
         /// The default value of the shorter sequence element type is used for padding.
         /// This operator uses deferred execution and streams its results.
@@ -46,6 +46,10 @@ namespace MoreLinq
         /// <param name="first">First sequence</param>
         /// <param name="second">Second sequence</param>
         /// <param name="resultSelector">Function to apply to each pair of elements</param>
+        /// <returns>
+        /// A sequence that contains elements of the two input sequences,
+        /// combined by <paramref name="resultSelector"/>.
+        /// </returns>
 
         public static IEnumerable<TResult> ZipLongest<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first,
              IEnumerable<TSecond> second,
@@ -74,14 +78,14 @@ namespace MoreLinq
                     }
                     else
                     {
-                        do { yield return resultSelector(e1.Current, default(TSecond)); } 
+                        do { yield return resultSelector(e1.Current, default(TSecond)); }
                         while (e1.MoveNext());
                         yield break;
                     }
                 }
                 if (e2.MoveNext())
                 {
-                    do { yield return resultSelector(default(TFirst), e2.Current); } 
+                    do { yield return resultSelector(default(TFirst), e2.Current); }
                     while (e2.MoveNext());
                 }
             }
