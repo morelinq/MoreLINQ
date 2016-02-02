3f1c5a6251ae0ffc1c5dca3579d8e4269ab7861f
Atif Aziz <code@raboof.com>
2016-02-02 21:35:09 +0100

    EquiZip return value documentation; closes #134

--- MoreLinq/EquiZip.cs
+++ MoreLinq/EquiZip.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,14 +23,9 @@ namespace MoreLinq
     static partial class MoreEnumerable
     {
         /// <summary>
-        /// Returns a projection of tuples, where each tuple contains the N-th element 
+        /// Returns a projection of tuples, where each tuple contains the N-th element
         /// from each of the argument sequences.
         /// </summary>
-        /// <remarks>
-        /// If the two input sequences are of different lengths then 
-        /// <see cref="InvalidOperationException"/> is thrown.
-        /// This operator uses deferred execution and streams its results.
-        /// </remarks>
         /// <example>
         /// <code>
         /// int[] numbers = { 1, 2, 3, 4 };
@@ -45,7 +40,16 @@ namespace MoreLinq
         /// <param name="first">First sequence</param>
         /// <param name="second">Second sequence</param>
         /// <param name="resultSelector">Function to apply to each pair of elements</param>
-        
+        /// <returns>
+        /// A sequence that contains elements of the two input sequences,
+        /// combined by <paramref name="resultSelector"/>.
+        /// </returns>
+        /// <remarks>
+        /// If the two input sequences are of different lengths then
+        /// <see cref="InvalidOperationException"/> is thrown.
+        /// This operator uses deferred execution and streams its results.
+        /// </remarks>
+
         public static IEnumerable<TResult> EquiZip<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first,
              IEnumerable<TSecond> second,
              Func<TFirst, TSecond, TResult> resultSelector)
