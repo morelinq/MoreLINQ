b4e323d0f2e625ba88a55c9e7735db54402d2fd1
Atif Aziz <code@raboof.com>
2016-02-02 19:19:04 +0100

    Generate return value documentation; closes #136

--- MoreLinq/Generate.cs
+++ MoreLinq/Generate.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ namespace MoreLinq
         /// <param name="generator">
         /// Generator function which takes the previous series element and uses it to generate the next element.
         /// </param>
+        /// <returns>A sequence containing the generated values.</returns>
         /// <remarks>
         /// This function defers element generation until needed and streams the results.
         /// </remarks>
@@ -38,7 +39,7 @@ namespace MoreLinq
         /// IEnumerable&lt;int&gt; result = Sequence.Generate(2, n => n * n).Take(5);
         /// </code>
         /// The <c>result</c> variable, when iterated over, will yield 2, 4, 16, 256, and 65536, in turn.
-        /// </example>        
+        /// </example>
 
         public static IEnumerable<TResult> Generate<TResult>(TResult initial, Func<TResult, TResult> generator)
         {
