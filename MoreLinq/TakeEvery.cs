beb91581b82b0ead394c706b97cb498ac2bd9ae6
Atif Aziz <code@raboof.com>
2016-02-03 20:11:58 +0100

    TakeEvery return value documentation; closes #139

--- MoreLinq/TakeEvery.cs
+++ MoreLinq/TakeEvery.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,11 +24,14 @@ namespace MoreLinq
     static partial class MoreEnumerable
     {
         /// <summary>
-        /// Returns every N-th element of a source sequence.
+        /// Returns every N-th element of a sequence.
         /// </summary>
         /// <typeparam name="TSource">Type of the source sequence</typeparam>
         /// <param name="source">Source sequence</param>
         /// <param name="step">Number of elements to bypass before returning the next element.</param>
+        /// <returns>
+        /// A sequence with every N-th element of the input sequence.
+        /// </returns>
         /// <remarks>
         /// This operator uses deferred execution and streams its results.
         /// </remarks>
@@ -39,7 +42,7 @@ namespace MoreLinq
         /// </code>
         /// The <c>result</c> variable, when iterated over, will yield 1, 3 and 5, in turn.
         /// </example>
-        
+
         public static IEnumerable<TSource> TakeEvery<TSource>(this IEnumerable<TSource> source, int step)
         {
             if (source == null) throw new ArgumentNullException("source");
