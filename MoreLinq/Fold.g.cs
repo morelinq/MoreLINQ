5a0e79f5fecad5a48d2b8cdbf71895e601ba179a
Atif Aziz <code@raboof.com>
2016-02-03 07:26:01 +0100

    Fold return value documentation; closes #135

--- MoreLinq/Fold.g.cs
+++ MoreLinq/Fold.g.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2008 Jonathan Skeet. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,17 +23,18 @@ namespace MoreLinq
     partial class MoreEnumerable
     {
         /// <summary>
-        /// Returns the result of applying a function to a sequence of 
+        /// Returns the result of applying a function to a sequence of
         /// 1 element.
         /// </summary>
         /// <remarks>
-        /// This operator uses immediate execution and effectively buffers 
+        /// This operator uses immediate execution and effectively buffers
         /// as many items of the source sequence as necessary.
         /// </remarks>
         /// <typeparam name="T">Type of element in the source sequence</typeparam>
         /// <typeparam name="TResult">Type of the result</typeparam>
         /// <param name="source">The sequence of items to fold.</param>
         /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
 
         public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, TResult> folder)
         {
@@ -41,17 +42,18 @@ namespace MoreLinq
         }
 
         /// <summary>
-        /// Returns the result of applying a function to a sequence of 
+        /// Returns the result of applying a function to a sequence of
         /// 2 elements.
         /// </summary>
         /// <remarks>
-        /// This operator uses immediate execution and effectively buffers 
+        /// This operator uses immediate execution and effectively buffers
         /// as many items of the source sequence as necessary.
         /// </remarks>
         /// <typeparam name="T">Type of element in the source sequence</typeparam>
         /// <typeparam name="TResult">Type of the result</typeparam>
         /// <param name="source">The sequence of items to fold.</param>
         /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
 
         public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, TResult> folder)
         {
@@ -59,17 +61,18 @@ namespace MoreLinq
         }
 
         /// <summary>
-        /// Returns the result of applying a function to a sequence of 
+        /// Returns the result of applying a function to a sequence of
         /// 3 elements.
         /// </summary>
         /// <remarks>
-        /// This operator uses immediate execution and effectively buffers 
+        /// This operator uses immediate execution and effectively buffers
         /// as many items of the source sequence as necessary.
         /// </remarks>
         /// <typeparam name="T">Type of element in the source sequence</typeparam>
         /// <typeparam name="TResult">Type of the result</typeparam>
         /// <param name="source">The sequence of items to fold.</param>
         /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
 
         public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, TResult> folder)
         {
@@ -77,17 +80,18 @@ namespace MoreLinq
         }
 
         /// <summary>
-        /// Returns the result of applying a function to a sequence of 
+        /// Returns the result of applying a function to a sequence of
         /// 4 elements.
         /// </summary>
         /// <remarks>
-        /// This operator uses immediate execution and effectively buffers 
+        /// This operator uses immediate execution and effectively buffers
         /// as many items of the source sequence as necessary.
         /// </remarks>
         /// <typeparam name="T">Type of element in the source sequence</typeparam>
         /// <typeparam name="TResult">Type of the result</typeparam>
         /// <param name="source">The sequence of items to fold.</param>
         /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
 
         public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, TResult> folder)
         {
