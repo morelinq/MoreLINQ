57295fff782ddc22d57c3883ed70dd77bd06e338
Atif Aziz <code@raboof.com>
2016-03-15 17:52:17 +0100

    Copyright fixes

--- MoreLinq/Properties/AssemblyInfo.cs
+++ MoreLinq/Properties/AssemblyInfo.cs
@@ -25,7 +25,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("MoreLINQ")]
 [assembly: AssemblyCopyright("\u00a9 2008 Jonathan Skeet. " +
                              "Portions \u00a9 2009 Atif Aziz, Chris Ammerman, Konrad Rudolph. " +
-                             "Portions \u00a9 2010 Johannes Rudolph. " +
+                             "Portions \u00a9 2010 Johannes Rudolph, Leopold Bushkin. " +
+                             "Portions \u00a9 2015 Felipe Sateler, \u201csholland\u201d. " +
                              "Portions \u00a9 Microsoft " +
                              "All rights reserved.")]
 [assembly: AssemblyTrademark("")]
