983b3081a7e15731b6e170e194420372eed45613
JamesFaix <jamesthefaix@gmail.com>
2016-04-24 14:55:44 -0400

    Issue #96 Added MaxByOrFallback and related methods
    Added MaxByOrFallback, MaxOrFallback, MinByOrFallback, and
    MinOrFallback.

--- MoreLinq/MaxBy.cs
+++ MoreLinq/MaxBy.cs
@@ -20,7 +20,7 @@ namespace MoreLinq
     using System;
     using System.Collections.Generic;
 
-    static partial class MoreEnumerable
+    partial class MoreEnumerable
     {
         /// <summary>
         /// Returns the maximal element of the given sequence, based on
@@ -39,11 +39,11 @@ namespace MoreLinq
         /// <returns>The maximal element, according to the projection.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="selector"/> is null</exception>
         /// <exception cref="InvalidOperationException"><paramref name="source"/> is empty</exception>
-
         public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> selector)
-        {
-            return source.MaxBy(selector, null);
+            Func<TSource, TKey> selector) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return MaxByOrFallbackImpl(source, selector, null, false, default(TSource));
         }
 
         /// <summary>
@@ -59,35 +59,177 @@ namespace MoreLinq
         /// <typeparam name="TKey">Type of the projected element</typeparam>
         /// <param name="source">Source sequence</param>
         /// <param name="selector">Selector to use to pick the results to compare</param>
-        /// <param name="comparer">Comparer to use to compare projected values</param>
+        /// <param name="comparer">Comparer to use to compare projected values.
+        /// If <c>null</c>, uses default comparer for TKey</param>        
         /// <returns>The maximal element, according to the projection.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="selector"/> 
         /// or <paramref name="comparer"/> is null</exception>
         /// <exception cref="InvalidOperationException"><paramref name="source"/> is empty</exception>
-        
         public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> selector, IComparer<TKey> comparer)
-        {
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer) {
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
+
+            return MaxByOrFallbackImpl(source, selector, comparer, false, default(TSource));
+        }
+
+        /// <summary>
+        /// Returns the maximal element of the given sequence, based on
+        /// the given projection, or a fallback value if sequence is empty.
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the maximal projected value, the first
+        /// one encountered will be returned. This overload uses the default comparer
+        /// for the projected type. This operator uses immediate execution, but
+        /// only buffers a single result (the current maximal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The maximal element, according to the projection, or fallback if empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="selector"/> is null</exception>
+        public static TSource MaxByOrFallback<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            TSource fallback) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return MaxByOrFallbackImpl(source, selector, null, true, fallback);
+        }
+
+        /// <summary>
+        /// Returns the maximal element of the given sequence, based on
+        /// the given projection and the specified comparer for projected values,
+        /// or the given fallback value if the sequence is empty. 
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the maximal projected value, the first
+        /// one encountered will be returned. This operator uses immediate execution, but
+        /// only buffers a single result (the current maximal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <param name="comparer">Comparer to use to compare projected values.  
+        /// If <c>null</c>, uses default comparer for TKey</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The maximal element, according to the projection.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="selector"/> 
+        /// or <paramref name="comparer"/> is null</exception>
+        public static TSource MaxByOrFallback<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer,
+            TSource fallback) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return MaxByOrFallbackImpl(source, selector, comparer, true, fallback);
+        }
+        
+        /// <summary>
+        /// Returns the maximal element of the given sequence, 
+        /// or a fallback value if sequence is empty.
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the maximal value, the first
+        /// one encountered will be returned. This overload uses the default comparer
+        /// for the projected type. This operator uses immediate execution, but
+        /// only buffers a single result (the current maximal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The maximal element, or fallback if empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        public static TSource MaxOrFallback<TSource>(this IEnumerable<TSource> source,
+            TSource fallback) {
+
+            if (source == null) throw new ArgumentNullException("source");
+            return MaxOrFallbackImpl(source, null, true, fallback);
+        }
+
+        /// <summary>
+        /// Returns the maximal element of the given sequence, 
+        /// based on the specified comparer,
+        /// or a fallback value if sequence is empty.
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the maximal value, the first
+        /// one encountered will be returned. This overload uses the default comparer
+        /// for the projected type. This operator uses immediate execution, but
+        /// only buffers a single result (the current maximal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="comparer">Comparer to use to compare projected values.  
+        /// If <c>null</c>, uses default comparer for TKey</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The maximal element, or fallback if empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        public static TSource MaxOrFallback<TSource>(this IEnumerable<TSource> source,
+            IComparer<TSource> comparer,
+            TSource fallback) {
+
+            if (source == null) throw new ArgumentNullException("source");
+            return MaxOrFallbackImpl(source, comparer, true, fallback);
+        }
+
+        private static TSource MaxByOrFallbackImpl<TSource, TKey>(IEnumerable<TSource> source,
+            Func<TSource, TKey> selector, 
+            IComparer<TKey> comparer, 
+            bool fallbackIfEmpty, 
+            TSource fallback) {
+            
             comparer = comparer ?? Comparer<TKey>.Default;
 
-            using (var sourceIterator = source.GetEnumerator())
-            {
-                if (!sourceIterator.MoveNext())
-                {
-                    throw new InvalidOperationException("Sequence contains no elements");
+            using (var sourceIterator = source.GetEnumerator()) {
+                if (!sourceIterator.MoveNext()) {
+                    if (fallbackIfEmpty) {
+                        return fallback;
+                    }
+                    else {
+                        throw new InvalidOperationException("Sequence contains no elements");
+                    }
                 }
                 var max = sourceIterator.Current;
                 var maxKey = selector(max);
-                while (sourceIterator.MoveNext())
-                {
+                while (sourceIterator.MoveNext()) {
                     var candidate = sourceIterator.Current;
                     var candidateProjected = selector(candidate);
-                    if (comparer.Compare(candidateProjected, maxKey) > 0)
-                    {
+                    if (comparer.Compare(candidateProjected, maxKey) > 0) {
                         max = candidate;
                         maxKey = candidateProjected;
+                    }
+                }
+                return max;
+            }
+        }
+
+        private static TSource MaxOrFallbackImpl<TSource>(IEnumerable<TSource> source,
+            IComparer<TSource> comparer,
+            bool fallbackIfEmpty,
+            TSource fallback) {
+
+            comparer = comparer ?? Comparer<TSource>.Default;
+
+            using (var sourceIterator = source.GetEnumerator()) {
+                if (!sourceIterator.MoveNext()) {
+                    if (fallbackIfEmpty) {
+                        return fallback;
+                    }
+                    else {
+                        throw new InvalidOperationException("Sequence contains no elements");
+                    }
+                }
+                var max = sourceIterator.Current;
+                while (sourceIterator.MoveNext()) {
+                    var candidate = sourceIterator.Current;
+                    if (comparer.Compare(candidate, max) > 0) {
+                        max = candidate;
                     }
                 }
                 return max;
--- MoreLinq/MinBy.cs
+++ MoreLinq/MinBy.cs
@@ -20,8 +20,9 @@ namespace MoreLinq
     using System;
     using System.Collections.Generic;
 
-    static partial class MoreEnumerable
+    partial class MoreEnumerable
     {
+
         /// <summary>
         /// Returns the minimal element of the given sequence, based on
         /// the given projection.
@@ -39,11 +40,11 @@ namespace MoreLinq
         /// <returns>The minimal element, according to the projection.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="selector"/> is null</exception>
         /// <exception cref="InvalidOperationException"><paramref name="source"/> is empty</exception>
-        
         public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> selector)
-        {
-            return source.MinBy(selector, null);
+            Func<TSource, TKey> selector) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+            return MinByOrFallbackImpl(source, selector, null, false, default(TSource));
         }
 
         /// <summary>
@@ -59,35 +60,177 @@ namespace MoreLinq
         /// <typeparam name="TKey">Type of the projected element</typeparam>
         /// <param name="source">Source sequence</param>
         /// <param name="selector">Selector to use to pick the results to compare</param>
-        /// <param name="comparer">Comparer to use to compare projected values</param>
+        /// <param name="comparer">Comparer to use to compare projected values.  
+        /// If <c>null</c>, uses default comparer for TKey</param>
         /// <returns>The minimal element, according to the projection.</returns>
         /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="selector"/> 
         /// or <paramref name="comparer"/> is null</exception>
         /// <exception cref="InvalidOperationException"><paramref name="source"/> is empty</exception>
-
         public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source,
-            Func<TSource, TKey> selector, IComparer<TKey> comparer)
-        {
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer) {
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
+
+            return MinByOrFallbackImpl(source, selector, comparer, false, default(TSource));
+        }
+
+        /// <summary>
+        /// Returns the minimal element of the given sequence, based on
+        /// the given projection, or a fallback value if sequence is empty.
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the minimal projected value, the first
+        /// one encountered will be returned. This overload uses the default comparer
+        /// for the projected type. This operator uses immediate execution, but
+        /// only buffers a single result (the current minimal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The minimal element, according to the projection, or fallback if empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="selector"/> is null</exception>
+        public static TSource MinByOrFallback<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            TSource fallback) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return MinByOrFallbackImpl(source, selector, null, true, fallback);
+        }
+
+        /// <summary>
+        /// Returns the minimal element of the given sequence, based on
+        /// the given projection and the specified comparer for projected values,
+        /// or the given fallback value if the sequence is empty. 
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the minimal projected value, the first
+        /// one encountered will be returned. This operator uses immediate execution, but
+        /// only buffers a single result (the current minimal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <typeparam name="TKey">Type of the projected element</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="selector">Selector to use to pick the results to compare</param>
+        /// <param name="comparer">Comparer to use to compare projected values.  
+        /// If <c>null</c>, uses default comparer for TKey</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The minimal element, according to the projection.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="selector"/> 
+        /// or <paramref name="comparer"/> is null</exception>
+        public static TSource MinByOrFallback<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer,
+            TSource fallback) {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            return MinByOrFallbackImpl(source, selector, comparer, true, fallback);
+        }
+
+        /// <summary>
+        /// Returns the minimal element of the given sequence, 
+        /// or a fallback value if sequence is empty.
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the minimal value, the first
+        /// one encountered will be returned. This overload uses the default comparer
+        /// for the projected type. This operator uses immediate execution, but
+        /// only buffers a single result (the current minimal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The minimal element, or fallback if empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        public static TSource MinOrFallback<TSource>(this IEnumerable<TSource> source,
+            TSource fallback) {
+
+            if (source == null) throw new ArgumentNullException("source");
+            return MinOrFallbackImpl(source, null, true, fallback);
+        }
+
+        /// <summary>
+        /// Returns the minimal element of the given sequence, 
+        /// based on the specified comparer,
+        /// or a fallback value if sequence is empty.
+        /// </summary>
+        /// <remarks>
+        /// If more than one element has the minimal value, the first
+        /// one encountered will be returned. This overload uses the default comparer
+        /// for the projected type. This operator uses immediate execution, but
+        /// only buffers a single result (the current minimal element).
+        /// </remarks>
+        /// <typeparam name="TSource">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <param name="comparer">Comparer to use to compare projected values.  
+        /// If <c>null</c>, uses default comparer for TKey</param>
+        /// <param name="fallback">Value to return if source sequence is empty.</param>
+        /// <returns>The minimal element, or fallback if empty.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        public static TSource MinOrFallback<TSource>(this IEnumerable<TSource> source,
+            IComparer<TSource> comparer,
+            TSource fallback) {
+
+            if (source == null) throw new ArgumentNullException("source");
+            return MinOrFallbackImpl(source, comparer, true, fallback);
+        }
+
+        private static TSource MinByOrFallbackImpl<TSource, TKey>(IEnumerable<TSource> source,
+            Func<TSource, TKey> selector,
+            IComparer<TKey> comparer,
+            bool fallbackIfEmpty,
+            TSource fallback) {
+            
             comparer = comparer ?? Comparer<TKey>.Default;
 
-            using (var sourceIterator = source.GetEnumerator())
-            {
-                if (!sourceIterator.MoveNext())
-                {
-                    throw new InvalidOperationException("Sequence contains no elements");
+            using (var sourceIterator = source.GetEnumerator()) {
+                if (!sourceIterator.MoveNext()) {
+                    if (fallbackIfEmpty) {
+                        return fallback;
+                    }
+                    else {
+                        throw new InvalidOperationException("Sequence contains no elements");
+                    }
                 }
                 var min = sourceIterator.Current;
                 var minKey = selector(min);
-                while (sourceIterator.MoveNext())
-                {
+                while (sourceIterator.MoveNext()) {
                     var candidate = sourceIterator.Current;
                     var candidateProjected = selector(candidate);
-                    if (comparer.Compare(candidateProjected, minKey) < 0)
-                    {
+                    if (comparer.Compare(candidateProjected, minKey) < 0) {
                         min = candidate;
                         minKey = candidateProjected;
+                    }
+                }
+                return min;
+            }
+        }
+
+        private static TSource MinOrFallbackImpl<TSource>(IEnumerable<TSource> source,
+            IComparer<TSource> comparer,
+            bool fallbackIfEmpty,
+            TSource fallback) {
+
+            comparer = comparer ?? Comparer<TSource>.Default;
+
+            using (var sourceIterator = source.GetEnumerator()) {
+                if (!sourceIterator.MoveNext()) {
+                    if (fallbackIfEmpty) {
+                        return fallback;
+                    }
+                    else {
+                        throw new InvalidOperationException("Sequence contains no elements");
+                    }
+                }
+                var min = sourceIterator.Current;
+                while (sourceIterator.MoveNext()) {
+                    var candidate = sourceIterator.Current;
+                    if (comparer.Compare(candidate, min) < 0) {
+                        min = candidate;
                     }
                 }
                 return min;
