ee493b574e8cb266f233a6f5b55d0de90ec8ded2
JamesFaix <jamesthefaix@gmail.com>
2016-04-28 22:05:15 -0400

    IntersectAll
    Added IntersectAll, IntersectAllBy, and IntersectAllKeys method groups,
    which share the same implementation.  Added unit tests for
    IntersectAllBy and IntersectAllKeys.

---
+++ MoreLinq.Test/IntersectAllByTest.cs
@@ -0,0 +1,115 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008 Jonathan Skeet. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MoreLinq.Test {
+    [TestFixture]
+    public class IntersectAllByTest {
+        [Test]
+        public void SimpleIntersectAllBy() {
+            string[] first = { "aaa", "bb", "c", "dddd" };
+            string[] second = { "bb", "c" };
+            var result = first.IntersectAllBy(second, x => x.Length);
+            result.AssertSequenceEqual("bb", "c");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllByNullFirstSequence() {
+            string[] first = null;
+            string[] second = { "aaa" };
+            first.IntersectAllBy(second, x => x.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllByNullSecondSequence() {
+            string[] first = { "aaa" };
+            string[] second = null;
+            first.IntersectAllBy(second, x => x.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllByNullKeySelector() {
+            string[] first = { "aaa" };
+            string[] second = { "aaa" };
+            first.IntersectAllBy<string, string>(second, (Func<string, string>)null);
+        }
+
+        [Test]
+        public void IntersectAllByIsLazy() {
+            new BreakingSequence<string>().IntersectAllBy(new string[0], x => x.Length);
+        }
+
+        [Test]
+        public void IntersectAllByRepeatsSourceElementsWithDuplicateKeys() {
+            string[] first = { "aaa", "bb", "c", "a", "b", "c", "dddd" };
+            string[] second = { "c" };
+            var result = first.IntersectAllBy(second, x => x.Length);
+            result.AssertSequenceEqual("c", "a", "b", "c");
+        }
+
+        [Test]
+        public void IntersectAllByWithComparer() {
+            string[] first = { "first", "second", "third", "fourth" };
+            string[] second = { "FIRST", "thiRD", "FIFTH" };
+            var result = first.IntersectAllBy(second, word => word, StringComparer.OrdinalIgnoreCase);
+            result.AssertSequenceEqual("first", "third");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllByNullFirstSequenceWithComparer() {
+            string[] first = null;
+            string[] second = { "aaa" };
+            first.IntersectAllBy(second, x => x.Length, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllByNullSecondSequenceWithComparer() {
+            string[] first = { "aaa" };
+            string[] second = null;
+            first.IntersectAllBy(second, x => x.Length, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllByNullKeySelectorWithComparer() {
+            string[] first = { "aaa" };
+            string[] second = { "aaa" };
+            first.IntersectAllBy(second, null, EqualityComparer<string>.Default);
+        }
+
+        [Test]
+        public void IntersectAllByNullComparer() {
+            string[] first = { "aaa", "bb", "bb", "c", "dddd" };
+            string[] second = { "xx", "y" };
+            var result = first.IntersectAllBy(second, x => x.Length, null);
+            result.AssertSequenceEqual("bb","bb", "c");
+        }
+
+        [Test]
+        public void IntersectAllByIsLazyWithComparer() {
+            new BreakingSequence<string>().IntersectAllBy(new string[0], x => x, StringComparer.Ordinal);
+        }
+    }
+}
---
+++ MoreLinq.Test/IntersectAllKeysTest.cs
@@ -0,0 +1,115 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008 Jonathan Skeet. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MoreLinq.Test {
+    [TestFixture]
+    public class IntersectAllKeysTest {
+        [Test]
+        public void SimpleIntersectAllKeys() {
+            string[] first = { "aaa", "bb", "c", "dddd" };
+            int[] second = { 1, 2 };
+            var result = first.IntersectAllKeys(second, x => x.Length);
+            result.AssertSequenceEqual("bb", "c");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllKeysNullFirstSequence() {
+            string[] first = null;
+            int[] second = { 1 };
+            first.IntersectAllKeys(second, x => x.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllKeysNullSecondSequence() {
+            string[] first = { "aaa" };
+            int[] second = null;
+            first.IntersectAllKeys(second, x => x.Length);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllKeysNullKeySelector() {
+            string[] first = { "aaa" };
+            int[] second = { 3 };
+            first.IntersectAllKeys<string, int>(second, (Func<string, int>)null);
+        }
+
+        [Test]
+        public void IntersectAllKeysIsLazy() {
+            new BreakingSequence<string>().IntersectAllKeys(new int[0], x => x.Length);
+        }
+
+        [Test]
+        public void IntersectAllKeysRepeatsSourceElementsWithDuplicateKeys() {
+            string[] first = { "aaa", "bb", "c", "a", "b", "c", "dddd" };
+            int[] second = { 1 };
+            var result = first.IntersectAllKeys(second, x => x.Length);
+            result.AssertSequenceEqual("c", "a", "b", "c");
+        }
+
+        [Test]
+        public void IntersectAllKeysWithComparer() {
+            string[] first = { "first", "second", "third", "fourth" };
+            string[] second = { "FIRST", "thiRD", "FIFTH" };
+            var result = first.IntersectAllKeys(second, word => word, StringComparer.OrdinalIgnoreCase);
+            result.AssertSequenceEqual("first", "third");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllKeysNullFirstSequenceWithComparer() {
+            string[] first = null;
+            int[] second = { 1 };
+            first.IntersectAllKeys(second, x => x.Length, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllKeysNullSecondSequenceWithComparer() {
+            string[] first = { "aaa" };
+            int[] second = null;
+            first.IntersectAllKeys(second, x => x.Length, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IntersectAllKeysNullKeySelectorWithComparer() {
+            string[] first = { "aaa" };
+            int[] second = { 1 };
+            first.IntersectAllKeys(second, null, EqualityComparer<int>.Default);
+        }
+
+        [Test]
+        public void IntersectAllKeysNullComparer() {
+            string[] first = { "aaa", "bb", "bb", "c", "dddd" };
+            int[] second = { 1, 2 };
+            var result = first.IntersectAllKeys(second, x => x.Length, null);
+            result.AssertSequenceEqual("bb", "bb", "c");
+        }
+
+        [Test]
+        public void IntersectAllKeysIsLazyWithComparer() {
+            new BreakingSequence<string>().IntersectAllKeys(new string[0], x => x, StringComparer.Ordinal);
+        }
+    }
+}
