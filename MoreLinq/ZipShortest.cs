c2c41b2b2aec7bbbfca9abd86f950f5fdb467b30
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2016-07-05 10:13:29 +0700

    Add missing documentation for Repeat and ZipShortest method. #148 #149

--- MoreLinq/ZipShortest.cs
+++ MoreLinq/ZipShortest.cs
@@ -49,6 +49,7 @@ namespace MoreLinq
         /// <param name="second">Second sequence</param>
         /// <param name="third">Third sequence</param>
         /// <param name="resultSelector">Function to apply to each triplet of elements</param>
+        /// <returns>A projection of tuples, where each tuple contains the N-th element from each of the argument sequences.</returns>
         
         public static IEnumerable<TResult> ZipShortest<T1, T2, T3, TResult>(this IEnumerable<T1> first,
             IEnumerable<T2> second, IEnumerable<T3> third, Func<T1, T2, T3, TResult> resultSelector)
@@ -91,6 +92,7 @@ namespace MoreLinq
         /// <param name="third">Third sequence</param>
         /// <param name="fourth">Fourth sequence</param>
         /// <param name="resultSelector">Function to apply to each quadruplet of elements</param>
+        /// <returns>A projection of tuples, where each tuple contains the N-th element from each of the argument sequences.</returns>
         
         public static IEnumerable<TResult> ZipShortest<T1, T2, T3, T4, TResult>(this IEnumerable<T1> first,
             IEnumerable<T2> second, IEnumerable<T3> third, IEnumerable<T4> fourth, Func<T1, T2, T3, T4, TResult> resultSelector)
@@ -127,6 +129,7 @@ namespace MoreLinq
         /// <param name="first">First sequence</param>
         /// <param name="second">Second sequence</param>
         /// <param name="resultSelector">Function to apply to each pair of elements</param>
+        /// <returns>A projection of tuples, where each tuple contains the N-th element from each of the argument sequences</returns>
         
         public static IEnumerable<TResult> ZipShortest<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first,
             IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
