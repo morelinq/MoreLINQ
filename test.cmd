2eebf57c435e1cf8bec1025e81983364fece5f40
Wang Tao <miffyliye@gmail.com>
2016-09-20 23:14:36 +0800

    tweak test script

--- test.cmd
+++ test.cmd
@@ -2,20 +2,20 @@
 setlocal
 cd "%~dp0"
 chcp 1252 > nul
-if not exist tools\NUnit.Runners (
-    for %%i in (NuGet.exe) do set nuget=%%~dpnx$PATH:i
-    if "%nuget%"=="" goto :nonuget
-    md tools
-    nuget.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
-    move .\tools\NUnit.Runners.2.6.4 .\tools\NUnit.Runners
-)
+if exist tools\NUnit.Runners goto :test
+for %%i in (NuGet.exe) do set nuget=%%~dpnx$PATH:i
+if "%nuget%"=="" goto :nonuget
+md tools
+nuget.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory tools || exit /b 1
+move .\tools\NUnit.Runners.2.6.4 .\tools\NUnit.Runners
 if not %errorlevel%==0 exit /b %errorlevel%
 
-call build /v:m
-.\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Debug\net40\MoreLinq.Test.dll
-.\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Release\net40\MoreLinq.Test.dll
-.\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Debug\net40-client\MoreLinq.Test.dll
-.\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Release\net40-client\MoreLinq.Test.dll
+:test
+call build /v:m ^
+  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Debug\net40\MoreLinq.Test.dll ^
+  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Release\net40\MoreLinq.Test.dll ^
+  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Debug\net40-client\MoreLinq.Test.dll ^
+  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Release\net40-client\MoreLinq.Test.dll
 goto :EOF
 
 :nonuget
