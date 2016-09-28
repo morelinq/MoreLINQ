a8639824a55a8640066014c704817727f671c3c9
Atif Aziz <code@raboof.com>
2016-09-28 09:40:55 +0200

    MS Build Tools 2015 URL fix

--- build.cmd
+++ build.cmd
@@ -8,7 +8,7 @@ if not exist "%MSBUILD%" (
     echo Microsoft Build Tools 2015 does not appear to be installed on this
     echo machine, which is required to build the solution. You can install
     echo it from the URL below and then try building again:
-    echo https://www.microsoft.com/en-us/download/detailscd .aspx?id=48159
+    echo https://www.microsoft.com/en-us/download/details.aspx?id=48159
     exit /b 1
 )
 if "%1"=="docs" call :docs %2 %3 %4 %5 %6 %7 %8 %9 & goto :EOF
