c6ba542aa01fcec159c084117305fb8715bc558f
Atif Aziz <code@raboof.com>
2016-09-28 09:25:14 +0200

    Test script review for redundancies

--- test.cmd
+++ test.cmd
@@ -1,21 +1,29 @@
 @echo off
+pushd "%~dp0"
+call :main %*
+popd
+goto :EOF
+
+:main
 setlocal
-cd "%~dp0"
-chcp 1252 > nul
-if exist tools\NUnit.Runners goto :test
+set NUNIT_CONSOLE_PATH=tools\NUnit.Runners\tools\nunit-console.exe
+if exist %NUNIT_CONSOLE_PATH% goto :test-all
 for %%i in (NuGet.exe) do set nuget=%%~dpnx$PATH:i
 if "%nuget%"=="" goto :nonuget
-md tools
 nuget.exe install NUnit.Runners -Version 2.6.4 -OutputDirectory tools || exit /b 1
-move .\tools\NUnit.Runners.2.6.4 .\tools\NUnit.Runners
+move tools\NUnit.Runners.2.6.4 tools\NUnit.Runners
 if not %errorlevel%==0 exit /b %errorlevel%
+:test-all
+call build /v:m ^
+  && call :test net40 ^
+  && call :test net40 ^
+  && call :test net40-client ^
+  && call :test net40-client
+goto :EOF
 
 :test
-call build /v:m ^
-  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Debug\net40\MoreLinq.Test.dll ^
-  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Release\net40\MoreLinq.Test.dll ^
-  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Debug\net40-client\MoreLinq.Test.dll ^
-  && .\tools\NUnit.Runners\tools\nunit-console.exe .\MoreLinq.Test\bin\Release\net40-client\MoreLinq.Test.dll
+setlocal
+%NUNIT_CONSOLE_PATH% MoreLinq.Test\bin\Release\%1\MoreLinq.Test.dll
 goto :EOF
 
 :nonuget
