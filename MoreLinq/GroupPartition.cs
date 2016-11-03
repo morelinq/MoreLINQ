4c94fb90eb40ac47719e25a38ed68c8a1c15c0b7
Atif Aziz <code@raboof.com>
2016-11-03 23:09:06 +0100

    Apparently it's 2016 :)

--- MoreLinq/GroupPartition.cs
+++ MoreLinq/GroupPartition.cs
@@ -1,6 +1,6 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
-// Copyright (c) 2009 Atif Aziz. All rights reserved.
+// Copyright (c) 2016 Atif Aziz. All rights reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
