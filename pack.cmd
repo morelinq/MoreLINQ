3a7c0761277738b8be361bba968309331b5ad08a
Atif Aziz <code@raboof.com>
2016-12-23 16:52:03 +0100

    Restore packages in build script

--- pack.cmd
+++ pack.cmd
@@ -2,8 +2,6 @@
 setlocal
 cd "%~dp0"
 chcp 1252 > nul
-for %%i in (dotnet.exe) do set dotnet=%%~dpnx$PATH:i
-if "%dotnet%"=="" goto :nodotnet
 for %%i in (NuGet.exe) do set nuget=%%~dpnx$PATH:i
 if "%nuget%"=="" goto :nonuget
 if not exist dist md dist
@@ -17,7 +15,3 @@ echo>&2 NuGet executable not found in PATH
 echo>&2 To download a version, see https://dist.nuget.org/
 exit /b 2
 
-:nodotnet
-echo>&2 dotnet executable not found in PATH
-echo>&2 For more on dotnet, see https://www.microsoft.com/net/core
-exit /b 2
