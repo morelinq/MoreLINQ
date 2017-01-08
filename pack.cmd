155fbcfcb809d50cf770e9206775a039ca02c35a
Atif Aziz <code@raboof.com>
2017-01-08 12:03:19 +0100

    NuGet is no longer needed for packaging

--- pack.cmd
+++ pack.cmd
@@ -6,16 +6,8 @@ goto :EOF
 
 :main
 setlocal
-for %%i in (NuGet.exe) do set nuget=%%~dpnx$PATH:i
-if "%nuget%"=="" goto :nonuget
 if not exist dist md dist
 if not %errorlevel%==0 exit /b %errorlevel%
 call build /v:m ^
     && dotnet pack -c Release MoreLinq\project.json -o dist
 goto :EOF
-
-:nonuget
-echo>&2 NuGet executable not found in PATH
-echo>&2 To download a version, see https://dist.nuget.org/
-exit /b 2
-
