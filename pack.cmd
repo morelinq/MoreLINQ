b83f6045a638cdfca82f3d51782a0d348e045ee1
Atif Aziz <code@raboof.com>
2017-01-08 12:10:07 +0100

    Optional version suffix for pack script
    Helps with continuous builds; adding to #230.

--- pack.cmd
+++ pack.cmd
@@ -8,6 +8,8 @@ goto :EOF
 setlocal
 if not exist dist md dist
 if not %errorlevel%==0 exit /b %errorlevel%
+set VERSION_SUFFIX=
+if not "%~1"=="" set VERSION_SUFFIX=--version-suffix %1
 call build /v:m ^
-    && dotnet pack -c Release MoreLinq -o dist
+    && dotnet pack -c Release %VERSION_SUFFIX% MoreLinq -o dist
 goto :EOF
