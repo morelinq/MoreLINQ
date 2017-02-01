33ea1a607860a4f0a23b5ce79eef587a763af4e3
Felipe Sateler <fsateler@gmail.com>
2017-02-01 10:39:22 -0300

    travis: drop repeated netcoreapp1.0 test

--- travis-test.sh
+++ travis-test.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-dotnet test -c Debug -f netcoreapp1.0 MoreLinq.Test
 RID=`dotnet --info | grep RID | sed -E 's/ *RID: *(.+)/\1/'`
 mono MoreLinq.Test/bin/Debug/net451/$RID/dotnet-test-nunit.exe \
 	MoreLinq.Test/bin/Debug/net451/$RID/MoreLinq.Test.dll
