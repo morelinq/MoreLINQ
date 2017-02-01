04f48bc8ab0c7adbdf4ba0d0516c5fc22383be2f
Felipe Sateler <fsateler@gmail.com>
2017-02-01 10:53:07 -0300

    travis: build and test debug and release builds

--- travis-mono-test.sh
+++ travis-mono-test.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-for dir in MoreLinq.Test/bin/Debug/net451/*/ ; do
+for dir in MoreLinq.Test/bin/$CONFIGURATION/net451/*/ ; do
 	mono $dir/dotnet-test-nunit.exe $dir/MoreLinq.Test.dll
 done
 
