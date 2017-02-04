1b618a386ac0bd4cbbb861e80b0cd02bbd7c75a1
Felipe Sateler <fsateler@gmail.com>
2017-02-04 11:10:02 -0300

    Avoid double slash in printed directory names

--- travis-mono-test.sh
+++ travis-mono-test.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 for dir in MoreLinq.Test/bin/$CONFIGURATION/net451/*/ ; do
-	mono $dir/dotnet-test-nunit.exe $dir/MoreLinq.Test.dll
+	mono ${dir}dotnet-test-nunit.exe ${dir}MoreLinq.Test.dll
 done
 
