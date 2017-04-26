d4be2b0bbaf3f44c4e0f969d2c837c7dbaf24dc1
Atif Aziz <code@raboof.com>
2017-04-26 16:03:48 +0200

    Fold overloads where 4 < arity <= 16; closes #276

--- MoreLinq.Test/FoldTest.cs
+++ MoreLinq.Test/FoldTest.cs
@@ -104,10 +104,24 @@ namespace MoreLinq.Test
         [Test]
         public void Fold()
         {
-            Assert.AreEqual(1,  Enumerable.Range(1, 1).Fold(a => a), "fold 1");
-            Assert.AreEqual(2,  Enumerable.Range(1, 2).Fold((a, b) => a * b), "fold 2");
-            Assert.AreEqual(6,  Enumerable.Range(1, 3).Fold((a, b, c) => a * b * c), "fold 3");
-            Assert.AreEqual(24, Enumerable.Range(1, 4).Fold((a, b, c, d) => a * b * c * d), "fold 4");
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            Assert.AreEqual("a"               , alphabet.Take( 1).Fold(a                                                => string.Join(string.Empty, a                                             )), "fold 1");
+            Assert.AreEqual("ab"              , alphabet.Take( 2).Fold((a, b                                          ) => string.Join(string.Empty, a, b                                          )), "fold 2");
+            Assert.AreEqual("abc"             , alphabet.Take( 3).Fold((a, b, c                                       ) => string.Join(string.Empty, a, b, c                                       )), "fold 3");
+            Assert.AreEqual("abcd"            , alphabet.Take( 4).Fold((a, b, c, d                                    ) => string.Join(string.Empty, a, b, c, d                                    )), "fold 4");
+            Assert.AreEqual("abcde"           , alphabet.Take( 5).Fold((a, b, c, d, e                                 ) => string.Join(string.Empty, a, b, c, d, e                                 )), "fold 5");
+            Assert.AreEqual("abcdef"          , alphabet.Take( 6).Fold((a, b, c, d, e, f                              ) => string.Join(string.Empty, a, b, c, d, e, f                              )), "fold 6");
+            Assert.AreEqual("abcdefg"         , alphabet.Take( 7).Fold((a, b, c, d, e, f, g                           ) => string.Join(string.Empty, a, b, c, d, e, f, g                           )), "fold 7");
+            Assert.AreEqual("abcdefgh"        , alphabet.Take( 8).Fold((a, b, c, d, e, f, g, h                        ) => string.Join(string.Empty, a, b, c, d, e, f, g, h                        )), "fold 8");
+            Assert.AreEqual("abcdefghi"       , alphabet.Take( 9).Fold((a, b, c, d, e, f, g, h, i                     ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i                     )), "fold 9");
+            Assert.AreEqual("abcdefghij"      , alphabet.Take(10).Fold((a, b, c, d, e, f, g, h, i, j                  ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j                  )), "fold 10");
+            Assert.AreEqual("abcdefghijk"     , alphabet.Take(11).Fold((a, b, c, d, e, f, g, h, i, j, k               ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j, k               )), "fold 11");
+            Assert.AreEqual("abcdefghijkl"    , alphabet.Take(12).Fold((a, b, c, d, e, f, g, h, i, j, k, l            ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j, k, l            )), "fold 12");
+            Assert.AreEqual("abcdefghijklm"   , alphabet.Take(13).Fold((a, b, c, d, e, f, g, h, i, j, k, l, m         ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j, k, l, m         )), "fold 13");
+            Assert.AreEqual("abcdefghijklmn"  , alphabet.Take(14).Fold((a, b, c, d, e, f, g, h, i, j, k, l, m, n      ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j, k, l, m, n      )), "fold 14");
+            Assert.AreEqual("abcdefghijklmno" , alphabet.Take(15).Fold((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o   ) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o   )), "fold 15");
+            Assert.AreEqual("abcdefghijklmnop", alphabet.Take(16).Fold((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p) => string.Join(string.Empty, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p)), "fold 16");
         }
     }
 }
