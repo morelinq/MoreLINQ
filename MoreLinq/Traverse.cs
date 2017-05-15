0be79e94a8c8ca908076e5c242e1604a13ee1d0c
Atif Aziz <code@raboof.com>
2017-05-15 19:43:37 +0200

    TraverseDepthFirst

--- MoreLinq/Traverse.cs
+++ MoreLinq/Traverse.cs
@@ -71,21 +71,21 @@ namespace MoreLinq
         public static IEnumerable<T> TraverseDepthFirst<T>(T root, Func<T, IEnumerable<T>> childrenSelector)
         {
             if (childrenSelector == null) throw new ArgumentNullException(nameof(childrenSelector));
-            return TraverseDepthFirstImpl(root, childrenSelector);
-        }
 
-        private static IEnumerable<T> TraverseDepthFirstImpl<T>(T root, Func<T, IEnumerable<T>> childrenSelector)
-        {
-            var stack = new Stack<T>();
-            stack.Push(root);
+            return _(); IEnumerable<T> _()
+            {
+                var stack = new Stack<T>();
+                stack.Push(root);
 
-            while (stack.Count != 0) {
-                var current = stack.Pop();
-                yield return current;
-                // because a stack pops the elements out in LIFO order, we need to push them in reverse
-                // if we want to traverse the returned list in the same order as was returned to us
-                foreach (var child in childrenSelector(current).Reverse())
-                    stack.Push(child);
+                while (stack.Count != 0)
+                {
+                    var current = stack.Pop();
+                    yield return current;
+                    // because a stack pops the elements out in LIFO order, we need to push them in reverse
+                    // if we want to traverse the returned list in the same order as was returned to us
+                    foreach (var child in childrenSelector(current).Reverse())
+                        stack.Push(child);
+                }
             }
         }
     }
