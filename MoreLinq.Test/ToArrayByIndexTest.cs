e3c38d2d117af49a1c4b8baafc7a50078cd84fb1
Atif Aziz <code@raboof.com>
2017-05-17 09:32:37 +0200

    Normal test cases

--- MoreLinq.Test/ToArrayByIndexTest.cs
+++ MoreLinq.Test/ToArrayByIndexTest.cs
@@ -23,11 +23,43 @@ namespace MoreLinq.Test
     [TestFixture]
     public class ToArrayByIndexTest
     {
-        [Test]
-        public void ToArrayByIndexByIndexWithEmptySequence()
+        [TestCase(-1, new int[0])]
+        [TestCase(-1, new[] { 5 })]
+        [TestCase(-1, new[] { 1, 5 })]
+        [TestCase(-1, new[] { 0, 9 })]
+        [TestCase(-1, new[] { 0, 5, 9 })]
+        [TestCase(-1, new[] { 2, 3, 5, 9 })]
+        [TestCase(-1, new[] { 5, 2, 9, 3 })]
+        [TestCase(10, new int[0])]
+        [TestCase(10, new[] { 5 })]
+        [TestCase(10, new[] { 1, 5 })]
+        [TestCase(10, new[] { 0, 9 })]
+        [TestCase(10, new[] { 0, 5, 9 })]
+        [TestCase(10, new[] { 2, 3, 5, 9 })]
+        [TestCase(10, new[] { 5, 2, 9, 3 })]
+        public void ToArrayByIndex(int length, int[] indicies)
         {
-            var result = Enumerable.Empty<object>().ToArrayByIndex(BreakingFunc.Of<object, int>());
-            Assert.That(result.Length, Is.EqualTo(0));
+            var inputs = indicies.Select(i => new { Index = i }).ToArray();
+            var result = length < 0 ? inputs.ToArrayByIndex(e => e.Index)
+                       : inputs.ToArrayByIndex(length, e => e.Index);
+            var nils = result.ToList();
+
+            var lastIndex = length < 0
+                          ? inputs.Select(e => e.Index).DefaultIfEmpty(-1).Max()
+                          : length - 1;
+            var expectedLength = lastIndex + 1;
+            Assert.That(result.Count, Is.EqualTo(expectedLength));
+
+            foreach (var e in from e in inputs
+                              orderby e.Index descending
+                              select e)
+            {
+                Assert.That(result[e.Index], Is.SameAs(inputs.Single(inp => inp.Index == e.Index)));
+                nils.RemoveAt(e.Index);
+            }
+
+            Assert.That(nils.Count, Is.EqualTo(expectedLength - inputs.Length));
+            Assert.That(nils.All(e => e == null), Is.True);
         }
     }
 }
