8476f5c4ddbdc4eb5e1c8e5be179e760b66c55a7
leandromoh <leandromoh@hotmail.com>
2017-06-24 18:05:25 -0300

    improvimentes

---
+++ MoreLinq.Test/TransposeTest.cs
@@ -0,0 +1,175 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2017 Leandro F. Vieira (leandromoh). All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MoreLinq.Test
+{
+    [TestFixture]
+    public class TransposeTest
+    {
+        [Test]
+        public void TransposeWithEqualsLengthEnumerables()
+        {
+            var matrix = new[]
+            {
+                new [] { 10, 11, 12, 13 },
+                new [] { 20, 21, 22, 23 },
+                new [] { 30, 31, 32, 33 },
+            };
+
+            var expectations = new[]
+            {
+                new [] { 10, 20, 30 },
+                new [] { 11, 21, 31 },
+                new [] { 12, 22, 32 },
+                new [] { 13, 23, 33 },
+            };
+
+            var innerTestSequences = matrix.Select(x => x.AsTestingSequence()).ToList();
+
+            using (var test = innerTestSequences.AsTestingSequence())
+            {
+                Assert.That(matrix.Transpose(), Is.EqualTo(expectations));
+            }
+
+            innerTestSequences.ForEach(seq => (seq as IDisposable).Dispose());
+        }
+
+        [Test]
+        public void TransposeWithDifferentsLengthEnumerables()
+        {
+            var matrix = new[]
+            {
+                new int[] { 10, 11 },
+                new int[] { 20 },
+                new int[] { },
+                new int[] { 30, 31, 32 }
+            };
+
+            var innerTestSequences = matrix.Select(x => x.AsTestingSequence()).ToList();
+            
+            var expectations = new[]
+            {
+                new int[] { 10, 20, 30 },
+                new int[] { 11, 31 },
+                new int[] { 32 }
+            };
+
+            using (var test = innerTestSequences.AsTestingSequence())
+            {
+                Assert.That(matrix.Transpose(), Is.EqualTo(expectations));
+            }
+
+            innerTestSequences.ForEach(seq => (seq as IDisposable).Dispose());
+        }
+
+        [Test]
+        public void TransposeAssertPositions()
+        {
+            var matrix = new[]
+            {
+                new int[] { 10, 11 },
+                new int[] { 20 },
+                new int[] { },
+                new int[] { 30, 31, 32 }
+            };
+
+            var traspose = matrix.Transpose();
+
+            Assert.That(matrix.Last().Last(), Is.EqualTo(traspose.Last().Last()));
+            Assert.That(matrix.First().First(), Is.EqualTo(traspose.First().First()));
+        }
+
+        [Test]
+        public void TransposeWithAllEnumerablesInfinite()
+        {
+            const int takeRow = 10, takeLine = 20;
+
+            var matrix = MoreEnumerable.Generate(1, n => n + 1)
+                                       .Select(n => new[] { n }.Repeat());
+
+            var result = matrix.Transpose().Take(takeRow).Select(x => x.Take(takeLine));
+
+            var expectations = Enumerable.Repeat(Enumerable.Range(1, takeLine), takeRow);
+
+            Assert.That(result, Is.EqualTo(expectations));
+        }
+
+
+        // add test pra quando inner sequence é nulo null
+
+        [TestCase(2, 3, 5)]
+        [TestCase(5, 10, 15)]
+        [TestCase(4, 6, 3)]
+        public void TransposeWithSomeEnumerablesInfinite(int count, int takeRow, int takeLine)
+        {
+            var naturals = MoreEnumerable.Generate(1, n => n + 1);
+            var matrix = naturals.Select(x => x % 2 == 0 ? Enumerable.Repeat(x, count)
+                                                         : new [] { x }.Repeat());
+
+            var tranposedSequenceWithoutPadding = naturals.Where(x => x % 2 != 0).Take(takeLine);
+
+            var expectations = Enumerable.Repeat(Enumerable.Range(1, takeLine), count)
+                                         .Concat(new [] {tranposedSequenceWithoutPadding }.Repeat())
+                                         .Take(takeRow);
+
+            var result = matrix.Transpose().Take(takeRow).Select(x => x.Take(takeLine));
+
+            Assert.That(result, Is.EqualTo(expectations));
+        }
+
+
+        [Test]
+        public void TransposeOrderSequenceAreIteratedIsIrrelevant()
+        {
+            var matrix = new[]
+            {
+                new int[] { 10, 11 },
+                new int[] { 20 },
+                new int[] { },
+                new int[] { 30, 31, 32 }
+            };
+
+            var transpose = matrix.Transpose().Take(2).ToList();
+
+            Assert.That(transpose[1], Is.EqualTo(new int[] { 11, 31 }));
+            Assert.That(transpose[0], Is.EqualTo(new int[] { 10, 20, 30 }));
+        }
+
+        [Test]
+        public void TransposeSequencesAreLazies()
+        {
+            var matrix = new[]
+            {
+                new int[] { 10, 11, 12 },
+                new int[] { 30, 31, 32 }.Select<int, int>(x => throw new Exception())
+            };
+
+            var first = matrix.First();
+            var count = first.Count();
+
+            matrix.Transpose().Take(count).ForEach((seq, i) => 
+            {
+                Assert.That(seq.First(), Is.EqualTo(first.ElementAt(i)));
+            });
+        }
+    }
+}
---
+++ MoreLinq/Transpose.cs
@@ -0,0 +1,116 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2017 Leandro F. Vieira (leandromoh). All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace MoreLinq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    static partial class MoreEnumerable
+    {
+        /// <summary>
+        /// Return a sequence with the columns in the source swapped into rows.
+        /// </summary>
+        /// <typeparam name="T">Type of the source sequence</typeparam>
+        /// <param name="source">Source sequence</param>
+        /// <returns>
+        /// The transpose version of source.
+        /// </returns>
+        /// <remarks>
+        /// If some of the rows are shorter than the following rows, their elements are skipped.
+        /// This operator uses deferred execution and streams its results.
+        /// </remarks>
+        /// <example>
+        /// <code>
+        /// var result = [[1,2,3],[4,5,6]].Transpose();
+        /// </code>
+        /// The <c>result</c> variable will contain [[1,4],[2,5],[3,6]].
+        /// </example>
+        public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            
+            return _().TakeWhile(x => x.Any());
+
+            IEnumerable<IEnumerable<T>> _()
+            {
+                var list = new List<IEnumerator<T>>();
+                var values = new List<List<T>>();
+                var empty = new T[0];
+                var count = 0;
+
+                var buffer = new List<IEnumerable<T>>();
+                var cached = buffer.Concat(EnumerateOnce(source).Pipe(x => buffer.Add(x)));
+                
+                var enumerators = cached.Select((p, i) =>
+                {
+                    if (i < list.Count)
+                        return list[i];
+
+                    if (p == null)
+                        throw new ArgumentNullException($"The sequence at index {i} inside source is null.");
+
+                    list.Add(p.GetEnumerator());
+
+                    return list[i];
+                });
+
+                while (true)
+                {
+                    var countClosure = count;
+
+                    yield return enumerators.SelectMany((p, j) =>
+                    {
+                        if (j >= values.Count)
+                            values.Add(new List<T>());
+
+                        var row = values[j];
+
+                        while (countClosure >= row.Count && p != null && p.MoveNext())
+                            row.Add(p.Current);
+
+                        if (countClosure >= row.Count && p != null)
+                        {
+                            list[j].Dispose();
+                            list[j] = null;
+                        }
+
+                        return countClosure < row.Count ? new[] { row[countClosure] } : empty;
+                    });
+
+                    count++;
+                }
+            }
+
+            IEnumerable<U> EnumerateOnce<U>(IEnumerable<U> sequence)
+            {
+                IEnumerator<U> ector = null;
+
+                return f(); IEnumerable<U> f()
+                {
+                    ector = ector ?? sequence.GetEnumerator();
+
+                    while (ector.MoveNext())
+                        yield return ector.Current;
+
+                    ector.Dispose();
+                }
+            }
+        }
+    }
+}
