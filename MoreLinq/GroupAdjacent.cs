f7769d238a9aadace830dadebacfc6d6c17c3d50
Atif Aziz <code@raboof.com>
2017-09-15 10:31:44 +0200

    Convert lambda to local function

--- MoreLinq/GroupAdjacent.cs
+++ MoreLinq/GroupAdjacent.cs
@@ -207,9 +207,9 @@ namespace MoreLinq
             if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
 
             // This should be removed once the target framework is bumped to something that supports covariance
-            Func<TKey, IList<TSource>, TResult> resultSelectorWrapper = (key, group) => resultSelector(key, group);
+            TResult ResultSelectorWrapper(TKey key, IList<TSource> group) => resultSelector(key, group);
 
-            return GroupAdjacentImpl(source, keySelector, i => i, resultSelectorWrapper,
+            return GroupAdjacentImpl(source, keySelector, i => i, ResultSelectorWrapper,
                                      EqualityComparer<TKey>.Default);
         }
 
