88967853e005a9f20100948a70525bb33b3bc289
Atif Aziz <code@raboof.com>
2017-09-19 09:42:16 +0200

    Import System.Linq with lower priority
    .NET Core 2.0 also introduced TakeLast on System.Linq.Enumerable
    and with the System.Linq import being inside the MoreLinq.Test
    namespace declaration, the tests were exercising the Enumerable
    version of TakeLast instead of our version from MoreEnumerable.
    Moving the import outside the namespace to give it lower
    priority sovles the problem.
    
    See #351 for more.

--- MoreLinq.Test/TakeLastTest.cs
+++ MoreLinq.Test/TakeLastTest.cs
@@ -15,10 +15,13 @@
 // limitations under the License.
 #endregion
 
+using System.Linq; // Keep this import outside the MoreLinq namespace
+                   // to avoid conflicts. For more, see:
+                   // https://github.com/morelinq/MoreLINQ/issues/351
+
 namespace MoreLinq.Test
 {
     using NUnit.Framework;
-    using System.Linq;
     using System.Collections.Generic;
     using System;
 
