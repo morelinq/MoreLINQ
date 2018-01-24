27a4c32f7a65c0cbcf283f08436b7850fb07252b
Atif Aziz <code@raboof.com>
2018-01-24 08:52:52 +0100

    Fix AssertCount to be lazy for collections too
    Fixes #364

--- MoreLinq.Test/BreakingSequence.cs
+++ MoreLinq.Test/BreakingSequence.cs
@@ -25,7 +25,7 @@ namespace MoreLinq.Test
     /// Enumerable sequence which throws InvalidOperationException as soon as its
     /// enumerator is requested. Used to check lazy evaluation.
     /// </summary>
-    sealed class BreakingSequence<T> : IEnumerable<T>
+    class BreakingSequence<T> : IEnumerable<T>
     {
         public IEnumerator<T> GetEnumerator() => throw new InvalidOperationException();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
