b86a4507f14acf1144e618dd0f236ec2f8ce5cb5
Atif Aziz <code@raboof.com>
2018-01-25 07:55:58 +0100

    No need to partition Fold arity 5-16 wrappers
    These were needed to exclude from the .NET Framework 3.5 target but
    that target was dropped earlier.

--- MoreLinq/Fold.g.cs
+++ MoreLinq/Fold.g.cs
@@ -110,5 +110,269 @@ namespace MoreLinq
             return FoldImpl(source, 4, folder4: folder);
         }
 
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 5 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 5 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 5, folder5: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 6 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 6 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 6, folder6: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 7 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 7 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 7, folder7: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 8 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 8 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 8, folder8: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 9 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 9 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 9, folder9: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 10 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 10 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 10, folder10: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 11 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 11 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 11, folder11: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 12 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 12 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 12, folder12: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 13 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 13 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 13, folder13: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 14 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 14 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 14, folder14: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 15 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 15 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 15, folder15: folder);
+        }
+
+        /// <summary>
+        /// Returns the result of applying a function to a sequence of
+        /// 16 elements.
+        /// </summary>
+        /// <remarks>
+        /// This operator uses immediate execution and effectively buffers
+        /// as many items of the source sequence as necessary.
+        /// </remarks>
+        /// <typeparam name="T">Type of element in the source sequence</typeparam>
+        /// <typeparam name="TResult">Type of the result</typeparam>
+        /// <param name="source">The sequence of items to fold.</param>
+        /// <param name="folder">Function to apply to the elements in the sequence.</param>
+        /// <returns>The folded value returned by <paramref name="folder"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="folder"/> is null</exception>
+        /// <exception cref="InvalidOperationException"><paramref name="source"/> does not contain exactly 16 elements</exception>
+
+        public static TResult Fold<T, TResult>(this IEnumerable<T> source, Func<T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, TResult> folder)
+        {
+            return FoldImpl(source, 16, folder16: folder);
+        }
+
     }
 }
