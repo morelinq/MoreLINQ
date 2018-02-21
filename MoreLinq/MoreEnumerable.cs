e5c16d0f9c4cc62296a73782d249dad639d20858
Atif Aziz <code@raboof.com>
2018-02-21 17:51:52 +0100

    Private modifier is redundant

--- MoreLinq/MoreEnumerable.cs
+++ MoreLinq/MoreEnumerable.cs
@@ -34,7 +34,7 @@ namespace MoreLinq
         /// <typeparam name="T">The type of objects to enumerate.</typeparam>
         /// <param name="source">The enumerable to get the count for.</param>
         /// <returns>The count if it is available without enumeration, or null if it is not.</returns>
-        private static int? TryGetCollectionCount<T>(this IEnumerable<T> source)
+        static int? TryGetCollectionCount<T>(this IEnumerable<T> source)
         {
             return source is ICollection<T> collection ? collection.Count
                 : source is IReadOnlyCollection<T> readOnlyCollection ? readOnlyCollection.Count
