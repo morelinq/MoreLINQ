cdb4174e24a7fe0bf7cb921510eb3c20935b5ac9
Atif Aziz <code@raboof.com>
2018-02-21 17:51:34 +0100

    Move TryGetCollectionCount to MoreEnumerable.cs

--- MoreLinq/MoreEnumerable.cs
+++ MoreLinq/MoreEnumerable.cs
@@ -27,5 +27,18 @@ namespace MoreLinq
     
     public static partial class MoreEnumerable
     {
+        /// <summary>
+        /// Gets the count of an enumerable if it is precalculated in <see cref="ICollection{T}"/>
+        /// or <see cref="IReadOnlyCollection{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+        /// <param name="source">The enumerable to get the count for.</param>
+        /// <returns>The count if it is available without enumeration, or null if it is not.</returns>
+        private static int? TryGetCollectionCount<T>(this IEnumerable<T> source)
+        {
+            return source is ICollection<T> collection ? collection.Count
+                : source is IReadOnlyCollection<T> readOnlyCollection ? readOnlyCollection.Count
+                : (int?)null;
+        }
     }
 }
