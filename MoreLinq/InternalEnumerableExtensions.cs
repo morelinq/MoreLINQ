2eb86ed5c55a98961fc3d59a7079b62053b2ff35
Avi Levin <avi@milyli.com>
2018-02-21 10:24:03 -0600

    refactor: move TryGetCollectionCount into MoreEnumerable

--- MoreLinq/InternalEnumerableExtensions.cs
+++ MoreLinq/InternalEnumerableExtensions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MoreLinq
 {
-    static class InternalEnumerableExtensions
+    partial class MoreEnumerable
     {
         /// <summary>
         /// Gets the count of an enumerable if it is precalculated in <see cref="ICollection{T}"/>
@@ -15,7 +15,7 @@ namespace MoreLinq
         /// <typeparam name="T">The type of objects to enumerate.</typeparam>
         /// <param name="source">The enumerable to get the count for.</param>
         /// <returns>The count if it is available without enumeration, or null if it is not.</returns>
-        public static int? TryGetCollectionCount<T>(this IEnumerable<T> source)
+        private static int? TryGetCollectionCount<T>(this IEnumerable<T> source)
         {
             return source is ICollection<T> collection ? collection.Count
                 : source is IReadOnlyCollection<T> readOnlyCollection ? readOnlyCollection.Count
