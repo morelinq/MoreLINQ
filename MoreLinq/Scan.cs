ee3448f2466406cbf24d2e6a101113f29063acbb
Atif Aziz <code@raboof.com>
2018-02-22 18:24:12 +0100

    Revert to minimal changes
    This removes whitespace changes 1c9378639e94b1df482817041c5f40d332dee906
    to aid with review.

--- MoreLinq/Scan.cs
+++ MoreLinq/Scan.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2009 Konrad Rudolph. All rights reserved.
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -50,7 +50,7 @@ namespace MoreLinq
         /// <param name="source">Source sequence</param>
         /// <param name="transformation">Transformation operation</param>
         /// <returns>The scanned sequence</returns>
-
+        
         public static IEnumerable<TSource> Scan<TSource>(this IEnumerable<TSource> source,
             Func<TSource, TSource, TSource> transformation)
         {
@@ -76,8 +76,8 @@ namespace MoreLinq
         }
 
         /// <summary>
-        /// Like <see cref="Enumerable.Aggregate{TSource}"/> except returns
-        /// the sequence of intermediate results as well as the final one.
+        /// Like <see cref="Enumerable.Aggregate{TSource}"/> except returns 
+        /// the sequence of intermediate results as well as the final one. 
         /// An additional parameter specifies a seed.
         /// </summary>
         /// <remarks>
@@ -95,7 +95,7 @@ namespace MoreLinq
         /// <param name="seed">Initial state to seed</param>
         /// <param name="transformation">Transformation operation</param>
         /// <returns>The scanned sequence</returns>
-
+        
         public static IEnumerable<TState> Scan<TSource, TState>(this IEnumerable<TSource> source,
             TState seed, Func<TState, TSource, TState> transformation)
         {
