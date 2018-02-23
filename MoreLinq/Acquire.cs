be90776f8a66a6045ed74a78fb679a1964d595f9
Atif Aziz <code@raboof.com>
2018-02-23 09:49:14 +0100

    Revert "Let's break the bank :clown_face:"
    This reverts commit c2d8cf9a3487ae3181150ed7bfffd0b055d8e921 as it
    was a test to check if the linting from
    b50a0ab0e8e88e0a21cf917c9dbcb8da16a5b6e7 was indeed working.

--- MoreLinq/Acquire.cs
+++ MoreLinq/Acquire.cs
@@ -1,13 +1,13 @@
 #region License and Terms
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2012 Atif Aziz. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@ namespace MoreLinq
     static partial class MoreEnumerable
     {
         /// <summary>
-        /// Ensures that a source sequence of <see cref="IDisposable"/> 
-        /// objects are all acquired successfully. If the acquisition of any 
-        /// one <see cref="IDisposable"/> fails then those successfully 
+        /// Ensures that a source sequence of <see cref="IDisposable"/>
+        /// objects are all acquired successfully. If the acquisition of any
+        /// one <see cref="IDisposable"/> fails then those successfully
         /// acquired till that point are disposed.
         /// </summary>
         /// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
@@ -37,7 +37,7 @@ namespace MoreLinq
         /// <remarks>
         /// This operator executes immediately.
         /// </remarks>
-        
+
         public static TSource[] Acquire<TSource>(this IEnumerable<TSource> source)
             where TSource : IDisposable
         {
