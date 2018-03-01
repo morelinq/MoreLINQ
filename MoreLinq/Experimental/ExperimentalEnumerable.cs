79d7233e3dc1f1e1fba585aec817b50af41040bc
Atif Aziz <code@raboof.com>
2018-03-01 19:10:29 +0100

    More prominent experimental warning

--- MoreLinq/Experimental/ExperimentalEnumerable.cs
+++ MoreLinq/Experimental/ExperimentalEnumerable.cs
@@ -20,12 +20,15 @@ namespace MoreLinq.Experimental
     using System.Collections.Generic;
 
     /// <summary>
+    /// <para>
     /// Provides a set of static methods for querying objects that
-    /// implement <see cref="IEnumerable{T}" />. THE METHODS ARE EXPERIMENTAL.
-    /// THEY MAY BE UNSTABLE AND UNTESTED. THEY MAY BE REMOVED FROM A FUTURE
-    /// MAJOR OR MINOR RELEASE AND POSSIBLY WITHOUT NOTICE. USE THEM AT YOUR
-    /// OWN RISK. THE METHODS ARE PUBLISHED FOR FIELD EXPERIMENTATION TO
-    /// SOLICIT FEEDBACK ON THEIR UTILITY AND DESIGN/IMPLEMENTATION DEFECTS.
+    /// implement <see cref="IEnumerable{T}" />.</para>
+    /// <para>
+    /// <strong>THE METHODS ARE EXPERIMENTAL. THEY MAY BE UNSTABLE AND
+    /// UNTESTED. THEY MAY BE REMOVED FROM A FUTURE MAJOR OR MINOR RELEASE AND
+    /// POSSIBLY WITHOUT NOTICE. USE THEM AT YOUR OWN RISK. THE METHODS ARE
+    /// PUBLISHED FOR FIELD EXPERIMENTATION TO SOLICIT FEEDBACK ON THEIR
+    /// UTILITY AND DESIGN/IMPLEMENTATION DEFECTS.</strong></para>
     /// </summary>
 
     public static partial class ExperimentalEnumerable
