49db8170fd4e50c8446827a62d1610350e050413
Atif Aziz <code@raboof.com>
2018-04-03 12:26:52 +0200

    Sort imports

--- MoreLinq.Test/TransposeTest.cs
+++ MoreLinq.Test/TransposeTest.cs
@@ -17,9 +17,9 @@
 
 namespace MoreLinq.Test
 {
-    using NUnit.Framework;
-    using System.Collections.Generic;
     using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
 
     [TestFixture]
     public class TransposeTest
