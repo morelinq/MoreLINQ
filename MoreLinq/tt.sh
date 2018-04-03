05dfa61d9ddfbd6c9fde57b2db06e0c62a478100
Atif Aziz <code@raboof.com>
2018-04-03 22:59:16 +0200

    Use the canonical -L for xargs

--- MoreLinq/tt.sh
+++ MoreLinq/tt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 [[ -e tt.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
-find . -name "*.tt" | xargs -t -n 1 dotnet tt
+find . -name "*.tt" | xargs -t -L 1 dotnet tt
