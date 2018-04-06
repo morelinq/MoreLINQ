1422bede52dafabf1d9e6d65ffd53c252c02cfb3
Atif Aziz <code@raboof.com>
2018-04-06 09:26:47 +0200

    Localize helper to the one test case where used

--- MoreLinq.Test/MemoizeTest.cs
+++ MoreLinq.Test/MemoizeTest.cs
@@ -37,56 +37,56 @@ namespace MoreLinq.Test
             var flowBuffer = InnerForEach(buffer);
 
             flowArray.AssertSequenceEqual(flowBuffer);
-        }
 
-        public List<string> InnerForEach(IEnumerable<int> source)
-        {
-            var flow = new List<string>();
-
-            var firstVisitAtInnerLoopDone = false;
-
-            //add 1-3 to cache (so enter inner loop)
-            //consume 4-5 already cached
-            //add 6-7 to cache (so enter inner loop)
-            //consume 8-10 already cached
-
-            flow.Add("enter outer loop");
-            foreach (var i in source)
+            List<string> InnerForEach(IEnumerable<int> source)
             {
-                flow.Add(i.ToString());
+                var flow = new List<string>();
 
-                if (i == 3 || i == 7)
+                var firstVisitAtInnerLoopDone = false;
+
+                //add 1-3 to cache (so enter inner loop)
+                //consume 4-5 already cached
+                //add 6-7 to cache (so enter inner loop)
+                //consume 8-10 already cached
+
+                flow.Add("enter outer loop");
+                foreach (var i in source)
                 {
-                    //consume 1-3 already cached
-                    //add 4-5 to cache (so go to outer loop)
-                    //consume 1-7 already cached
-                    //add 8-10 to cache (so go to outer loop)
+                    flow.Add(i.ToString());
 
-                    flow.Add("enter inner loop");
-                    foreach (var j in source)
+                    if (i == 3 || i == 7)
                     {
-                        flow.Add(j.ToString());
+                        //consume 1-3 already cached
+                        //add 4-5 to cache (so go to outer loop)
+                        //consume 1-7 already cached
+                        //add 8-10 to cache (so go to outer loop)
 
-                        if (!firstVisitAtInnerLoopDone && j == 5)
+                        flow.Add("enter inner loop");
+                        foreach (var j in source)
                         {
-                            firstVisitAtInnerLoopDone = true;
-                            break;
+                            flow.Add(j.ToString());
+
+                            if (!firstVisitAtInnerLoopDone && j == 5)
+                            {
+                                firstVisitAtInnerLoopDone = true;
+                                break;
+                            }
                         }
+                        flow.Add("exit inner loop");
                     }
-                    flow.Add("exit inner loop");
                 }
-            }
-            flow.Add("exit outer loop");
+                flow.Add("exit outer loop");
 
-            flow.Add("enter last loop");
-            //consume 1-10 (all item were already cached)
-            foreach (var k in source)
-            {
-                flow.Add(k.ToString());
-            }
-            flow.Add("exit last loop");
+                flow.Add("enter last loop");
+                //consume 1-10 (all item were already cached)
+                foreach (var k in source)
+                {
+                    flow.Add(k.ToString());
+                }
+                flow.Add("exit last loop");
 
-            return flow;
+                return flow;
+            }
         }
 
         [Test]
