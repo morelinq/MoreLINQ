931c652ef55f18be43d930ce89e481d20b7f46f7
leandromoh <leandromoh@hotmail.com>
2018-04-09 08:21:30 -0300

    fix max validation message

--- MoreLinq/MoreEnumerable.cs
+++ MoreLinq/MoreEnumerable.cs
@@ -39,7 +39,7 @@ namespace MoreLinq
         static int CountUpTo<T>(this IEnumerable<T> source, int max)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
-            if (max < 0) throw new ArgumentOutOfRangeException(nameof(max), "Max cannot be negative.");
+            if (max < 0) throw new ArgumentOutOfRangeException(nameof(max), "The maximum count argument cannot be negative.");
 
             var count = 0;
 
