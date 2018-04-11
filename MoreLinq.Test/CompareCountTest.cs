a3dbf2ed7b6389737c1a91afa8e90ff1452bc9ac
Atif Aziz <code@raboof.com>
2018-04-11 18:43:40 +0200

    Use TestCaseSource to expand combinations of tests

--- MoreLinq.Test/CompareCountTest.cs
+++ MoreLinq.Test/CompareCountTest.cs
@@ -24,20 +24,25 @@ namespace MoreLinq.Test
     [TestFixture]
     public class CompareCountTest
     {
-        [TestCase(0, 0,  0)]
-        [TestCase(0, 1, -1)]
-        [TestCase(1, 0,  1)]
-        [TestCase(1, 1,  0)]
-        public void CompareCount(int s1Count,
-            int s2Count,
-            int expectedCompareCount)
-        {
-            var s1 = Enumerable.Range(1, s1Count);
-            var s2 = Enumerable.Range(1, s2Count);
+        static readonly IEnumerable<TestCaseData> CompareCountData =
+            from e in new[]
+            {
+                new { Count1 = 0, Count2 = 0, Comparison =  0 },
+                new { Count1 = 0, Count2 = 1, Comparison = -1 },
+                new { Count1 = 1, Count2 = 0, Comparison =  1 },
+                new { Count1 = 1, Count2 = 1, Comparison =  0 },
+            }
+            from s in GetTestSequenceKinds(
+                          Enumerable.Range(1, e.Count1),
+                          Enumerable.Range(1, e.Count2),
+                          (xs, ys) => new { First = xs, Second = ys })
+            select new TestCaseData(s.First.Data, s.Second.Data)
+                    .Returns(e.Comparison)
+                    .SetName($"{{m}}({s.First.Kind}[{e.Count1}], {s.Second.Kind}[{e.Count2}]) = {e.Comparison}");
 
-            foreach (var (xs, ys) in ArrangeCollectionTestCases(s1, s2))
-                Assert.AreEqual(expectedCompareCount, xs.CompareCount(ys));
-        }
+        [TestCaseSource(nameof(CompareCountData))]
+        public int CompareCount(IEnumerable<int> xs, IEnumerable<int> ys) =>
+            xs.CompareCount(ys);
 
         [TestCase(0, 0,  0, 1)]
         [TestCase(0, 1, -1, 1)]
@@ -140,31 +145,41 @@ namespace MoreLinq.Test
             Assert.AreEqual(-1, seq2.CompareCount(seq1));
         }
 
-        static IEnumerable<(IEnumerable<T>, IEnumerable<T>)> ArrangeCollectionTestCases<T>(IEnumerable<T> s1, IEnumerable<T> s2)
+        enum SequenceKind
+        {
+            Sequence,
+            Collection,
+            ReadOnlyCollection,
+        }
+
+        static IEnumerable<TResult> GetTestSequenceKinds<T, TResult>(
+            IEnumerable<T> s1, IEnumerable<T> s2,
+            Func<(IEnumerable<T> Data, SequenceKind Kind),
+                (IEnumerable<T> Data, SequenceKind Kind), TResult> selector)
         {
             // Test that the operator is optimized for collections
 
-            var s1Seq = s1.Select(x => x);
-            var s2Seq = s2.Select(x => x);
+            var s1Seq = (s1.Select(x => x), SequenceKind.Sequence);
+            var s2Seq = (s2.Select(x => x), SequenceKind.Sequence);
 
-            var s1Col = s1.ToBreakingCollection(false);
-            var s2Col = s2.ToBreakingCollection(false);
+            var s1Col = (s1.ToBreakingCollection(false), SequenceKind.Collection);
+            var s2Col = (s2.ToBreakingCollection(false), SequenceKind.Collection);
 
-            var s1ReadOnlyCol = s1.ToBreakingCollection(true);
-            var s2ReadOnlyCol = s2.ToBreakingCollection(true);
+            var s1ReadOnlyCol = (s1.ToBreakingCollection(true), SequenceKind.ReadOnlyCollection);
+            var s2ReadOnlyCol = (s2.ToBreakingCollection(true), SequenceKind.ReadOnlyCollection);
 
             // sequences
-            yield return (s1Seq, s2Seq);
+            yield return selector(s1Seq, s2Seq);
 
             // sequences and collections
-            yield return (s1Seq, s2Col);
-            yield return (s1Col, s2Seq);
-            yield return (s1Col, s2Col);
+            yield return selector(s1Seq, s2Col);
+            yield return selector(s1Col, s2Seq);
+            yield return selector(s1Col, s2Col);
 
             // sequences and readOnlyCollections
-            yield return (s1Seq, s2ReadOnlyCol);
-            yield return (s1ReadOnlyCol, s2Seq);
-            yield return (s1ReadOnlyCol, s2ReadOnlyCol);
+            yield return selector(s1Seq, s2ReadOnlyCol);
+            yield return selector(s1ReadOnlyCol, s2Seq);
+            yield return selector(s1ReadOnlyCol, s2ReadOnlyCol);
         }
     }
 }
