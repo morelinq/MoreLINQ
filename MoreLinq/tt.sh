082382c74ceffd88a942652dbab9c62552994ceb
Atif Aziz <code@raboof.com>
2018-04-20 19:07:22 +0200

    Make BASH scripts runnable from anywhere

--- MoreLinq/tt.sh
+++ MoreLinq/tt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-[[ -e tt.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
+cd "$(dirname "$0")"
 find . -name "*.tt" -print0 | xargs -0 -t -L 1 sh -c '(dotnet tt "$0" || exit 255)'
