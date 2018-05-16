b1722890bc703a0fc5c7a40b8bef66d8af6457fa
Craig Norton <c9952594@gmail.com>
2018-05-16 01:31:08 +0100

    Added examples of how the other overloads map to the full parameter function

--- MoreLinq.Test/OrderedMergeTest.cs
+++ MoreLinq.Test/OrderedMergeTest.cs
@@ -122,5 +122,196 @@ namespace MoreLinq.Test
         class UpendedComparer : IComparer<int> {
             public int Compare(int x, int y) => y.CompareTo(x);
         }
+
+        [TestFixture]
+        public class AllParametersEquivalent { 
+
+            [Test]
+            public void first_second() {
+                var first = new[] { 1, 4, 5 };
+                var second = new int[] { 2, 3, 6 };
+
+                var exampleMerged = MoreEnumerable.OrderedMerge(
+                    first,
+                    second);
+
+                int Id(int id) => id;
+                int First(int f, int _) => f;
+
+                var allParametersMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    firstKeySelector: Id,
+                    secondKeySelector: Id,
+                    firstSelector: Id,
+                    secondSelector: Id,
+                    bothSelector: First,
+                    comparer: null);
+
+                Assert.That(exampleMerged, Is.EqualTo(allParametersMerged));
+                Assert.That(allParametersMerged, Is.EqualTo(new [] { 1, 2, 3, 4, 5, 6 }));
+            }
+
+            [Test]
+            public void first_second_comparer()
+            {
+                var first = new[] { 1, 4, 5 };
+                var second = new int[] { 2, 3, 6 };
+                var comparer = Comparer.Create<int>((x, y) => y.CompareTo(x));
+
+                var exampleMerged = MoreEnumerable.OrderedMerge(
+                    first,
+                    second,
+                    comparer);
+
+                int Id(int id) => id;
+                int First(int f, int _) => f;
+
+                var allParametersMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    comparer: comparer,
+                    firstKeySelector: Id,
+                    secondKeySelector: Id,
+                    firstSelector: Id,
+                    secondSelector: Id,
+                    bothSelector: First);
+
+                Assert.That(exampleMerged, Is.EqualTo(allParametersMerged));
+                Assert.That(allParametersMerged, Is.EqualTo(new[] { 2, 3, 6, 1, 4, 5 }));
+            }
+
+            [Test]
+            public void first_second_keySelector()
+            {
+                var first = new[] { 16, 4, 1 }; // 16, 64, 256
+                var second = new int[] { 32, 8, 2 }; // 8, 32, 128
+                int KeySelector(int key) => 256 / key;
+
+                var exampleMerged = MoreEnumerable.OrderedMerge(
+                    first,
+                    second,
+                    KeySelector);
+
+                int Id(int id) => id;
+                int First(int f, int _) => f;
+
+                var allParametersMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    firstKeySelector: KeySelector,
+                    secondKeySelector: KeySelector,
+                    firstSelector: Id,
+                    secondSelector: Id,
+                    bothSelector: First,
+                    comparer: null);
+
+                Assert.That(exampleMerged, Is.EqualTo(allParametersMerged));
+                Assert.That(allParametersMerged, Is.EqualTo(new[] { 32, 16, 8, 4, 2, 1 }));
+            }
+
+            [Test]
+            public void first_second_keySelector_firstSelector_secondSelector_bothSelector()
+            {
+                var first = new[] { 1, 4, 5 };
+                var second = new int[] { 2, 3, 6 };
+                int KeySelector(int id) => id;
+                int FirstSelector(int id) => id;
+                int SecondSelector(int id) => id;
+                int BothSelector(int f, int _) => f;
+
+                var exampleMerged = MoreEnumerable.OrderedMerge(first: first,
+                    second: second,
+                    keySelector: KeySelector,
+                    firstSelector: FirstSelector,
+                    secondSelector: SecondSelector,
+                    bothSelector: BothSelector);
+
+                var allParametersMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    firstKeySelector: KeySelector,
+                    secondKeySelector: KeySelector,
+                    firstSelector: FirstSelector,
+                    secondSelector: SecondSelector,
+                    bothSelector: BothSelector,
+                    comparer: null);
+
+                Assert.That(exampleMerged, Is.EqualTo(allParametersMerged));
+                Assert.That(allParametersMerged, Is.EqualTo(new[] { 1, 2, 3, 4, 5, 6 }));
+            }
+
+            [Test]
+            public void first_second_keySelector_firstSelector_secondSelector_bothSelector_comparer()
+            {
+                var first = new[] { 1, 4, 5 };
+                var second = new int[] { 2, 3, 6 };
+                int KeySelector(int id) => id;
+                int FirstSelector(int id) => id;
+                int SecondSelector(int id) => id;
+                int BothSelector(int f, int _) => f;
+                var comparer = Comparer.Create<int>((x, y) => y.CompareTo(x));
+
+                var exampleMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    keySelector: KeySelector,
+                    firstSelector: FirstSelector,
+                    secondSelector: SecondSelector,
+                    bothSelector: BothSelector,
+                    comparer: comparer);
+
+                var allParametersMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    firstKeySelector: KeySelector,
+                    secondKeySelector: KeySelector,
+                    firstSelector: FirstSelector,
+                    secondSelector: SecondSelector,
+                    bothSelector: BothSelector,
+                    comparer: comparer);
+
+                Assert.That(exampleMerged, Is.EqualTo(allParametersMerged));
+                Assert.That(allParametersMerged, Is.EqualTo(new[] { 2, 3, 6, 1, 4, 5 }));
+            }
+
+            [Test]
+            public void first_second_firstKeySelector_secondKeySelector_firstSelector_secondSelector_bothSelector()
+            {
+                var first = new[] { 1, 4, 5 };
+                var second = new int[] { 2, 3, 6 };
+                int FirstKeySelector(int id) => id;
+                int SecondKeySelector(int id) => id;
+                int FirstSelector(int id) => id;
+                int SecondSelector(int id) => id;
+                int BothSelector(int f, int _) => f;
+
+                var exampleMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    firstKeySelector: FirstKeySelector,
+                    secondKeySelector: SecondKeySelector,
+                    firstSelector: FirstSelector,
+                    secondSelector: SecondSelector,
+                    bothSelector: BothSelector);
+
+                var allParametersMerged = MoreEnumerable.OrderedMerge(
+                    first: first,
+                    second: second,
+                    firstKeySelector: FirstKeySelector,
+                    secondKeySelector: SecondKeySelector,
+                    firstSelector: FirstSelector,
+                    secondSelector: SecondSelector,
+                    bothSelector: BothSelector,
+                    comparer: null);
+
+                Assert.That(exampleMerged, Is.EqualTo(allParametersMerged));
+                Assert.That(allParametersMerged, Is.EqualTo(new[] { 1, 2, 3, 4, 5, 6 }));
+            }
+
+
+
+        }
+
     }
 }
