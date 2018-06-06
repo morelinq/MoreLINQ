7f82aa0363af30f99ba6b269318d0b6a4cc4e6c0
leandromoh <leandromoh@hotmail.com>
2018-06-06 02:36:09 -0300

    added overloads without predicates

--- MoreLinq/IgnoreErrors.cs
+++ MoreLinq/IgnoreErrors.cs
@@ -23,7 +23,72 @@ namespace MoreLinq
 
     static partial class MoreEnumerable
     {
-        static Func<Exception, bool> defaultErrorPredicate = (Exception ex) => false;
+   //   static Func<Exception, bool> defaultErrorPredicate = (Exception ex) => false;
+
+        /// <summary>
+        /// Ignore some exceptions that can occurs during the iteration of the sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TException">Type of the exception that can be ignored.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <returns>
+        /// A sequence of elements <paramref name="source"/> that ignore the exceptions of type
+        /// <typeparamref name="TException"/>.
+        /// </returns>
+        /// <remarks>
+        /// This operator uses deferred execution and streams its results.
+        /// </remarks>
+
+        public static IEnumerable<T> IgnoreErrors<T, TException>(
+            this IEnumerable<T> source)
+            where TException : Exception
+            => IgnoreErrorsImpl<T, TException, TException, TException>(source, null, null, null);
+
+        /// <summary>
+        /// Ignore some exceptions that can occurs during the iteration of the sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TException1">First type of the exception that can be ignored.</typeparam>
+        /// <typeparam name="TException2">Second type of the exception that can be ignored.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <returns>
+        /// A sequence of elements <paramref name="source"/> that ignore the exceptions of type
+        /// <typeparamref name="TException1"/> or <typeparamref name="TException2"/>.
+        /// </returns>
+        /// <remarks>
+        /// This operator uses deferred execution and streams its results.
+        /// Exceptions are caught in order which were passed, as well in a catch block.
+        /// </remarks>
+
+        public static IEnumerable<T> IgnoreErrors<T, TException1, TException2>(
+            this IEnumerable<T> source)
+            where TException1 : Exception
+            where TException2 : Exception
+            => IgnoreErrorsImpl<T, TException1, TException2, TException2>(source, null, null, null);
+
+        /// <summary>
+        /// Ignore some exceptions that can occurs during the iteration of the sequence.
+        /// </summary>
+        /// <typeparam name="T">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TException1">First type of the exception that can be ignored.</typeparam>
+        /// <typeparam name="TException2">Second type of the exception that can be ignored.</typeparam>
+        /// <typeparam name="TException3">Third type of the exception that can be ignored.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <returns>
+        /// A sequence of elements <paramref name="source"/> that ignore the exceptions of type
+        /// <typeparamref name="TException1"/>, <typeparamref name="TException2"/> or <typeparamref name="TException3"/>.
+        /// </returns>
+        /// <remarks>
+        /// This operator uses deferred execution and streams its results.
+        /// Exceptions are caught in order which were passed, as well in a catch block.
+        /// </remarks>
+
+        public static IEnumerable<T> IgnoreErrors<T, TException1, TException2, TException3>(
+            this IEnumerable<T> source)
+            where TException1 : Exception
+            where TException2 : Exception
+            where TException3 : Exception
+            => IgnoreErrorsImpl<T, TException1, TException2, TException3>(source, null, null, null);
 
         /// <summary>
         /// Ignore some exceptions that can occurs during the iteration of the sequence.
@@ -47,8 +112,11 @@ namespace MoreLinq
             this IEnumerable<T> source,
             Func<TException, bool> errorPredicate)
             where TException : Exception
-            => IgnoreErrors(source, errorPredicate ?? throw new ArgumentNullException(nameof(errorPredicate)),
-                            defaultErrorPredicate, defaultErrorPredicate);
+        {
+            if (errorPredicate == null) throw new ArgumentNullException(nameof(errorPredicate));
+
+            return IgnoreErrorsImpl(source, errorPredicate, errorPredicate, errorPredicate);
+        }
 
         /// <summary>
         /// Ignore some exceptions that can occurs during the iteration of the sequence.
@@ -81,7 +149,12 @@ namespace MoreLinq
             Func<TException2, bool> error2Predicate)
             where TException1 : Exception
             where TException2 : Exception
-            => IgnoreErrors(source, error1Predicate, error2Predicate, defaultErrorPredicate);
+        {
+            if (error1Predicate == null) throw new ArgumentNullException(nameof(error1Predicate));
+            if (error2Predicate == null) throw new ArgumentNullException(nameof(error2Predicate));
+
+            return IgnoreErrorsImpl(source, error1Predicate, error2Predicate, error2Predicate);
+        }
 
         /// <summary>
         /// Ignore some exceptions that can occurs during the iteration of the sequence.
@@ -122,10 +195,23 @@ namespace MoreLinq
             where TException2 : Exception
             where TException3 : Exception
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
             if (error1Predicate == null) throw new ArgumentNullException(nameof(error1Predicate));
             if (error2Predicate == null) throw new ArgumentNullException(nameof(error2Predicate));
             if (error3Predicate == null) throw new ArgumentNullException(nameof(error3Predicate));
+
+            return IgnoreErrorsImpl(source, error1Predicate, error2Predicate, error3Predicate);
+        }
+
+        static IEnumerable<T> IgnoreErrorsImpl<T, TException1, TException2, TException3>(
+            this IEnumerable<T> source,
+            Func<TException1, bool> error1Predicate,
+            Func<TException2, bool> error2Predicate,
+            Func<TException3, bool> error3Predicate)
+            where TException1 : Exception
+            where TException2 : Exception
+            where TException3 : Exception
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
 
             return _(); IEnumerable<T> _()
             {
@@ -137,15 +223,15 @@ namespace MoreLinq
                         {
                             if (!e.MoveNext()) break;
                         }
-                        catch (TException1 ex) when (error1Predicate(ex))
+                        catch (TException1 ex) when (error1Predicate?.Invoke(ex) ?? true)
                         {
                             continue;
                         }
-                        catch (TException2 ex) when (error2Predicate(ex))
+                        catch (TException2 ex) when (error2Predicate?.Invoke(ex) ?? true)
                         {
                             continue;
                         }
-                        catch (TException3 ex) when (error3Predicate(ex))
+                        catch (TException3 ex) when (error3Predicate?.Invoke(ex) ?? true)
                         {
                             continue;
                         }
