41dba18dc19829dd7ecdd859a17cb7ed01e90f74
Atif Aziz <code@raboof.com>
2018-06-28 22:54:34 +0200

    Rename Windowed to Window & return windows as lists

--- MoreLinq/Window.cs
+++ MoreLinq/Window.cs
@@ -34,12 +34,12 @@ namespace MoreLinq
         /// <param name="size">The size (number of elements) in each window</param>
         /// <returns>A series of sequences representing each sliding window subsequence</returns>
 
-        public static IEnumerable<IEnumerable<TSource>> Windowed<TSource>(this IEnumerable<TSource> source, int size)
+        public static IEnumerable<IList<TSource>> Window<TSource>(this IEnumerable<TSource> source, int size)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
             if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
 
-            return _(); IEnumerable<IEnumerable<TSource>> _()
+            return _(); IEnumerable<IList<TSource>> _()
             {
                 using (var iter = source.GetEnumerator())
                 {
@@ -70,5 +70,21 @@ namespace MoreLinq
                 }
             }
         }
+
+        /// <summary>
+        /// Processes a sequence into a series of subsequences representing a windowed subset of the original
+        /// </summary>
+        /// <remarks>
+        /// The number of sequences returned is: <c>Max(0, sequence.Count() - windowSize) + 1</c><br/>
+        /// Returned subsequences are buffered, but the overall operation is streamed.<br/>
+        /// </remarks>
+        /// <typeparam name="TSource">The type of the elements of the source sequence</typeparam>
+        /// <param name="source">The sequence to evaluate a sliding window over</param>
+        /// <param name="size">The size (number of elements) in each window</param>
+        /// <returns>A series of sequences representing each sliding window subsequence</returns>
+
+        [Obsolete("Use " + nameof(Window) + " instead.")]
+        public static IEnumerable<IEnumerable<TSource>> Windowed<TSource>(this IEnumerable<TSource> source, int size) =>
+            source.Window(size);
     }
 }
