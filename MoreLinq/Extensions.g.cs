c215552d016d0e2499dbe2f0e7541503a8382f82
Atif Aziz <code@raboof.com>
2019-03-07 15:49:03 +0100

    Generate wrappers

--- MoreLinq/Extensions.g.cs
+++ MoreLinq/Extensions.g.cs
@@ -2560,6 +2560,55 @@ namespace MoreLinq.Extensions
 
     }
 
+    /// <summary><c>IndexBy</c> extension.</summary>
+
+    [GeneratedCode("MoreLinq.ExtensionsGenerator", "1.0.0.0")]
+    public static partial class IndexByExtension
+    {
+
+        /// <summary>
+        /// Applies a key-generating function to each element of a sequence and returns a sequence that
+        /// contains the elements of the original sequence as well its key and index inside the group of its key.
+        /// An additional argument specifies a comparer to use for testing equivalence of keys.
+        /// </summary>
+        /// <typeparam name="TSource">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TKey">Type of the projected key.</typeparam>
+        /// <typeparam name="TResult">Type of the projected element.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="keySelector">Function that transforms each item of source sequence into a key to be compared against the others.</param>
+        /// <param name="resultSelector">The projection to each element, its key and index.</param>
+        /// <returns>A sequence of unique keys and their number of occurrences in the original sequence.</returns>
+
+        public static IEnumerable<TResult> IndexBy<TSource, TKey, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TKey, int, TResult> resultSelector)
+            => MoreEnumerable.IndexBy(source, keySelector, resultSelector);
+
+        /// <summary>
+        /// Applies a key-generating function to each element of a sequence and returns a sequence that
+        /// contains the elements of the original sequence as well its key and index inside the group of its key.
+        /// An additional argument specifies a comparer to use for testing equivalence of keys.
+        /// </summary>
+        /// <typeparam name="TSource">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TKey">Type of the projected key.</typeparam>
+        /// <typeparam name="TResult">Type of the projected element.</typeparam>
+        /// <param name="source">Source sequence.</param>
+        /// <param name="keySelector">Function that transforms each item of source sequence into a key to be compared against the others.</param>
+        /// <param name="resultSelector">The projection to each element, its key and index.</param>
+        /// <param name="comparer">The equality comparer to use to determine whether or not keys are equal.
+        /// If null, the default equality comparer for <typeparamref name="TSource"/> is used.</param>
+        /// <returns>A sequence of unique keys and their number of occurrences in the original sequence.</returns>
+
+        public static IEnumerable<TResult> IndexBy<TSource, TKey, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TSource, TKey, int, TResult> resultSelector,
+            IEqualityComparer<TKey> comparer)
+            => MoreEnumerable.IndexBy(source, keySelector, resultSelector, comparer);
+
+    }
+
     /// <summary><c>Insert</c> extension.</summary>
 
     [GeneratedCode("MoreLinq.ExtensionsGenerator", "1.0.0.0")]
