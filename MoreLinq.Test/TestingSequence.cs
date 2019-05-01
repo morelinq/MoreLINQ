72823fee04a9e48e3bd5edf7fdd4b57b42ecb174
leandromoh <leandromoh@hotmail.com>
2019-05-01 16:48:40 -0300

    add validation for dispose once

--- MoreLinq.Test/TestingSequence.cs
+++ MoreLinq.Test/TestingSequence.cs
@@ -67,7 +67,11 @@ namespace MoreLinq.Test
             Assert.That(_sequence, Is.Not.Null, "LINQ operators should not enumerate a sequence more than once.");
             var enumerator = _sequence.GetEnumerator().AsWatchtable();
             _disposed = false;
-            enumerator.Disposed += delegate { _disposed = true; };
+            enumerator.Disposed += delegate 
+            { 
+                Assert.That(_disposed, Is.False, "LINQ operators should not dispose a sequence more than once.");
+                _disposed = true; 
+            };
             enumerator.MoveNextCalled += delegate { MoveNextCallCount++; };
             _sequence = null;
             return enumerator;
