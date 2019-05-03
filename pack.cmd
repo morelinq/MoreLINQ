b37a9852eccfc4cdb54230eaf194988a531e47ec
Atif Aziz <code@raboof.com>
2019-05-03 16:49:30 +0200

    Move packaging properties into project

--- pack.cmd
+++ pack.cmd
@@ -11,10 +11,7 @@ if not %errorlevel%==0 exit /b %errorlevel%
 set VERSION_SUFFIX=
 if not "%~1"=="" set VERSION_SUFFIX=/p:VersionSuffix=%1
 call build ^
-  && dotnet pack /p:Configuration=Release       ^
-                 /p:IncludeSymbols=true         ^
-                 /p:IncludeSource=true          ^
-                 /p:PackageOutputPath=%cd%/dist ^
-                 %VERSION_SUFFIX%               ^
+  && dotnet pack /p:Configuration=Release   ^
+                 %VERSION_SUFFIX%           ^
                  MoreLinq\MoreLinq.csproj
 goto :EOF
