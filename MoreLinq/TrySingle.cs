d8e8fc79d02de4eb59c9cba38f9fe84a017f21c8
James Webster <jim@agilewallaby.net>
2019-10-10 10:34:49 +1000

    Update MoreLinq/TrySingle.cs
    Co-Authored-By: Atif Aziz <code@raboof.com>

--- MoreLinq/TrySingle.cs
+++ MoreLinq/TrySingle.cs
@@ -40,7 +40,7 @@ namespace MoreLinq
         /// <typeparam name="TResult">The result type of the resultSelector.</typeparam>
         /// <returns>The value provided by the resultSelector.</returns>
 
-                public static TResult TrySingle<T, TCardinality, TResult>(this IEnumerable<T> source,
+        public static TResult TrySingle<T, TCardinality, TResult>(this IEnumerable<T> source,
             TCardinality zero, TCardinality one, TCardinality many,
             Func<TCardinality, T, TResult> resultSelector)
         {
