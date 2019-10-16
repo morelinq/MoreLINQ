4d9f6d8bac36fadf35655c8a57e5c321091eaa54
Atif Aziz <code@raboof.com>
2019-10-16 14:41:13 +0200

    Move classes closer to sole use and test

--- MoreLinq.Test/TrySingleTest.cs
+++ MoreLinq.Test/TrySingleTest.cs
@@ -70,6 +70,42 @@ namespace MoreLinq.Test
             new TestCaseData(new BreakingSingleElementReadOnlyCollection<int>(20), 20)
         };
 
+        class BreakingSingleElementCollectionBase<T> : IEnumerable<T>
+        {
+            readonly T _element;
+
+            protected BreakingSingleElementCollectionBase(T element) => _element = element;
+
+            public int Count => 1;
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                yield return _element;
+                throw new Exception($"{nameof(MoreEnumerable.TrySingle)} should not have attempted to consume a second element.");
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        sealed class BreakingSingleElementCollection<T> :
+            BreakingSingleElementCollectionBase<T>, ICollection<T>
+        {
+            public BreakingSingleElementCollection(T element) : base(element) {}
+
+            public void Add(T item) => throw new NotImplementedException();
+            public void Clear() => throw new NotImplementedException();
+            public bool Contains(T item) => throw new NotImplementedException();
+            public void CopyTo(T[] array, int arrayIndex) => throw new NotImplementedException();
+            public bool Remove(T item) => throw new NotImplementedException();
+            public bool IsReadOnly => true;
+        }
+
+        sealed class BreakingSingleElementReadOnlyCollection<T> :
+            BreakingSingleElementCollectionBase<T>, IReadOnlyCollection<T>
+        {
+            public BreakingSingleElementReadOnlyCollection(T element) : base(element) {}
+        }
+
         [TestCase(SourceKind.Sequence)]
         [TestCase(SourceKind.BreakingList)]
         [TestCase(SourceKind.BreakingReadOnlyList)]
@@ -112,40 +148,5 @@ namespace MoreLinq.Test
             }
         }
 
-        class BreakingSingleElementCollectionBase<T> : IEnumerable<T>
-        {
-            readonly T _element;
-
-            protected BreakingSingleElementCollectionBase(T element) => _element = element;
-
-            public int Count => 1;
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                yield return _element;
-                throw new Exception($"{nameof(MoreEnumerable.TrySingle)} should not have attempted to consume a second element.");
-            }
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-
-        sealed class BreakingSingleElementCollection<T> :
-            BreakingSingleElementCollectionBase<T>, ICollection<T>
-        {
-            public BreakingSingleElementCollection(T element) : base(element) {}
-
-            public void Add(T item) => throw new NotImplementedException();
-            public void Clear() => throw new NotImplementedException();
-            public bool Contains(T item) => throw new NotImplementedException();
-            public void CopyTo(T[] array, int arrayIndex) => throw new NotImplementedException();
-            public bool Remove(T item) => throw new NotImplementedException();
-            public bool IsReadOnly => true;
-        }
-
-        sealed class BreakingSingleElementReadOnlyCollection<T> :
-            BreakingSingleElementCollectionBase<T>, IReadOnlyCollection<T>
-        {
-            public BreakingSingleElementReadOnlyCollection(T element) : base(element) {}
-        }
     }
 }
