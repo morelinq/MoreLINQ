7a58b00de4277b0607b9078db514fff64833c2f8
Atif Aziz <code@raboof.com>
2019-10-24 16:14:32 +0200

    Fix pack (bash) script; was still using MSBuild

--- pack.cmd
+++ pack.cmd
@@ -11,7 +11,7 @@ if not %errorlevel%==0 exit /b %errorlevel%
 set VERSION_SUFFIX=
 if not "%~1"=="" set VERSION_SUFFIX=/p:VersionSuffix=%1
 call build ^
-  && dotnet pack /p:Configuration=Release   ^
-                 %VERSION_SUFFIX%           ^
+  && dotnet pack -c Release       ^
+                 %VERSION_SUFFIX% ^
                  MoreLinq\MoreLinq.csproj
 goto :EOF
