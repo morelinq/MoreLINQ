7d1fc72959de920d872ce3b867eceef4fbd3ea43
Atif Aziz <code@raboof.com>
2019-10-25 10:26:26 +0200

    Build just the configuration for *nix/macOS CI

--- build.sh
+++ build.sh
@@ -13,6 +13,11 @@ codegen() {
 codegen MoreLinq/Extensions.g.cs -x "[/\\\\]ToDataTable\.cs$" -u System.Linq -u System.Collections MoreLinq
 codegen MoreLinq/Extensions.ToDataTable.g.cs -i "[/\\\\]ToDataTable\.cs$" -u System.Data -u System.Linq.Expressions MoreLinq
 MoreLinq/tt.sh
-for c in Debug Release; do
-    dotnet build --no-restore -c $c
+if [[ -z "$1" ]]; then
+    configs="Debug Release"
+else
+    configs="$1"
+fi
+for c in $configs; do
+    echo dotnet build --no-restore -c $c
 done
