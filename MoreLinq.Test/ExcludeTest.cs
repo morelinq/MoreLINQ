3f42ffbcb6e1279fb49ff8fff9b96f58a82746d9
Orace <pierre.lando@gmail.com>
2019-11-08 20:15:53 +0100

    Add TestExcludeOnKnownInput

--- MoreLinq.Test/ExcludeTest.cs
+++ MoreLinq.Test/ExcludeTest.cs
@@ -1,3 +1,20 @@
+#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008 Jonathan Skeet. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
 namespace MoreLinq.Test
 {
     using NUnit.Framework;
@@ -142,6 +159,15 @@ namespace MoreLinq.Test
             var result = sequence.Exclude(count + 5, count);
 
             Assert.That(result, Is.EqualTo(sequence));
+        }
+
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5 }, 0, 6, ExpectedResult = new int[0])]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5 }, 2, 6, ExpectedResult = new[] { 0, 1 })]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5 }, 0, 3, ExpectedResult = new[] { 3, 4, 5 })]
+        [TestCase(new[] { 0, 1, 2, 3, 4, 5 }, 2, 3, ExpectedResult = new[] { 0, 1, 5 })]
+        public int[] TestExcludeOnKnownInput(int[] source, int startIndex, int count)
+        {
+            return source.AsTestingSequence().Exclude(startIndex, count).ToArray();
         }
     }
 }
