257329df2501df99788567a68fe08f8827dad1e4
Orace <pierre.lando@gmail.com>
2019-11-08 20:07:00 +0100

    Add TestSkipUntilOnKnownInput

--- MoreLinq.Test/SkipUntilTest.cs
+++ MoreLinq.Test/SkipUntilTest.cs
@@ -18,6 +18,8 @@
 namespace MoreLinq.Test
 {
     using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class SkipUntilTest
@@ -56,6 +58,21 @@ namespace MoreLinq.Test
             // started returning items after -1.
             var sequence = Enumerable.Range(-2, 5).SkipUntil(x => 1 / x == -1);
             sequence.AssertSequenceEqual(0, 1, 2);
+        }
+
+        public static readonly IEnumerable<ITestCaseData> TestData =
+            from e in new[]
+            {
+                new {s = new[] {1, 2, 3}, o = 1, r = new[] {2, 3}},
+                new {s = new[] {1, 2, 3}, o = 2, r = new[] {3}},
+                new {s = new[] {1, 2, 3}, o = 3, r = new int[0]}
+            }
+            select new TestCaseData(e.s, e.o).Returns(e.r);
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public int[] TestSkipUntilOnKnownInput(int[] source, int min)
+        {
+            return source.AsTestingSequence().SkipUntil(v => v >= min).ToArray();
         }
     }
 }
