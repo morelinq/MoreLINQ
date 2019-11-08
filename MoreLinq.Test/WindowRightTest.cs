cc21cc4f55b1cc946e3ad0d25b4676bf355f04b8
Orace <pierre.lando@gmail.com>
2019-11-08 19:39:11 +0100

    Clean-up tests

--- MoreLinq.Test/WindowRightTest.cs
+++ MoreLinq.Test/WindowRightTest.cs
@@ -1,9 +1,25 @@
-using NUnit.Framework.Interfaces;
+#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2008 Jonathan Skeet. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
 
 namespace MoreLinq.Test
 {
     using System.Collections.Generic;
     using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
 
     [TestFixture]
     public class WindowRightTest
@@ -122,26 +138,6 @@ namespace MoreLinq.Test
                 reader.Read().AssertSequenceEqual(3, 4, 5);
                 reader.ReadEnd();
             }
-        }
-
-        static IEnumerable<T> Seq<T>(params T[] values) => values;
-
-        public static readonly IEnumerable<ITestCaseData> TestData =
-            from e in new[]
-            {
-                new {Source = Enumerable.Range(0, 4), Size = 1, Result = new[] {Seq(0), Seq(1), Seq(2), Seq(3)}},
-                new {Source = Enumerable.Range(0, 4), Size = 2, Result = new[] {Seq(0), Seq(0, 1), Seq(1, 2), Seq(2, 3)}},
-                new {Source = Enumerable.Range(0, 4), Size = 3, Result = new[] {Seq(0), Seq(0, 1), Seq(0, 1, 2), Seq(1, 2, 3)}},
-                new {Source = Enumerable.Range(0, 4), Size = 4, Result = new[] {Seq(0), Seq(0, 1), Seq(0, 1, 2), Seq(0, 1, 2, 3)}}
-            }
-            select new TestCaseData(e.Source, e.Size).Returns(e.Result);
-
-        [Test, TestCaseSource(nameof(TestData))]
-        public IEnumerable<IEnumerable<int>> TestWindowRightOnKnownResults(IEnumerable<int> sequence, int sizes)
-        {
-            using var testingSequence = sequence.AsTestingSequence();
-            var r = testingSequence.WindowRight(sizes).ToList();
-            return r;
         }
     }
 }
