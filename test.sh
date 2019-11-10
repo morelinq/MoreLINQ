31fd6ba3a83990f330905897e26eaa68fcc1f4bd
Atif Aziz <code@raboof.com>
2019-11-10 16:32:58 +0100

    Publish coverage report over Travis

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")"
-./build.sh
+./build.sh $c
+if [[ -z "$1" ]]; then
+    configs="Debug Release"
+else
+    configs="$1"
+fi
 for v in 2.1 3.0; do
-    for c in Debug Release; do
+    for c in $configs; do
         if [[ "$c" == "Debug" ]]; then
             coverage_args="-p:CollectCoverage=true
                            -p:CoverletOutputFormat=opencover
@@ -18,6 +23,7 @@ if [[ -z `which mono 2>/dev/null` ]]; then
     echo>&2 NOTE! Mono does not appear to be installed so unit tests
     echo>&2 against the Mono runtime will be skipped.
 else
-    mono MoreLinq.Test/bin/Debug/net451/MoreLinq.Test.exe
-    mono MoreLinq.Test/bin/Release/net451/MoreLinq.Test.exe
+    for c in $configs; do
+        mono MoreLinq.Test/bin/$c/net451/MoreLinq.Test.exe
+    done
 fi
