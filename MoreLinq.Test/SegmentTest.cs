b0517e1a853a737ba1020939a0f1a4aab7d4617e
Orace <pierre.lando@gmail.com>
2019-11-12 10:13:45 +0100

    Add test for Segment on known input:  - input sequence is empty  - input sequence contains only new segment start  - input sequence do not contains new segment start  - input sequence start with a segment start  - input sequence do not start with a segment start

--- MoreLinq.Test/SegmentTest.cs
+++ MoreLinq.Test/SegmentTest.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using NUnit.Framework.Interfaces;
+
 namespace MoreLinq.Test
 {
     using NUnit.Framework;
@@ -86,7 +89,7 @@ namespace MoreLinq.Test
         [Test]
         public void TestSegmentationStartsWithSecondItem()
         {
-            var sequence = new[] { 0 };
+            var sequence = new[] {0};
             var resultA = sequence.Segment(BreakingFunc.Of<int, bool>());
             var resultB = sequence.Segment(BreakingFunc.Of<int, int, bool>());
             var resultC = sequence.Segment(BreakingFunc.Of<int, int, int, bool>());
@@ -123,11 +126,35 @@ namespace MoreLinq.Test
         {
             const int repCount = 5;
             var sequence = Enumerable.Range(1, 3)
-                                     .SelectMany(x => Enumerable.Repeat(x, repCount));
+                .SelectMany(x => Enumerable.Repeat(x, repCount));
             var result = sequence.Segment((curr, prev, i) => curr != prev);
 
             Assert.AreEqual(sequence.Distinct().Count(), result.Count());
             Assert.IsTrue(result.All(s => s.Count() == repCount));
+        }
+
+        static IEnumerable<T> Seq<T>(params T[] values) => values;
+
+        public static readonly IEnumerable<ITestCaseData> TestData =
+            from e in new[]
+            {
+                // input sequence is empty
+                new {s = Enumerable.Empty<int>(), r = Enumerable.Empty<IEnumerable<int>>()},
+                // input sequence contains only new segment start
+                new {s = Seq(0, 3, 6), r = Seq(Seq(0), Seq(3), Seq(6))},
+                // input sequence do not contains new segment start
+                new {s = Seq(1, 2, 4, 5), r = Seq(Seq(1, 2, 4, 5))},
+                // input sequence start with a segment start
+                new {s = Seq(0, 1, 2, 3, 4, 5), r = Seq(Seq(0, 1, 2), Seq(3, 4, 5))},
+                // input sequence do not start with a segment start
+                new {s = Seq(1, 2, 3, 4, 5), r = Seq(Seq(1, 2), Seq(3, 4, 5))}
+            }
+            select new TestCaseData(e.s).Returns(e.r);
+
+        [Test, TestCaseSource(nameof(TestData))]
+        public IEnumerable<IEnumerable<int>> TestSegmentOnKnownInput(IEnumerable<int> source)
+        {
+            return source.AsTestingSequence().Segment(v => v % 3 == 0);
         }
     }
 }
