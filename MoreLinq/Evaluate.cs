2511f14b34c4de4c108a3d4be54acf7450cea4c4
Orace <pierre.lando@gmail.com>
2019-11-22 16:40:02 +0100

    Nipticks for Evaluate Add a more specific message if an element of the provided functions is null update the documentation accordingly.

--- MoreLinq/Evaluate.cs
+++ MoreLinq/Evaluate.cs
@@ -34,10 +34,10 @@ namespace MoreLinq
         /// <typeparam name="T">The type of the object returned by the functions.</typeparam>
         /// <param name="functions">The functions to evaluate.</param>
         /// <returns>A sequence with results from invoking <paramref name="functions"/>.</returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="functions"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="functions"/> or one of its elements is <c>null</c>.</exception>
 
         public static IEnumerable<T> Evaluate<T>(this IEnumerable<Func<T>> functions) =>
-            from f in functions ?? throw new ArgumentNullException(nameof(functions))
+            from f in functions ?? throw new ArgumentNullException(nameof(functions), "At least one of the provided functions is null.")
             select f();
     }
 }
--- MoreLinq/Extensions.g.cs
+++ MoreLinq/Extensions.g.cs
@@ -1465,7 +1465,7 @@ namespace MoreLinq.Extensions
         /// <typeparam name="T">The type of the object returned by the functions.</typeparam>
         /// <param name="functions">The functions to evaluate.</param>
         /// <returns>A sequence with results from invoking <paramref name="functions"/>.</returns>
-        /// <exception cref="ArgumentNullException">When <paramref name="functions"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentNullException">When <paramref name="functions"/> or one of its elements is <c>null</c>.</exception>
 
         public static IEnumerable<T> Evaluate<T>(this IEnumerable<Func<T>> functions)             => MoreEnumerable.Evaluate(functions);
 
