26f1d96d4cbdd6a2df3e89dba1096f26fb509a98
Atif Aziz <code@raboof.com>
2020-08-07 19:26:02 +0200

    Avoid list allocation for head count == 1

--- MoreLinq/SpillSpan.cs
+++ MoreLinq/SpillSpan.cs
@@ -20,6 +20,7 @@ namespace MoreLinq
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using static Optuple;
 
     partial class MoreEnumerable
     {
@@ -55,7 +56,10 @@ namespace MoreLinq
             if (headerSelector == null) throw new ArgumentNullException(nameof(headerSelector));
             if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
 
-            return source.SpillSpan(1, h => headerSelector(h.Single()),
+            return source.SpillSpan((h, i) => i == 0 ? Some(h) : default,
+                                    default, Some, (a, _) => a,
+                                    h => h is (true, var v) ? headerSelector(v)
+                                                            : throw new InvalidOperationException(),
                                     (h, e, _) => resultSelector(h, e));
         }
 
@@ -185,7 +189,7 @@ namespace MoreLinq
             if (headerSelector == null) throw new ArgumentNullException(nameof(headerSelector));
             if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
 
-            return source.SpillSpan((e, i) => predicate(e, i) ? (true, e) : default,
+            return source.SpillSpan((e, i) => predicate(e, i) ? Some(e) : default,
                                     empty, seeder, accumulator, headerSelector, resultSelector);
         }
 
