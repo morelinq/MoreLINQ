f669558facbbec9269a466c81594d5db8a19b4d1
Atif Aziz <code@raboof.com>
2020-12-26 13:05:58 +0100

    Integrate templated code generation into project file (#776)

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ codegen() {
 }
 codegen MoreLinq/Extensions.g.cs -x "[/\\\\]ToDataTable\.cs$" -u System.Linq -u System.Collections MoreLinq
 codegen MoreLinq/Extensions.ToDataTable.g.cs -i "[/\\\\]ToDataTable\.cs$" -u System.Data -u System.Linq.Expressions MoreLinq
-MoreLinq/tt.sh
+dotnet build MoreLinq -t:TransformTextTemplates -p:BuildInParallel=false
 if [[ -z "$1" ]]; then
     configs="Debug Release"
 else
