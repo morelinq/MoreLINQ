2331f3da57d452479a2bea7d9b06dac4de9c445c
Atif Aziz <code@raboof.com>
2021-01-14 23:59:16 +0100

    Add initial implementation of Merge with some tests

---
+++ MoreLinq.Test/Async/TestingAsyncSequence.cs
@@ -0,0 +1,88 @@
+#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2009 Atif Aziz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace MoreLinq.Test.Async
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using NUnit.Framework;
+
+    static class TestingAsyncSequence
+    {
+        public static TestingAsyncSequence<T> Of<T>(params T[] elements) =>
+            new TestingAsyncSequence<T>(elements.ToAsyncEnumerable());
+
+        public static TestingAsyncSequence<T> AsTestingSequence<T>(this IAsyncEnumerable<T> source) =>
+            source is not null
+            ? new TestingAsyncSequence<T>(source)
+            : throw new ArgumentNullException(nameof(source));
+    }
+
+    /// <summary>
+    /// Sequence that asserts whether its iterator has been disposed
+    /// when it is disposed itself and also whether GetEnumerator() is
+    /// called exactly once or not.
+    /// </summary>
+    sealed class TestingAsyncSequence<T> : IAsyncEnumerable<T>, IDisposable
+    {
+        bool? _disposed;
+        IAsyncEnumerable<T> _source;
+
+        internal TestingAsyncSequence(IAsyncEnumerable<T> sequence) =>
+            _source = sequence;
+
+        public bool IsDisposed => _disposed == true;
+        public int MoveNextCallCount { get; private set; }
+
+        void IDisposable.Dispose() =>
+            AssertDisposed();
+
+        /// <summary>
+        /// Checks that the iterator was disposed, and then resets.
+        /// </summary>
+        void AssertDisposed()
+        {
+            if (_disposed is null)
+                return;
+            Assert.IsTrue(_disposed, "Expected sequence to be disposed.");
+            _disposed = null;
+        }
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            Assert.That(_source, Is.Not.Null, "LINQ operators should not enumerate a sequence more than once.");
+            var enumerator = _source.GetAsyncEnumerator(cancellationToken).AsWatchable();
+            _disposed = false;
+            enumerator.Disposed += delegate
+            {
+                Assert.That(_disposed, Is.False, "LINQ operators should not dispose a sequence more than once.");
+                _disposed = true;
+            };
+            var ended = false;
+            enumerator.MoveNextCalled += (_, moved) =>
+            {
+                Assert.That(ended, Is.False, "LINQ operators should not continue iterating a sequence that has terminated.");
+                ended = !moved;
+                MoveNextCallCount++;
+            };
+            _source = null;
+            return enumerator;
+        }
+    }
+}
