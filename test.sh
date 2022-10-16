9ecd1dc0093757d62d5c55277dbe0afc81cde4da
Atif Aziz <code@raboof.com>
2022-10-16 18:28:04 +0200

    Fix code coverage with new collector (#845)

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 set -e
 cd "$(dirname "$0")"
 ./build.sh $c
+if [[ -d "MoreLinq.Test/TestResults" ]]; then
+    rm -rf MoreLinq.Test/TestResults
+fi
 if [[ -z "$1" ]]; then
     configs="Debug Release"
 else
@@ -9,16 +12,15 @@ else
 fi
 for f in netcoreapp3.1 net6.0; do
     for c in $configs; do
-        if [[ "$c" == "Debug" ]]; then
-            coverage_args="-p:CollectCoverage=true
-                           -p:CoverletOutputFormat=opencover
-                           -p:Exclude=\"[NUnit*]*,[MoreLinq]MoreLinq.Extensions.*,[MoreLinq]MoreLinq.Experimental.*\""
-        else
-            unset coverage_args
-        fi
-        dotnet test --no-build -c $c -f $f MoreLinq.Test $coverage_args
+        dotnet test --no-build -c $c -f $f --settings MoreLinq.Test/coverlet.runsettings MoreLinq.Test
+        TEST_RESULTS_DIR="$(ls -dc MoreLinq.Test/TestResults/* | head -1)"
+        cp "$TEST_RESULTS_DIR/coverage.opencover.xml" "MoreLinq.Test/TestResults/coverage-$f-$c.opencover.xml"
     done
 done
+dotnet reportgenerator -reports:MoreLinq.Test/TestResults/coverage-*.opencover.xml \
+                       -reporttypes:Html\;TextSummary \
+                       -targetdir:MoreLinq.Test/TestResults/reports
+cat MoreLinq.Test/TestResults/reports/Summary.txt
 if [[ -z `which mono 2>/dev/null` ]]; then
     echo>&2 NOTE! Mono does not appear to be installed so unit tests
     echo>&2 against the Mono runtime will be skipped.
