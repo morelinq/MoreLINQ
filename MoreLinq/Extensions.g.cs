6f8e9016c84c70d9b7959f6b1f964c3696175e49
Stuart Turner <stuart@turner-isageek.com>
2022-11-14 17:17:45 -0600

    Reduce comment column width

--- MoreLinq/Extensions.g.cs
+++ MoreLinq/Extensions.g.cs
@@ -4917,26 +4917,28 @@ namespace MoreLinq.Extensions
     public static partial class ScanByExtension
     {
         /// <summary>
-        /// Applies an accumulator function over sequence element keys, returning the keys along with intermediate
-        /// accumulator states.
+        /// Applies an accumulator function over sequence element keys,
+        /// returning the keys along with intermediate accumulator states.
         /// </summary>
-        /// <typeparam name="TSource">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TSource">Type of the elements of the source
+        /// sequence.</typeparam>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TState">Type of the state.</typeparam>
         /// <param name="source">The source sequence.</param>
         /// <param name="keySelector">
         /// A function that returns the key given an element.</param>
         /// <param name="seedSelector">
-        /// A function to determine the initial value for the accumulator that is invoked once per key
-        /// encountered.</param>
+        /// A function to determine the initial value for the accumulator that
+        /// is invoked once per key encountered.</param>
         /// <param name="accumulator">
         /// An accumulator function invoked for each element.</param>
         /// <returns>
         /// A sequence of keys paired with intermediate accumulator states.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref name="seedSelector"/>, or <paramref
-        /// name="accumulator"/> is <see langword="null"/>.
+        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref
+        /// name="seedSelector"/>, or <paramref name="accumulator"/> is <see
+        /// langword="null"/>.
         /// </exception>
 
         public static IEnumerable<KeyValuePair<TKey, TState>> ScanBy<TSource, TKey, TState>(
@@ -4947,28 +4949,32 @@ namespace MoreLinq.Extensions
             => MoreEnumerable.ScanBy(source, keySelector, seedSelector, accumulator);
 
         /// <summary>
-        /// Applies an accumulator function over sequence element keys, returning the keys along with intermediate
-        /// accumulator states. An additional parameter specifies the comparer to use to compare keys.
+        /// Applies an accumulator function over sequence element keys,
+        /// returning the keys along with intermediate accumulator states. An
+        /// additional parameter specifies the comparer to use to compare keys.
         /// </summary>
-        /// <typeparam name="TSource">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TSource">Type of the elements of the source
+        /// sequence.</typeparam>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TState">Type of the state.</typeparam>
         /// <param name="source">The source sequence.</param>
         /// <param name="keySelector">
         /// A function that returns the key given an element.</param>
         /// <param name="seedSelector">
-        /// A function to determine the initial value for the accumulator that is invoked once per key
-        /// encountered.</param>
+        /// A function to determine the initial value for the accumulator that
+        /// is invoked once per key encountered.</param>
         /// <param name="accumulator">
         /// An accumulator function invoked for each element.</param>
-        /// <param name="comparer">The equality comparer to use to determine whether or not keys are equal. If <see
-        /// langword="null"/>, <see cref="EqualityComparer{T}.Default"/> is used.</param>
+        /// <param name="comparer">The equality comparer to use to determine
+        /// whether or not keys are equal. If <see langword="null"/>, <see
+        /// cref="EqualityComparer{T}.Default"/> is used.</param>
         /// <returns>
         /// A sequence of keys paired with intermediate accumulator states.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref name="seedSelector"/>, or <paramref
-        /// name="accumulator"/> is <see langword="null"/>.
+        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref
+        /// name="seedSelector"/>, or <paramref name="accumulator"/> is <see
+        /// langword="null"/>.
         /// </exception>
 
         public static IEnumerable<KeyValuePair<TKey, TState>> ScanBy<TSource, TKey, TState>(
--- MoreLinq/ScanBy.cs
+++ MoreLinq/ScanBy.cs
@@ -23,26 +23,28 @@ namespace MoreLinq
     static partial class MoreEnumerable
     {
         /// <summary>
-        /// Applies an accumulator function over sequence element keys, returning the keys along with intermediate
-        /// accumulator states.
+        /// Applies an accumulator function over sequence element keys,
+        /// returning the keys along with intermediate accumulator states.
         /// </summary>
-        /// <typeparam name="TSource">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TSource">Type of the elements of the source
+        /// sequence.</typeparam>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TState">Type of the state.</typeparam>
         /// <param name="source">The source sequence.</param>
         /// <param name="keySelector">
         /// A function that returns the key given an element.</param>
         /// <param name="seedSelector">
-        /// A function to determine the initial value for the accumulator that is invoked once per key
-        /// encountered.</param>
+        /// A function to determine the initial value for the accumulator that
+        /// is invoked once per key encountered.</param>
         /// <param name="accumulator">
         /// An accumulator function invoked for each element.</param>
         /// <returns>
         /// A sequence of keys paired with intermediate accumulator states.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref name="seedSelector"/>, or <paramref
-        /// name="accumulator"/> is <see langword="null"/>.
+        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref
+        /// name="seedSelector"/>, or <paramref name="accumulator"/> is <see
+        /// langword="null"/>.
         /// </exception>
 
         public static IEnumerable<KeyValuePair<TKey, TState>> ScanBy<TSource, TKey, TState>(
@@ -55,28 +57,32 @@ namespace MoreLinq
         }
 
         /// <summary>
-        /// Applies an accumulator function over sequence element keys, returning the keys along with intermediate
-        /// accumulator states. An additional parameter specifies the comparer to use to compare keys.
+        /// Applies an accumulator function over sequence element keys,
+        /// returning the keys along with intermediate accumulator states. An
+        /// additional parameter specifies the comparer to use to compare keys.
         /// </summary>
-        /// <typeparam name="TSource">Type of the elements of the source sequence.</typeparam>
+        /// <typeparam name="TSource">Type of the elements of the source
+        /// sequence.</typeparam>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TState">Type of the state.</typeparam>
         /// <param name="source">The source sequence.</param>
         /// <param name="keySelector">
         /// A function that returns the key given an element.</param>
         /// <param name="seedSelector">
-        /// A function to determine the initial value for the accumulator that is invoked once per key
-        /// encountered.</param>
+        /// A function to determine the initial value for the accumulator that
+        /// is invoked once per key encountered.</param>
         /// <param name="accumulator">
         /// An accumulator function invoked for each element.</param>
-        /// <param name="comparer">The equality comparer to use to determine whether or not keys are equal. If <see
-        /// langword="null"/>, <see cref="EqualityComparer{T}.Default"/> is used.</param>
+        /// <param name="comparer">The equality comparer to use to determine
+        /// whether or not keys are equal. If <see langword="null"/>, <see
+        /// cref="EqualityComparer{T}.Default"/> is used.</param>
         /// <returns>
         /// A sequence of keys paired with intermediate accumulator states.
         /// </returns>
         /// <exception cref="ArgumentNullException">
-        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref name="seedSelector"/>, or <paramref
-        /// name="accumulator"/> is <see langword="null"/>.
+        /// <paramref name="source"/>, <paramref name="keySelector"/>, <paramref
+        /// name="seedSelector"/>, or <paramref name="accumulator"/> is <see
+        /// langword="null"/>.
         /// </exception>
 
         public static IEnumerable<KeyValuePair<TKey, TState>> ScanBy<TSource, TKey, TState>(
@@ -90,8 +96,6 @@ namespace MoreLinq
             if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
             if (seedSelector == null) throw new ArgumentNullException(nameof(seedSelector));
             if (accumulator == null) throw new ArgumentNullException(nameof(accumulator));
-
-            comparer ??= EqualityComparer<TKey>.Default;
 
             return _(comparer ?? EqualityComparer<TKey>.Default);
 
