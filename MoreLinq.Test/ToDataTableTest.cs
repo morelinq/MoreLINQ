bd097241827b32b710e18888009c191ff7cd4051
Atif Aziz <code@raboof.com>
2023-01-15 12:55:36 +0100

    Fix CA1805: Do not initialize unnecessarily
    https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1805

--- MoreLinq.Test/ToDataTableTest.cs
+++ MoreLinq.Test/ToDataTableTest.cs
@@ -192,7 +192,9 @@ namespace MoreLinq.Test
 
         struct Point
         {
+#pragma warning disable CA1805 // Do not initialize unnecessarily (avoids CS0649)
             public static Point Empty = new Point();
+#pragma warning restore CA1805 // Do not initialize unnecessarily
             public bool IsEmpty => X == 0 && Y == 0;
             public int X { get; }
             public int Y { get; }
