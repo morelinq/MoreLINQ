3ba47dd555bef798a4915b938f4704dea34835fd
Atif Aziz <code@raboof.com>
2023-01-29 22:32:26 +0100

    Revert "Test fresh extensions wrapper (generated) code"
    This reverts commit 982a2b70e21d9f7bf34c4aa219b3ce5d502e1930.

--- MoreLinq/Index.cs
+++ MoreLinq/Index.cs
@@ -24,15 +24,15 @@ namespace MoreLinq
     static partial class MoreEnumerable
     {
         /// <summary>
-        /// Returns a sequence of <see cref="KeyValuePair{TKey,TValue}"/> where the key is the
-        /// zero-based index of the value in the source sequence.
+        /// Returns a sequence of <see cref="KeyValuePair{TKey,TValue}"/>
+        /// where the key is the zero-based index of the value in the source
+        /// sequence.
         /// </summary>
-        /// <typeparam name="TSource">Type of elements in <paramref name="source"/>
-        /// sequence.</typeparam>
+        /// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
         /// <param name="source">The source sequence.</param>
         /// <returns>A sequence of <see cref="KeyValuePair{TKey,TValue}"/>.</returns>
-        /// <remarks>
-        /// This operator uses deferred execution and streams its results.</remarks>
+        /// <remarks>This operator uses deferred execution and streams its
+        /// results.</remarks>
 
         public static IEnumerable<KeyValuePair<int, TSource>> Index<TSource>(this IEnumerable<TSource> source)
         {
