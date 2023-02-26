b234b52f8141d5c5f9acfb476188bf898cfccb50
Atif Aziz <code@raboof.com>
2023-02-26 21:42:08 +0100

    Remove final blank line in generated "Fold" overloads

--- MoreLinq/Fold.g.cs
+++ MoreLinq/Fold.g.cs
@@ -437,6 +437,5 @@ namespace MoreLinq
             var elements = source.Fold(16);
             return folder(elements[0], elements[1], elements[2], elements[3], elements[4], elements[5], elements[6], elements[7], elements[8], elements[9], elements[10], elements[11], elements[12], elements[13], elements[14], elements[15]);
         }
-
     }
 }
