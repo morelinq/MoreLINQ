8db4bef22ce0ba465e980d9e381d84f99aae31db
Pawel Flajszer <45766938+pflajszer@users.noreply.github.com>
2023-06-24 11:17:40 +0200

    Remove obsolete "Concat" method
    This is a squashed merge of PR #1002 that closes #993.

--- MoreLinq/Append.cs
+++ MoreLinq/Append.cs
@@ -38,18 +38,5 @@ namespace MoreLinq
                 ? node.Concat(tail)
                 : PendNode<T>.WithSource(head).Concat(tail);
         }
-
-        /// <summary>
-        /// Returns a sequence consisting of the head elements and the given tail element.
-        /// </summary>
-        /// <typeparam name="T">Type of sequence</typeparam>
-        /// <param name="head">All elements of the head. Must not be null.</param>
-        /// <param name="tail">Tail element of the new sequence.</param>
-        /// <returns>A sequence consisting of the head elements and the given tail element.</returns>
-        /// <remarks>This operator uses deferred execution and streams its results.</remarks>
-
-        [Obsolete("Use " + nameof(Append) + " instead.")]
-        public static IEnumerable<T> Concat<T>(this IEnumerable<T> head, T tail) =>
-            head.Append(tail);
     }
 }
