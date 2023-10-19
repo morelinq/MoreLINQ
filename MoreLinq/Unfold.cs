d325375208a5a4580db581ef0f986f335c686f5a
Atif Aziz <code@raboof.com>
2023-10-19 18:14:01 +0200

    Fix example per "swap result and state order"

--- MoreLinq/Unfold.cs
+++ MoreLinq/Unfold.cs
@@ -45,7 +45,7 @@ namespace MoreLinq
         /// <code><![CDATA[
         /// var fibonacciNumbersLowerThan100 =
         ///     MoreEnumerable.Unfold((Curr: 0, Next: 1),
-        ///                           s => s.Curr < 100 ? (true, (s.Next, s.Curr + s.Next), s.Curr) : default);
+        ///                           s => s.Curr < 100 ? (true, s.Curr, (s.Next, s.Curr + s.Next)) : default);
         /// ]]></code>
         /// The <c>result</c> will be a sequence that will yield
         /// <c>{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 }</c>.
