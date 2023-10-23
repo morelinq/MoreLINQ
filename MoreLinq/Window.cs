dd8e8a89bfb13af9f6082b6911460eb06e9bae69
Atif Aziz <code@raboof.com>
2023-10-23 21:32:51 +0200

    Remove "Windowed" that's superseded by "Window"
    This is a squashed merge of PR #1017 that closes #1016.

--- MoreLinq/Window.cs
+++ MoreLinq/Window.cs
@@ -73,25 +73,5 @@ namespace MoreLinq
                 yield return window;
             }
         }
-
-        /// <summary>
-        /// Processes a sequence into a series of sub-sequences representing a windowed subset of the original.
-        /// </summary>
-        /// <typeparam name="TSource">The type of the elements of the source sequence.</typeparam>
-        /// <param name="source">The sequence to evaluate a sliding window over.</param>
-        /// <param name="size">The size (number of elements) in each window.</param>
-        /// <returns>
-        /// A series of sequences representing each sliding window subsequence.</returns>
-        /// <remarks>
-        /// <para>
-        /// The number of sequences returned is: <c>Max(0, sequence.Count() - windowSize) +
-        /// 1</c></para>
-        /// <para>
-        /// Returned sub-sequences are buffered, but the overall operation is streamed.</para>
-        /// </remarks>
-
-        [Obsolete("Use " + nameof(Window) + " instead.")]
-        public static IEnumerable<IEnumerable<TSource>> Windowed<TSource>(this IEnumerable<TSource> source, int size) =>
-            source.Window(size);
     }
 }
