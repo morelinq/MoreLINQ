2fe88a550fee99a313f46d7865a72a96eb815324
Julien Aspirot <julien.aspirot@usherbrooke.ca>
2023-11-15 14:18:20 -0500

    add blanklines

--- MoreLinq.Test/DuplicatesTest.cs
+++ MoreLinq.Test/DuplicatesTest.cs
@@ -14,6 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
+
 namespace MoreLinq.Test
 {
     using System.Collections.Generic;
